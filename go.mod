faf8b1d38a30949fb34ad1d3e579298a07c40430
Sergey Cherepanov <cheggaaa@gmail.com>
2019-11-01 01:03:49 +0300

    linkpreview: parse content

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/anytypeio/go-anytype-middleware
 go 1.12
 
 require (
+	github.com/PuerkitoBio/goquery v1.5.0 // indirect
 	github.com/anytypeio/go-anytype-library v0.0.0-20191019100520-f545fa654778
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/protobuf v1.3.2
 	github.com/ipfs/go-log v0.0.1
+	github.com/mauidude/go-readability v0.0.0-20141216012317-2f30b1a346f1
+	github.com/microcosm-cc/bluemonday v1.0.2
 	github.com/otiai10/opengraph v1.1.0
 	github.com/stretchr/testify v1.3.0
 	github.com/textileio/go-textile v0.7.2-0.20190907000013-95a885123536
--- util/linkpreview/linkpreview.go
+++ util/linkpreview/linkpreview.go
@@ -2,30 +2,37 @@ package linkpreview
 
 import (
 	"context"
+	"io"
 	"net/http"
 	"path/filepath"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/anytypeio/go-anytype-middleware/pb"
+	"github.com/mauidude/go-readability"
+	"github.com/microcosm-cc/bluemonday"
 	"github.com/otiai10/opengraph"
 )
 
 func New() LinkPreview {
-	return &linkPreview{}
+	return &linkPreview{bmPolicy: bluemonday.NewPolicy().AddSpaceWhenStrippingTag(true)}
 }
 
 type LinkType string
 
 const (
 	// read no more than 400 kb
-	maxBytesToRead = 400000
+	maxBytesToRead     = 400000
+	maxDescriptionSize = 200
 )
 
 type LinkPreview interface {
 	Fetch(ctx context.Context, url string) (pb.LinkPreviewResponse, error)
 }
 
-type linkPreview struct{}
+type linkPreview struct {
+	bmPolicy *bluemonday.Policy
+}
 
 func (l *linkPreview) Fetch(ctx context.Context, url string) (pb.LinkPreviewResponse, error) {
 	rt := &proxyRoundTripper{RoundTripper: http.DefaultTransport}
@@ -37,7 +44,11 @@ func (l *linkPreview) Fetch(ctx context.Context, url string) (pb.LinkPreviewResp
 		}
 		return pb.LinkPreviewResponse{}, err
 	}
-	return l.convertOGToInfo(og), nil
+	res := l.convertOGToInfo(og)
+	if len(res.Description) == 0 {
+		res.Description = l.findContent(rt.lastBody)
+	}
+	return res, nil
 }
 
 func (l *linkPreview) convertOGToInfo(og *opengraph.OpenGraph) (i pb.LinkPreviewResponse) {
@@ -51,6 +62,25 @@ func (l *linkPreview) convertOGToInfo(og *opengraph.OpenGraph) (i pb.LinkPreview
 	}
 	if len(og.Image) != 0 {
 		i.ImageUrl = og.Image[0].URL
+	}
+	return
+}
+
+func (l *linkPreview) findContent(data []byte) (content string) {
+	defer func() {
+		if e := recover(); e != nil {
+			// ignore possible panic while html parsing
+		}
+	}()
+	doc, err := readability.NewDocument(string(data))
+	if err != nil {
+		return
+	}
+	content = doc.Content()
+	content = strings.TrimSpace(l.bmPolicy.Sanitize(content))
+	content = strings.Join(strings.Fields(content), " ") // removes repetitive whitespaces
+	if utf8.RuneCountInString(content) > maxDescriptionSize {
+		content = string([]rune(content)[:maxDescriptionSize]) + "..."
 	}
 	return
 }
@@ -73,13 +103,32 @@ func (l *linkPreview) makeNonHtml(url string, resp *http.Response) (i pb.LinkPre
 type proxyRoundTripper struct {
 	http.RoundTripper
 	lastResponse *http.Response
+	lastBody     []byte
 }
 
 func (p *proxyRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	resp, err := p.RoundTripper.RoundTrip(req)
 	if err == nil {
 		p.lastResponse = resp
-		resp.Body = http.MaxBytesReader(nil, resp.Body, maxBytesToRead)
+		resp.Body = &limitReader{ReadCloser: resp.Body, rt: p}
 	}
 	return resp, err
+}
+
+type limitReader struct {
+	rt     *proxyRoundTripper
+	nTotal int
+	io.ReadCloser
+}
+
+func (l *limitReader) Read(p []byte) (n int, err error) {
+	if l.nTotal > maxBytesToRead {
+		return 0, io.EOF
+	}
+	n, err = l.ReadCloser.Read(p)
+	if err == nil || err == io.EOF {
+		l.rt.lastBody = append(l.rt.lastBody, p[:n]...)
+	}
+	l.nTotal += n
+	return
 }
