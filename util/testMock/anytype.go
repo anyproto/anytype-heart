3d8822ea7c377fd62cef744678b6e2187b059a64
Sergey Cherepanov <cheggaaa@gmail.com>
2020-01-24 16:53:04 +0300

    block history tests

--- util/testMock/anytype.go
+++ util/testMock/anytype.go
@@ -1,2 +1,3 @@
 //go:generate mockgen -package testMock -destination anytype_mock.go github.com/anytypeio/go-anytype-middleware/core/anytype Anytype,Block,BlockVersion,BlockVersionMeta,File,Image
+//go:generate mockgen -package testMock -destination history_mock.go github.com/anytypeio/go-anytype-middleware/core/block/history History
 package testMock
