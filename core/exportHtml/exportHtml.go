5e31c8cb916ff7ab1e9da4e95b6a206707c71558
Kirill <kirill@chain.cloud>
2020-01-29 13:17:00 +0300

    Add blocks to html: part of a naive implementation, need to add nesting

---
+++ core/exportHtml/exportHtml.go
@@ -0,0 +1,133 @@
+package exportHtml
+
+import (
+	"github.com/anytypeio/go-anytype-library/pb/model"
+	"reflect"
+	"strings"
+	"github.com/yosssi/gohtml"
+)
+
+/*oneof content {
+	Content.Dashboard dashboard = 11;
+	Content.Page page = 12;
+	Content.Dataview dataview = 13;
+
+	Content.Text text = 14;
+	Content.File file = 15;
+	Content.Layout layout = 16;
+	Content.Div div = 17;
+	Content.Bookmark bookmark = 18;
+	Content.Icon icon = 19;
+	Content.Link link = 20;
+}*/
+
+/*enum Style {
+	Paragraph = 0;
+	Header1 = 1;
+	Header2 = 2;
+	Header3 = 3;
+	Header4 = 4;
+	Quote = 5;
+	Code = 6;
+	Title = 7;
+	Checkbox = 8;
+	Marked = 9;
+	Numbered = 10;
+	Toggle = 11;
+}*/
+
+/*message Mark {
+	Range range = 1; // range of symbols to apply this mark. From(symbol) To(symbol)
+	Type type = 2;
+	string param = 3; // link, color, etc
+
+	enum Type {
+		Strikethrough = 0;
+		Keyboard = 1;
+		Italic = 2;
+		Bold = 3;
+		Underscored = 4;
+		Link = 5;
+		TextColor = 6;
+		BackgroundColor = 7;
+	}
+}*/
+
+
+func BlocksToHtml (blocks []*model.Block) string {
+	var htmlArr []string
+	for _, b := range blocks {
+		switch reflect.TypeOf(b.GetContent()) {
+			case reflect.TypeOf(&model.BlockContentOfText{}): htmlArr = append(htmlArr, textBlockToHtml(b))
+			case reflect.TypeOf(&model.BlockContentOfFile{}): htmlArr = append(htmlArr, fileToHtml(b))
+			case reflect.TypeOf(&model.BlockContentOfBookmark{}): htmlArr = append(htmlArr, bookmarkToHtml(b))
+			case reflect.TypeOf(&model.BlockContentOfDiv{}): htmlArr = append(htmlArr, divToHtml(b))
+			case reflect.TypeOf(&model.BlockContentOfIcon{}): htmlArr = append(htmlArr, iconToHtml(b))
+
+			case reflect.TypeOf(&model.BlockContentOfDashboard{}): break
+			case reflect.TypeOf(&model.BlockContentOfPage{}): break
+			case reflect.TypeOf(&model.BlockContentOfDataview{}): break
+			case reflect.TypeOf(&model.BlockContentOfLayout{}): break
+			case reflect.TypeOf(&model.BlockContentOfLink{}): break
+		}
+	}
+
+	output := strings.Join(htmlArr, "\n")
+	return gohtml.Format(wrapHtml(output))
+}
+
+func wrapHtml (innerHtml string) string {
+	title := "" // TODO: add title
+	styles := "" // TODO: add styles
+	output := `
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+				<title>` + title + `</title>
+				<style type="text/css">` + styles + `</style>
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/styles/github.min.css">
+				<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/highlight.min.js"></script>
+			</head>
+			<body>
+				<div class="container">` + innerHtml + `</div>
+			</body>
+		</html>`
+
+	return output
+}
+
+func textBlockToHtml (block *model.Block) string {
+	return ""
+}
+
+func fileToHtml (block *model.Block) string {
+	output := ""
+
+	if block.GetFile().Type == model.BlockContentFile_Image {
+		 // TODO: how to export an image from the IPFS? We need an export service, I guess
+	}
+
+	return output
+}
+
+func bookmarkToHtml (block *model.Block) string {
+	return `
+		<div class="bookmark" ${attrJoin(attr)}>
+			<a href="${bookMark.url}" ${attrJoin(attr)}>
+				${bookMark.title}
+			</a>
+		</div>`
+
+}
+
+func divToHtml (block *model.Block) string {
+	return "<hr>"
+}
+
+func iconToHtml (block *model.Block) string {
+	icon := "" // TODO: get icon
+	return `<div class="smile">` + icon + `</div>`
+}
+
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826
 	github.com/stretchr/testify v1.4.0
 	github.com/textileio/go-textile v0.7.8-0.20200102164400-98b263e32c0c
+	github.com/yosssi/gohtml v0.0.0-20190915184251-7ff6f235ecaf // indirect
 	gotest.tools v2.1.0+incompatible
 )
 
