f0627f92c750992c4c653ea571b9815e0cc4e161
requilence <requilence@gmail.com>
2021-02-24 14:09:50 +0400

    add isHidden detail for obj type and relation

--- core/block/source/bundledrelation.go
+++ core/block/source/bundledrelation.go
@@ -50,6 +50,7 @@ func getDetailsForRelation(prefix string, rel *relation.Relation) *types.Struct 
 		bundle.RelationKeyDescription.String(): pbtypes.String(rel.Description),
 		bundle.RelationKeyId.String(): pbtypes.String(prefix+rel.Key),
 		bundle.RelationKeyLayout.String(): pbtypes.Float64(float64(relation.ObjectType_relation)),
+		"isHidden": pbtypes.Bool(rel.Hidden),
 	}}
 }
 
