aa5c81900e545b3453dc5485277f8ac1561f1eb2
Roman Khafizianov <requilence@gmail.com>
2021-07-08 14:52:05 +0400

    reindex fulltext to add missing files
    reindex bundled to include readonly detail

--- core/indexer/indexer.go
+++ core/indexer/indexer.go
@@ -34,9 +34,9 @@ const (
 	// increasing counters below will trigger existing account to reindex their data
 	ForceThreadsObjectsReindexCounter int32 = 0 // reindex thread-based objects
 	ForceFilesReindexCounter          int32 = 2 // reindex ipfs-file-based objects
-	ForceBundledObjectsReindexCounter int32 = 1 // reindex objects like anytypeProfile
+	ForceBundledObjectsReindexCounter int32 = 2 // reindex objects like anytypeProfile
 	ForceIdxRebuildCounter            int32 = 5 // erases localstore indexes and reindex all type of objects (no need to increase ForceThreadsObjectsReindexCounter & ForceFilesReindexCounter)
-	ForceFulltextIndexCounter         int32 = 1 // performs fulltext indexing for all type of objects (useful when we change fulltext config)
+	ForceFulltextIndexCounter         int32 = 2 // performs fulltext indexing for all type of objects (useful when we change fulltext config)
 )
 
 var log = logging.Logger("anytype-doc-indexer")
