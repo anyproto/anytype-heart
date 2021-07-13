d133a90f5f8b4a422810eb8cfdd48db0f685a30a
Roman Khafizianov <requilence@gmail.com>
2021-07-13 14:09:03 +0400

    rework deadlock fix

--- pkg/lib/localstore/objectstore/objects.go
+++ pkg/lib/localstore/objectstore/objects.go
@@ -1400,9 +1400,8 @@ func (m *dsObjectStore) updateObjectDetails(txn ds.Txn, id string, before model.
 	return nil
 }
 
+// should be called under the mutex
 func (m *dsObjectStore) sendUpdatesToSubscriptions(id string, details *types.Struct) {
-	m.l.Lock()
-	defer m.l.Unlock()
 	detCopy := pbtypes.CopyStruct(details)
 	detCopy.Fields[database.RecordIDField] = pb.ToValue(id)
 	for i := range m.subscriptions {
@@ -1486,6 +1485,8 @@ func (m *dsObjectStore) ListIds() ([]string, error) {
 }
 
 func (m *dsObjectStore) UpdateRelationsInSet(setId, objType, creatorId string, relations []*model.Relation) error {
+	m.l.Lock()
+	defer m.l.Unlock()
 	txn, err := m.ds.NewTransaction(false)
 	if err != nil {
 		return err
