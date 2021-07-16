eaa4cc90de0651ec11db8187a09405c534eb8a28
Roman Khafizianov <requilence@gmail.com>
2021-07-16 10:19:01 +0400

    order: fix nil deref

--- pkg/lib/database/filter/order_test.go
+++ pkg/lib/database/filter/order_test.go
@@ -29,6 +29,13 @@ func TestKeyOrder_Compare(t *testing.T) {
 		assert.Equal(t, -1, asc.Compare(a, b))
 	})
 
+	t.Run("asc_nil", func(t *testing.T) {
+		a := testGetter{"k": nil}
+		b := testGetter{"k": pbtypes.String("")}
+		asc := KeyOrder{Key: "k", Type: model.BlockContentDataviewSort_Asc}
+		assert.Equal(t, -1, asc.Compare(a, b))
+	})
+
 	t.Run("asc_notemptylast", func(t *testing.T) {
 		a := testGetter{"k": pbtypes.String("a")}
 		b := testGetter{"k": pbtypes.String("")}
