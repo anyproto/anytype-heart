2615d1101129fae5013724421d9c0c3c5d80de81
Roman Khafizianov <requilence@gmail.com>
2021-09-07 16:55:56 +0300

    fix comment

--- core/block/editor/state/state.go
+++ core/block/editor/state/state.go
@@ -553,8 +553,8 @@ func (s *State) apply(fast, one, withLayouts bool) (msgs []simple.EventMessage, 
 		// revert lastModified update if we don't have any actual changes being made
 		prevModifiedDate := pbtypes.Get(s.parent.LocalDetails(), bundle.RelationKeyLastModifiedDate.String())
 		if s.localDetails != nil {
-			if prevModifiedDate == nil {
-				//delete(s.localDetails.Fields, bundle.RelationKeyLastModifiedDate.String())
+			if prevModifiedDate != nil {
+				log.With("thread", s.rootId).Errorf("failed to revert prev modifed date: prev date is nil")
 			} else {
 				s.localDetails.Fields[bundle.RelationKeyLastModifiedDate.String()] = prevModifiedDate
 			}
