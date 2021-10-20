320fd7c90e6701575232c08d0aaaf8b155968187
Roman Khafizianov <requilence@gmail.com>
2021-10-20 20:05:02 +0400

    fix files tests
    run offload tests before others

--- core/files_test.go
+++ core/files_test.go
@@ -43,6 +43,53 @@ func TestFile(t *testing.T) {
 		return
 	}
 
+	t.Run("image_offload", func(t *testing.T) {
+		coreService := mw.app.MustComponent(core.CName).(core.Service)
+		f, err := os.OpenFile("../pkg/lib/mill/testdata/image.jpeg", os.O_RDONLY, 0600)
+		require.NoError(t, err)
+
+		i, err := coreService.ImageAdd(context.Background(), files.WithReader(f))
+		require.NoError(t, err)
+
+		bytesRemoved, err := coreService.FileOffload(i.Hash())
+		require.NoError(t, err)
+		require.Equal(t, uint64(168368), bytesRemoved)
+
+		bytesRemoved, err = coreService.FileOffload(i.Hash())
+		require.NoError(t, err)
+		require.Equal(t, uint64(0), bytesRemoved)
+	})
+
+	t.Run("offload_all", func(t *testing.T) {
+		if os.Getenv("ANYTYPE_TEST_FULL") != "1" {
+			return
+		}
+		coreService := mw.app.MustComponent(core.CName).(core.Service)
+		for i := 1; i <= 200; i++ {
+			r := rand.New(rand.NewSource(int64(i)))
+			// lets the file not fit in the single block
+			b := make([]byte, 1024*1024*3)
+			r.Read(b)
+
+			f, err := coreService.FileAdd(context.Background(), files.WithBytes(b))
+			require.NoError(t, err)
+			require.Equal(t, int64(1024*1024*3), f.Meta().Size)
+		}
+		m, err := getMetrics(filepath.Join(rootPath, coreService.Account(), "ipfslite"))
+		require.NoError(t, err)
+		require.Equal(t, 10, m.NumVLOG)
+		fmt.Printf("BADGER METRICS AFTER ADD: %+v\n", m)
+		resp := mw.FileListOffload(&pb.RpcFileListOffloadRequest{IncludeNotPinned: true})
+		require.Equal(t, 0, int(resp.Error.Code), resp.Error.Description)
+		require.Equal(t, int32(200), resp.FilesOffloaded)
+		require.Equal(t, uint64(1024*1024*3*200+247400), resp.BytesOffloaded) // 247400 is the overhead for the links and meta
+
+		m, err = getMetrics(filepath.Join(rootPath, coreService.Account(), "ipfslite"))
+		require.NoError(t, err)
+		fmt.Printf("BADGER METRICS AFTER OFFLOAD: %+v\n", m)
+		require.LessOrEqual(t, m.NumVLOG, 3)
+
+	})
 	t.Run("image_should_open_as_object", func(t *testing.T) {
 		respUploadImage := mw.UploadFile(&pb.RpcUploadFileRequest{LocalPath: "./block/testdata/testdir/a.jpg"})
 		require.Equal(t, 0, int(respUploadImage.Error.Code), respUploadImage.Error.Description)
@@ -76,49 +123,6 @@ func TestFile(t *testing.T) {
 		respUploadFile2 := mw.UploadFile(&pb.RpcUploadFileRequest{LocalPath: "./block/testdata/testdir/a.jpg"})
 		require.Equal(t, 0, int(respUploadFile1.Error.Code), respUploadFile1.Error.Description)
 		require.Equal(t, respUploadFile1.Hash, respUploadFile2.Hash)
-	})
-	t.Run("image_offload", func(t *testing.T) {
-		coreService := mw.app.MustComponent(core.CName).(core.Service)
-		f, err := os.OpenFile("../pkg/lib/mill/testdata/image.jpeg", os.O_RDONLY, 0600)
-		require.NoError(t, err)
-
-		i, err := coreService.ImageAdd(context.Background(), files.WithReader(f))
-		require.NoError(t, err)
-
-		bytesRemoved, err := coreService.FileOffload(i.Hash())
-		require.NoError(t, err)
-		require.Equal(t, uint64(168368), bytesRemoved)
-	})
-
-	t.Run("offload_all", func(t *testing.T) {
-		if os.Getenv("ANYTYPE_TEST_FULL") != "1" {
-			//	return
-		}
-		coreService := mw.app.MustComponent(core.CName).(core.Service)
-		for i := 1; i <= 200; i++ {
-			r := rand.New(rand.NewSource(int64(i)))
-			// lets the file not fit in the single block
-			b := make([]byte, 1024*1024*3)
-			r.Read(b)
-
-			f, err := coreService.FileAdd(context.Background(), files.WithBytes(b))
-			require.NoError(t, err)
-			require.Equal(t, int64(1024*1024*3), f.Meta().Size)
-		}
-		m, err := getMetrics(filepath.Join(rootPath, coreService.Account(), "ipfslite"))
-		require.NoError(t, err)
-		require.Equal(t, 10, m.NumVLOG)
-		fmt.Printf("BADGER METRICS AFTER ADD: %+v\n", m)
-		resp := mw.FileListOffload(&pb.RpcFileListOffloadRequest{IncludeNotPinned: true})
-		require.Equal(t, 0, int(resp.Error.Code), resp.Error.Description)
-		require.Equal(t, uint64(1024*1024*3*200+247400), resp.BytesOffloaded) // 247400 is the overhead for the links and meta
-		require.Equal(t, int32(200), resp.FilesOffloaded)
-
-		m, err = getMetrics(filepath.Join(rootPath, coreService.Account(), "ipfslite"))
-		require.NoError(t, err)
-		fmt.Printf("BADGER METRICS AFTER OFFLOAD: %+v\n", m)
-		require.Equal(t, 2, m.NumVLOG)
-
 	})
 
 }
