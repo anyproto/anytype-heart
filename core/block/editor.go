a78d807009b750c93f2f5839f60293675d5acd9f
Timofey Molchanov <timopheym@gmail.com>
2021-11-24 15:46:16 +0100

    change method BlockSetFileStyle to BlackListSetFileStyle

--- core/block/editor.go
+++ core/block/editor.go
@@ -648,9 +648,9 @@ func (s *service) DropFiles(req pb.RpcExternalDropFilesRequest) (err error) {
 	})
 }
 
-func (s *service) SetFileStyle(ctx *state.Context, req pb.RpcBlockSetFileStyleRequest) error {
-	return s.Do(req.ContextId, func(b smartblock.SmartBlock) error {
-		return b.(file.File).SetFileStyle(ctx, req)
+func (s *service) SetFileStyle(ctx *state.Context, contextId string, style model.BlockContentFileStyle, blockIds ...string) error {
+	return s.Do(contextId, func(b smartblock.SmartBlock) error {
+		return b.(file.File).SetFileStyle(ctx, style, blockIds...)
 	})
 }
 
