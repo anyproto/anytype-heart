c17c9e5dfa4ea211dded7d4fa614f584e0ee3d4c
Roman Khafizianov <requilence@gmail.com>
2021-11-24 12:25:57 +0300

    refactor IPFS blockstore GC

--- pkg/lib/datastore/clientds/clientds.go
+++ pkg/lib/datastore/clientds/clientds.go
@@ -3,8 +3,11 @@ package clientds
 import (
 	"fmt"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/logging"
+	"github.com/ipfs/go-datastore/query"
 	"os"
 	"path/filepath"
+	"sort"
+	"strconv"
 	"time"
 
 	"github.com/hashicorp/go-multierror"
@@ -23,7 +26,12 @@ const (
 	liteDSDir      = "ipfslite"
 	logstoreDSDir  = "logstore"
 	threadsDbDSDir = "collection" + string(os.PathSeparator) + "eventstore"
+
+	valueLogExtenderKey  = "_extend"
+	valueLogExtenderSize = 1024
 )
+
+var log = logging.Logger("anytype-clientds")
 
 type clientds struct {
 	running     bool
@@ -106,6 +114,90 @@ func (r *clientds) Run() error {
 	}
 	r.running = true
 	return nil
+}
+
+type ValueLogInfo struct {
+	Index int64
+	Size  int64
+}
+
+func (r *clientds) RunBlockstoreGC() (freed int64, err error) {
+	getValueLogsInfo := func() (totalSize int64, valLogs []*ValueLogInfo, err error) {
+		err = filepath.Walk(filepath.Join(r.repoPath, liteDSDir), func(_ string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			ext := filepath.Ext(info.Name())
+			switch ext {
+			case ".vlog":
+				index, err := strconv.ParseInt(info.Name(), 10, 64)
+				if err != nil {
+					return nil
+				}
+				valLogs = append(valLogs, &ValueLogInfo{Index: index, Size: info.Size()})
+				totalSize += info.Size()
+			}
+			return nil
+		})
+		if err != nil {
+			return
+		}
+
+		sort.Slice(valLogs, func(i, j int) bool {
+			return valLogs[i].Index < valLogs[j].Index
+		})
+		return totalSize, valLogs, nil
+	}
+
+	totalSizeBefore, valLogs, err := getValueLogsInfo()
+	if err != nil {
+		return 0, err
+	}
+	log.With("vlogs_count", len(valLogs)).With("vlogs", valLogs).Infof("GC: before the cycle")
+
+	if len(valLogs) == 0 {
+		return 0, nil
+	}
+
+	if valLogs[len(valLogs)-1].Size > DefaultConfig.Litestore.ValueLogFileSize {
+		// in case we have the last value log exceeding the max value log size
+		v := make([]byte, valueLogExtenderSize)
+		r.litestoreDS.Put(ds.NewKey(valueLogExtenderKey), v)
+	}
+
+	var total int
+	var maxErrors = 1
+	for {
+		// set the discard ratio to the lowest value means we want to rewrite value log if we have any values removed
+		err = r.litestoreDS.DB.RunValueLogGC(0.000000000001)
+		if err != nil && err.Error() == "Value log GC attempt didn't result in any cleanup" {
+			maxErrors--
+			if maxErrors == 0 {
+				log.Infof("badger gc exit on %d attempt", total)
+				break
+			}
+			continue
+		}
+		total++
+	}
+
+	totalSizeAfter, vlogsAfter, err := getValueLogsInfo()
+
+	results, err := r.litestoreDS.Query(query.Query{Limit: 0, KeysOnly: true, ReturnsSizes: true})
+	var (
+		keysTotal     int64
+		keysTotalSize int64
+	)
+
+	for result := range results.Next() {
+		keysTotal++
+		keysTotalSize += int64(result.Size)
+	}
+
+	if keysTotalSize > totalSizeAfter /* + DefaultConfig.Litestore.ValueLogFileSize*/ {
+		log.With("vlogs_count", len(vlogsAfter)).With("keys_size_b", keysTotalSize).With("vlog_overhead_b", totalSizeAfter-keysTotalSize).With("vlogs", vlogsAfter).Errorf("Badger GC: got badger value logs overhead after GC")
+	}
+	return totalSizeAfter - totalSizeBefore, nil
 }
 
 func (r *clientds) PeerstoreDS() (ds.Batching, error) {
