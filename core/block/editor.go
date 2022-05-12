9be1407bc1e22dda7ed92ec34c93360539c3b52f
Sergey <juesid@protonmail.com>
2022-05-12 18:38:18 +0300

    [x1az3m] Refactor: extract code into separate function
    Refactor tests
    
    Add test case

--- core/block/editor.go
+++ core/block/editor.go
@@ -9,7 +9,6 @@ import (
 	"github.com/anytypeio/go-anytype-middleware/metrics"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/core"
 	"github.com/anytypeio/go-anytype-middleware/util/ocache"
-	"github.com/globalsign/mgo/bson"
 	ds "github.com/ipfs/go-datastore"
 	"github.com/textileio/go-threads/core/thread"
 
@@ -25,7 +24,6 @@ import (
 	"github.com/anytypeio/go-anytype-middleware/core/block/editor/state"
 	"github.com/anytypeio/go-anytype-middleware/core/block/editor/stext"
 	"github.com/anytypeio/go-anytype-middleware/core/block/simple"
-	"github.com/anytypeio/go-anytype-middleware/core/block/simple/base"
 	"github.com/anytypeio/go-anytype-middleware/core/block/simple/text"
 	"github.com/anytypeio/go-anytype-middleware/pb"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/bundle"
@@ -991,153 +989,7 @@ func (s *service) ListAvailableRelations(objectId string) (aggregatedRelations [
 // ConvertChildrenToPages extracts child blocks from the page to separate pages and
 // replaces these blocks to the links to these pages
 func (s *service) ConvertChildrenToPages(req pb.RpcBlockListConvertChildrenToPagesRequest) (linkIds []string, err error) {
-	blocks := make(map[string]*model.Block)
-	err = s.Do(req.ContextId, func(contextBlock smartblock.SmartBlock) error {
-		for _, b := range contextBlock.Blocks() {
-			blocks[b.Id] = b
-		}
-		return nil
-	})
-	if err != nil {
-		return linkIds, err
-	}
-
-	var toRemove []string
-
-	type linkAdd struct {
-		blockId string
-		pageId  string
-	}
-	var linksToAdd []linkAdd
-
-	visited := map[string]struct{}{}
-	for _, blockId := range req.BlockIds {
-		if blocks[blockId] == nil || blocks[blockId].GetText() == nil {
-			continue
-		}
-
-		subtree := extractBlocksSubtree(blockId, blocks, visited)
-		if len(subtree) == 0 {
-			continue
-		}
-		// Collect all child blocks to remove later
-		for _, b := range subtree {
-			if b.Id != blockId {
-				toRemove = append(toRemove, b.Id)
-			}
-		}
-
-		newRoot, newBlocks := reassignSubtreeIds(blockId, subtree)
-
-		// Build a state for the new page from child blocks
-		st := state.NewDoc("", nil).NewState()
-		for _, b := range newBlocks {
-			st.Add(base.NewBase(b))
-		}
-		st.Add(base.NewBase(&model.Block{
-			// This id will be replaced by id of the new page
-			Id:          "_root",
-			ChildrenIds: []string{newRoot},
-		}))
-
-		fields := map[string]*types.Value{
-			"name": pbtypes.String(blocks[blockId].GetText().Text),
-		}
-		if req.ObjectType != "" {
-			fields[bundle.RelationKeyType.String()] = pbtypes.String(req.ObjectType)
-		}
-		_, pageId, err := s.CreatePageFromState(nil, "", pb.RpcBlockCreatePageRequest{
-			ContextId: req.ContextId,
-			Details: &types.Struct{
-				Fields: fields,
-			},
-		}, st)
-		if err != nil {
-			return linkIds, err
-		}
-		linksToAdd = append(linksToAdd, linkAdd{
-			blockId: blockId,
-			pageId:  pageId,
-		})
-	}
-
-	err = s.Do(req.ContextId, func(b smartblock.SmartBlock) error {
-		st := b.NewState()
-
-		t := basic.NewStateTransformer(st)
-
-		for _, l := range linksToAdd {
-			linkId, err := t.CreateBlock("", pb.RpcBlockCreateRequest{
-				TargetId: l.blockId,
-				Block: &model.Block{
-					Content: &model.BlockContentOfLink{
-						Link: &model.BlockContentLink{
-							TargetBlockId: l.pageId,
-							Style:         model.BlockContentLink_Page,
-						},
-					},
-				},
-				Position: model.Block_Replace,
-			})
-			if err != nil {
-				return fmt.Errorf("create link to page %s: %w", l.pageId, err)
-			}
-			linkIds = append(linkIds, linkId)
-		}
-		t.CutBlocks(toRemove)
-
-		return b.Apply(st)
-	})
-
-	return linkIds, err
-}
-
-// extractBlocksSubtree extracts a subtree with specific root from blocks and marks visited blocks
-func extractBlocksSubtree(root string, blocks map[string]*model.Block, visited map[string]struct{}) []*model.Block {
-	var (
-		queue     = []string{root}
-		extracted []*model.Block
-	)
-
-	for len(queue) > 0 {
-		cur := queue[0]
-		queue = queue[1:]
-
-		b, ok := blocks[cur]
-		if !ok {
-			continue
-		}
-		if _, ok := visited[cur]; ok {
-			continue
-		}
-		visited[cur] = struct{}{}
-
-		extracted = append(extracted, b)
-		queue = append(queue, b.ChildrenIds...)
-	}
-
-	return extracted
-}
-
-// reassignSubtreeIds makes a copy of a subtree of blocks and assign a new id for each block
-func reassignSubtreeIds(root string, blocks []*model.Block) (string, []*model.Block) {
-	res := make([]*model.Block, 0, len(blocks))
-	mapping := map[string]string{}
-	for _, b := range blocks {
-		newId := bson.NewObjectId().Hex()
-		mapping[b.Id] = newId
-
-		newBlock := pbtypes.CopyBlock(b)
-		newBlock.Id = newId
-		res = append(res, newBlock)
-	}
-
-	for _, b := range res {
-		for i, id := range b.ChildrenIds {
-			b.ChildrenIds[i] = mapping[id]
-		}
-	}
-	return mapping[root], res
+	return editor.ExtractPages(s.Do, s.CreatePageFromState, req)
 }
 
 func (s *service) MoveBlocksToNewPage(ctx *state.Context, req pb.RpcBlockListMoveToNewPageRequest) (linkId string, err error) {
---
+++ core/block/editor/extract_pages_test.go
@@ -0,0 +1,187 @@
+package editor
+
+import (
+	"testing"
+
+	"github.com/anytypeio/go-anytype-middleware/core/block/editor/smartblock"
+	"github.com/anytypeio/go-anytype-middleware/core/block/editor/smartblock/smarttest"
+	"github.com/anytypeio/go-anytype-middleware/core/block/editor/state"
+	"github.com/anytypeio/go-anytype-middleware/core/block/simple"
+	"github.com/anytypeio/go-anytype-middleware/pb"
+	"github.com/anytypeio/go-anytype-middleware/pkg/lib/pb/model"
+	"github.com/anytypeio/go-anytype-middleware/util/slice"
+	"github.com/globalsign/mgo/bson"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type testExtractPages struct {
+	pages map[string]*smarttest.SmartTest
+}
+
+func (t *testExtractPages) Add(page *smarttest.SmartTest) {
+	t.pages[page.Id()] = page
+}
+
+func (t *testExtractPages) Do(id string, apply func(b smartblock.SmartBlock) error) error {
+	return apply(t.pages[id])
+}
+
+func (t *testExtractPages) CreatePageFromState(ctx *state.Context, groupId string, req pb.RpcBlockCreatePageRequest, state *state.State) (linkId string, pageId string, err error) {
+	id := bson.NewObjectId().Hex()
+	page := smarttest.New(id)
+	t.pages[id] = page
+
+	state.SetRootId(id)
+	page.Doc = state
+
+	return "", id, nil
+}
+
+func newTextBlock(id string, childrenIds []string, text string) simple.Block {
+	return simple.New(&model.Block{Id: id, ChildrenIds: childrenIds, Content: &model.BlockContentOfText{
+		Text: &model.BlockContentText{
+			Text: text,
+		},
+	}})
+}
+
+func assertNoCommonElements(t *testing.T, a, b []string) {
+	got := slice.Difference(a, b)
+
+	assert.Equal(t, got, a)
+}
+
+func assertHasTextBlocks(t *testing.T, page *smarttest.SmartTest, texts []string) {
+	var gotTexts []string
+
+	for _, b := range page.Blocks() {
+		if b.GetText() != nil {
+			gotTexts = append(gotTexts, b.GetText().Text)
+		}
+	}
+
+	assert.Subset(t, gotTexts, texts)
+}
+
+func assertLinkedPageHasTextBlocks(t *testing.T, ts testExtractPages, sourcePage *smarttest.SmartTest, linkId string, texts []string) {
+	b := sourcePage.Pick(linkId).Model()
+
+	link := b.GetLink()
+	require.NotNil(t, link)
+
+	page := ts.pages[link.TargetBlockId]
+	require.NotNil(t, page)
+
+	assertHasTextBlocks(t, page, texts)
+}
+
+func TestExtractPages(t *testing.T) {
+	makeTestPage := func() *smarttest.SmartTest {
+		sb := smarttest.New("test")
+		sb.AddBlock(newTextBlock("test", []string{"1", "2"}, ""))
+		sb.AddBlock(newTextBlock("1", []string{"1.1", "1.2"}, "text 1"))
+		sb.AddBlock(newTextBlock("1.1", []string{"1.1.1"}, "text 1.1"))
+		sb.AddBlock(newTextBlock("1.1.1", nil, "text 1.1.1"))
+		sb.AddBlock(newTextBlock("1.2", nil, "text 1.2"))
+		sb.AddBlock(newTextBlock("2", []string{"2.1"}, "text 2"))
+		sb.AddBlock(newTextBlock("2.1", nil, "text 2.1"))
+		return sb
+	}
+
+	for _, tc := range []struct {
+		name               string
+		blockIds           []string
+		wantPagesWithTexts [][]string
+	}{
+		{
+			name:               "undefined block",
+			blockIds:           []string{"3.1.1"},
+			wantPagesWithTexts: [][]string{},
+		},
+		{
+			name:     "leaf block",
+			blockIds: []string{"1.1.1"},
+			wantPagesWithTexts: [][]string{
+				{"text 1.1.1"},
+			},
+		},
+		{
+			name:     "block with one child",
+			blockIds: []string{"2"},
+			wantPagesWithTexts: [][]string{
+				{"text 2", "text 2.1"},
+			},
+		},
+		{
+			name:     "block with one child, child id also presents in request",
+			blockIds: []string{"2", "2.1"},
+			wantPagesWithTexts: [][]string{
+				{"text 2", "text 2.1"},
+			},
+		},
+		{
+			name:     "block with multiple children",
+			blockIds: []string{"1"},
+			wantPagesWithTexts: [][]string{
+				{
+					"text 1",
+					"text 1.1", "text 1.1.1",
+					"text 1.2",
+				},
+			},
+		},
+		{
+			name: "multiple blocks, all descendants present in requests",
+			blockIds: []string{
+				"1", "1.1", "1.1.1", "1.2",
+				"2", "2.1",
+			},
+			wantPagesWithTexts: [][]string{
+				// First page
+				{
+					"text 1",
+					"text 1.1", "text 1.1.1",
+					"text 1.2",
+				},
+				// Second page
+				{
+					"text 2",
+					"text 2.1",
+				},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			ts := testExtractPages{
+				pages: map[string]*smarttest.SmartTest{},
+			}
+
+			sb := makeTestPage()
+			ts.Add(sb)
+
+			req := pb.RpcBlockListConvertChildrenToPagesRequest{
+				ContextId:  "test",
+				BlockIds:   tc.blockIds,
+				ObjectType: "page",
+			}
+			linkIds, err := ExtractPages(ts.Do, ts.CreatePageFromState, req)
+			assert.NoError(t, err)
+
+			var gotBlockIds []string
+			for _, b := range sb.Blocks() {
+				gotBlockIds = append(gotBlockIds, b.Id)
+			}
+
+			// Check that requested blocks are removed from page
+			assertNoCommonElements(t, gotBlockIds, req.BlockIds)
+
+			// Check that linked pages has desired text blocks
+			require.Len(t, linkIds, len(tc.wantPagesWithTexts))
+			for i, wantTexts := range tc.wantPagesWithTexts {
+				assertLinkedPageHasTextBlocks(t, ts, sb, linkIds[i], wantTexts)
+			}
+
+		})
+	}
+}
