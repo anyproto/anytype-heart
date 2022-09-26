dc30b3f9271e2fab0e7f81ebedfc03a3f880fdfe
Pavel Zavyalov <zavyalov.one@gmail.com>
2022-09-26 13:58:19 +0600

    [feat-GO-212-changes-for-kanban] add rand test cases

--- util/slice/diff_test.go
+++ util/slice/diff_test.go
@@ -1,8 +1,11 @@
 package slice
 
 import (
+	"github.com/globalsign/mgo/bson"
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func Test_Diff(t *testing.T) {
@@ -20,11 +23,40 @@ func Test_Diff(t *testing.T) {
 
 func Test_ChangesApply(t *testing.T) {
 	origin := []string{"000", "001", "002", "003", "004", "005", "006", "007", "008", "009"}
-	changed := []string{"000", "008", "001", "002", "003", "005", "006", "007", "009", "004"}
+	changed := []string{"000", "008", "001", "002", "003", "005", "006", "007", "009", "004", "new"}
 
 	chs := Diff(origin, changed)
 
 	res := ApplyChanges(origin, chs)
 
 	assert.Equal(t, changed, res)
+}
+
+func Test_RandomSameLength(t *testing.T) {
+	for i := 0; i< 1000; i++ {
+		l := randNum(5, 100)
+		origin := getRandArray(l)
+		changed := make([]string, len(origin))
+		copy(changed, origin)
+		rand.Shuffle(len(changed),
+			func(i, j int) { changed[i], changed[j] = changed[j], changed[i] })
+
+		chs := Diff(origin, changed)
+		res := ApplyChanges(origin, chs)
+
+		assert.Equal(t, res, changed)
+	}
+}
+
+func randNum(min, max int) int{
+	rand.Seed(time.Now().UnixNano())
+	return  rand.Intn(max - min) + min
+}
+
+func getRandArray(len int) []string {
+	res := make([]string, len)
+	for i := 0; i < len; i++ {
+		res[i] = bson.NewObjectId().Hex()
+	}
+	return res
 }
