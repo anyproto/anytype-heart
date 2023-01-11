2e088bbb3904a7e6cae61100e2d22397b65b45d3
Roman Khafizianov <requilence@gmail.com>
2023-01-11 18:42:52 +0100

    WithDefaultFeaturedRelations: add a case for set

--- core/block/editor/set.go
+++ core/block/editor/set.go
@@ -15,7 +15,6 @@ import (
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/localstore/objectstore"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/pb/model"
 	"github.com/anytypeio/go-anytype-middleware/util/pbtypes"
-	"github.com/anytypeio/go-anytype-middleware/util/slice"
 )
 
 var ErrAlreadyHasDataviewBlock = fmt.Errorf("already has the dataview block")
@@ -57,21 +56,12 @@ func (p *Set) Init(ctx *smartblock.InitContext) (err error) {
 		return err
 	}
 
-	var featuredRelations []string
-	if ctx.State != nil {
-		featuredRelations = pbtypes.GetStringList(ctx.State.Details(), bundle.RelationKeyFeaturedRelations.String())
-	}
-	// Add missing required featured relations
-	featuredRelations = slice.Union(featuredRelations, []string{bundle.RelationKeyDescription.String(), bundle.RelationKeyType.String(), bundle.RelationKeySetOf.String()})
-	featuredRelations = slice.Remove(featuredRelations, bundle.RelationKeyCreator.String())
-
 	templates := []template.StateTransformer{
 		template.WithDataviewRelationMigrationRelation(template.DataviewBlockId, bundle.TypeKeyBookmark.URL(), bundle.RelationKeyUrl, bundle.RelationKeySource),
 		template.WithObjectTypesAndLayout([]string{bundle.TypeKeySet.URL()}, model.ObjectType_set),
 		template.WithRelations([]bundle.RelationKey{bundle.RelationKeySetOf}),
 		template.WithDescription,
-		template.WithDetail(bundle.RelationKeyFeaturedRelations, pbtypes.StringList(featuredRelations)),
-		template.WithFeaturedRelations,
+		template.WithDefaultFeaturedRelations,
 		template.WithBlockEditRestricted(p.Id()),
 	}
 	if dvBlock := p.Pick(template.DataviewBlockId); dvBlock != nil {
