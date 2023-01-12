4bac2a3c1a41549b07a7ff563e0e82546987014d
AnastasiaShemyakinskaya <shem98a@mail.ru>
2023-01-12 16:31:21 +0800

    GO-573: move ping code to validator
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/block/import/notion/validator.go
+++ core/block/import/notion/validator.go
@@ -1,22 +1,82 @@
 package notion
 
 import (
+	"bytes"
 	"context"
-	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
 
 	"github.com/anytypeio/go-anytype-middleware/core/block/import/notion/api/client"
-	"github.com/anytypeio/go-anytype-middleware/core/block/import/notion/api/ping"
 	"github.com/anytypeio/go-anytype-middleware/pb"
+	"github.com/anytypeio/go-anytype-middleware/pkg/lib/logging"
 )
 
+var (
+	ErrorInternal     = errors.New("internal")
+	ErrorUnauthorized = errors.New("unauthorized")
+)
+
+var logger = logging.Logger("notion-ping")
+
+const (
+	endpoint = "/users?page_size=1"
+)
+
+type Service struct {
+	client *client.Client
+}
+
+// NewPingService is a constructor for PingService
+func NewPingService(client *client.Client) *Service {
+	return &Service{
+		client: client,
+	}
+}
+
+// Ping is function to validate token, it calls users endpoint and checks given error,
+func (s *Service) Ping(ctx context.Context, apiKey string) error {
+	req, err := s.client.PrepareRequest(ctx, apiKey, http.MethodGet, endpoint, &bytes.Buffer{})
+	if err != nil {
+		logger.With(zap.String("method", "PrepareRequest")).Error(err)
+		return errors.Wrap(ErrorInternal, fmt.Sprintf("ping: %s", err.Error()))
+	}
+	res, err := s.client.HTTPClient.Do(req)
+	if err != nil {
+		logger.With(zap.String("method", "Do")).Error(err)
+		return errors.Wrap(ErrorInternal, fmt.Sprintf("ping: %s", err.Error()))
+	}
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		logger.With(zap.String("method", "ioutil.ReadAll")).Error(err)
+		return errors.Wrap(ErrorInternal, err.Error())
+	}
+	if res.StatusCode != http.StatusOK {
+		if res.StatusCode == http.StatusUnauthorized {
+			return ErrorUnauthorized
+		}
+		err = client.TransformHTTPCodeToError(b)
+		if err != nil {
+			return errors.Wrap(ErrorInternal, err.Error())
+		}
+	}
+	return nil
+}
+
 type TokenValidator struct {
-	ping *ping.Service
+	ping *Service
 }
 
 func NewTokenValidator() *TokenValidator {
 	cl := client.NewClient()
 	return &TokenValidator{
-		ping: ping.New(cl),
+		ping: NewPingService(cl),
 	}
 }
 
@@ -24,10 +84,10 @@ func NewTokenValidator() *TokenValidator {
 func (v TokenValidator) Validate(ctx context.Context,
 	apiKey string) pb.RpcObjectImportNotionTokenValidateResponseErrorCode {
 	err := v.ping.Ping(ctx, apiKey)
-	if errors.Is(err, ping.ErrorInternal) {
+	if errors.Is(err, ErrorInternal) {
 		return pb.RpcObjectImportNotionTokenValidateResponseError_INTERNAL_ERROR
 	}
-	if errors.Is(err, ping.ErrorUnauthorized) {
+	if errors.Is(err, ErrorUnauthorized) {
 		return pb.RpcObjectImportNotionTokenValidateResponseError_UNAUTHORIZED
 	}
 	return pb.RpcObjectImportNotionTokenValidateResponseError_NULL
