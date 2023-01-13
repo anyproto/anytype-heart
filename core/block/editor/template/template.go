ff68ce63063ce874727bb44ce65f184a554f38aa
Roman Khafizianov <requilence@gmail.com>
2023-01-13 15:27:55 +0100

    WithDefaultFeaturedRelations: add description for set

--- core/block/editor/template/template.go
+++ core/block/editor/template/template.go
@@ -277,7 +277,7 @@ var WithDefaultFeaturedRelations = StateTransformer(func(s *state.State) {
 		if layout == model.ObjectType_basic || layout == model.ObjectType_note {
 			fr = []string{bundle.RelationKeyType.String()}
 		} else if layout == model.ObjectType_set {
-			fr = []string{bundle.RelationKeyType.String(), bundle.RelationKeySetOf.String()}
+			fr = []string{bundle.RelationKeyDescription.String(), bundle.RelationKeyType.String(), bundle.RelationKeySetOf.String()}
 		}
 		s.SetDetail(bundle.RelationKeyFeaturedRelations.String(), pbtypes.StringList(fr))
 	}
