525fe8e9f3c1f03f1f78bac0db476340c07823db
Pavel Zavyalov <zavyalov.one@gmail.com>
2023-01-16 13:15:55 +0600

    [GO-120-include-time-sort] add time location fix

--- util/time/time.go
+++ util/time/time.go
@@ -1,13 +1,13 @@
 package time
 
 import (
-	"github.com/gogo/protobuf/types"
-
 	"github.com/anytypeio/go-anytype-middleware/util/pbtypes"
+	"github.com/gogo/protobuf/types"
+	"time"
 )
 
-func CutValueToDay(t *types.Value) *types.Value {
-	fullDate := int64(t.GetNumberValue())
-	seconds := fullDate % 86400
-	return pbtypes.Int64(fullDate - seconds)
+func CutValueToDay(val *types.Value) *types.Value {
+	t := time.Unix(int64(val.GetNumberValue()), 0)
+	roundTime := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+	return pbtypes.Int64(roundTime.Unix())
 }
