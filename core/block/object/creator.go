5e89e698bfa59eb152dfbddc9da64f2cd7835703
Roman Khafizianov <requilence@gmail.com>
2023-02-10 16:31:00 +0100

    fix incorrect featuredRelations after objectToSet

--- core/block/object/creator.go
+++ core/block/object/creator.go
@@ -234,6 +234,9 @@ func (c *Creator) CreateSet(req *pb.RpcObjectCreateSetRequest) (setID string, ne
 
 	// TODO remove it, when schema will be refactored
 	source := req.Source
+	if len(req.Source) > 0 {
+		req.Details.Fields[bundle.RelationKeySetOf.String()] = pbtypes.StringList(req.Source)
+	}
 	if len(source) == 0 {
 		source = []string{bundle.TypeKeyPage.URL()}
 	}
@@ -250,9 +253,7 @@ func (c *Creator) CreateSet(req *pb.RpcObjectCreateSetRequest) (setID string, ne
 	if dvSchema != nil {
 		blockContent = &dvContent
 	}
-	if len(req.Source) > 0 {
-		req.Details.Fields[bundle.RelationKeySource.String()] = pbtypes.StringList(req.Source)
-	}
+
 	if blockContent != nil {
 		for i, view := range blockContent.Dataview.Views {
 			if view.Relations == nil {
