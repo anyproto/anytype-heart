6ced2b3343905c7b8305bfae96c25b59c1fe300f
Mikhail Iudin <yudin.reso@gmail.com>
2023-03-08 20:29:51 +0300

    GO-386 Improve search query relevance in bleve

--- pkg/lib/localstore/ftsearch/ftsearch_test.go
+++ pkg/lib/localstore/ftsearch/ftsearch_test.go
@@ -2,10 +2,12 @@ package ftsearch
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/anytypeio/go-anytype-middleware/app/testapp"
 	"github.com/anytypeio/go-anytype-middleware/core/wallet"
+	"github.com/blevesearch/bleve/v2"
 	"github.com/golang/mock/gomock"
-	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -32,7 +34,61 @@ func newFixture(path string, t *testing.T) *fixture {
 }
 
 func TestNewFTSearch(t *testing.T) {
-	tmpDir, _ := ioutil.TempDir("", "")
+	tmpDir, _ := os.MkdirTemp("", "")
+	assertSearch(t, tmpDir)
+	assertThaiSubstrFound(t, tmpDir)
+	assertChineseFound(t, tmpDir)
+	assertFoundPartsOfTheWords(t, tmpDir)
+}
+
+func assertChineseFound(t *testing.T, tmpDir string) {
+	fixture := newFixture(tmpDir, t)
+	ft := fixture.ft
+	require.NoError(t, ft.Index(SearchDoc{
+		Id:    "1",
+		Title: "",
+		Text:  "你好",
+	}))
+	require.NoError(t, ft.Index(SearchDoc{
+		Id:    "2",
+		Title: "",
+		Text:  "交代",
+	}))
+	require.NoError(t, ft.Index(SearchDoc{
+		Id:    "3",
+		Title: "",
+		Text:  "长江大桥",
+	}))
+
+	queries := []string{
+		"你好世界",
+		"亲口交代",
+		"长江",
+	}
+
+	for _, qry := range queries {
+		validateSearch(t, ft, qry, 1)
+	}
+
+	_ = ft.Close()
+}
+
+func assertThaiSubstrFound(t *testing.T, tmpDir string) {
+	fixture := newFixture(tmpDir, t)
+	ft := fixture.ft
+	require.NoError(t, ft.Index(SearchDoc{
+		Id:    "test",
+		Title: "ตัวอย่าง",
+		Text:  "พรระเจ้า \n kumamon",
+	}))
+
+	validateSearch(t, ft, "ระเ", 1)
+	validateSearch(t, ft, "ระเ ma", 1)
+
+	_ = ft.Close()
+}
+
+func assertSearch(t *testing.T, tmpDir string) {
 	fixture := newFixture(tmpDir, t)
 	ft := fixture.ft
 	require.NoError(t, ft.Index(SearchDoc{
@@ -40,16 +96,128 @@ func TestNewFTSearch(t *testing.T) {
 		Title: "one",
 		Text:  "two",
 	}))
-	res, err := ft.Search("one")
-	require.NoError(t, err)
-	assert.Len(t, res, 1)
-	ft.Close()
-	fixture = newFixture(tmpDir, t)
-	ft = fixture.ft
 
+	validateSearch(t, ft, "one", 1)
+	validateSearch(t, ft, "two", 1)
+
+	_ = ft.Close()
+}
+
+func assertFoundPartsOfTheWords(t *testing.T, tmpDir string) {
+	fixture := newFixture(tmpDir, t)
+	ft := fixture.ft
+	require.NoError(t, ft.Index(SearchDoc{
+		Id:    "1",
+		Title: "This is the title",
+		Text:  "two",
+	}))
+	require.NoError(t, ft.Index(SearchDoc{
+		Id:    "2",
+		Title: "is the title",
+		Text:  "two",
+	}))
+
+	validateSearch(t, ft, "this", 1)
+	validateSearch(t, ft, "his", 1)
+	validateSearch(t, ft, "is", 2)
+	validateSearch(t, ft, "i t", 2)
+
+	_ = ft.Close()
+}
+
+func validateSearch(t *testing.T, ft FTSearch, qry string, times int) {
+	res, err := ft.Search(qry)
 	require.NoError(t, err)
-	res, err = ft.Search("one")
-	require.NoError(t, err)
-	assert.Len(t, res, 1)
-	ft.Close()
+	assert.Len(t, res, times)
+}
+
+func TestChineseSearch(t *testing.T) {
+	//given
+	index := givenPrefilledChineseIndex()
+	defer func() { _ = index.Close() }()
+
+	expected := givenExpectedChinese()
+
+	//when
+	queries := []string{
+		"你好世界",
+		"亲口交代",
+		"长江",
+	}
+
+	//then
+	result := validateChinese(queries, index)
+	assert.Equal(t, expected, result)
+}
+
+func prettify(res *bleve.SearchResult) string {
+	type Result struct {
+		Id    string  `json:"id"`
+		Score float64 `json:"score"`
+	}
+	results := []Result{}
+	for _, item := range res.Hits {
+		results = append(results, Result{item.ID, item.Score})
+	}
+	b, err := json.Marshal(results)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
+
+func validateChinese(queries []string, index bleve.Index) [3]string {
+	result := [3]string{}
+	for i, q := range queries {
+		req := bleve.NewSearchRequest(bleve.NewQueryStringQuery(q))
+		req.Highlight = bleve.NewHighlight()
+		res, err := index.Search(req)
+		if err != nil {
+			panic(err)
+		}
+		result[i] = prettify(res)
+	}
+	return result
+}
+
+func givenExpectedChinese() [3]string {
+	return [3]string{
+		`[{"id":"1","score":0.3192794660708729}]`,
+		`[{"id":"2","score":0.3192794660708729}]`,
+		`[{"id":"3","score":0.8888941720598743}]`,
+	}
+}
+
+func givenPrefilledChineseIndex() bleve.Index {
+	tmpDir, _ := os.MkdirTemp("", "")
+	messages := []struct {
+		Id   string
+		Text string
+	}{
+		{
+			Id:   "1",
+			Text: "你好",
+		},
+		{
+			Id:   "2",
+			Text: "交代",
+		},
+		{
+			Id:   "3",
+			Text: "长江大桥",
+		},
+	}
+
+	indexMapping := makeMapping()
+
+	index, err := bleve.New(tmpDir, indexMapping)
+	if err != nil {
+		panic(err)
+	}
+	for _, msg := range messages {
+		if err := index.Index(msg.Id, msg); err != nil {
+			panic(err)
+		}
+	}
+	return index
 }
