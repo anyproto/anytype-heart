fce840b8c927bc48b3feb8b05f4d1996cd75c1be
AnastasiaShemyakinskaya <shem98a@mail.ru>
2023-03-31 17:50:15 +0300

    GO-1113: fix not working migration
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/block/export/export.go
+++ core/block/export/export.go
@@ -269,23 +269,16 @@ func (e *export) getNested(id string, docs map[string]*types.Struct) {
 }
 
 func (e *export) getExistedObjects() (map[string]*types.Struct, error) {
-	res, err := e.a.ObjectStore().ListIds()
+	res, err := e.a.ObjectStore().List()
 	if err != nil {
 		return nil, err
 	}
 	objectDetails := make(map[string]*types.Struct, len(res))
-	for _, id := range res {
-		details, storeErr := e.a.ObjectStore().GetWithLinksInfoByID(id)
-		if storeErr != nil {
+	for _, r := range res {
+		if !e.objectValid(r.Id, r) {
 			continue
 		}
-		if details == nil {
-			continue
-		}
-		if !e.objectValid(id, details.Info) {
-			continue
-		}
-		objectDetails[id] = details.Info.Details
+		objectDetails[r.Id] = r.Details
 
 	}
 	if err != nil {
