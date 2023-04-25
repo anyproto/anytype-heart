f5cbdf7dfc882ab9f61b958d9903d4214c75b831
Sergey <juesid@protonmail.com>
2023-04-25 18:17:59 +0200

    GO-897: Add missing relation key to status grouper

--- core/kanban/service.go
+++ core/kanban/service.go
@@ -4,18 +4,21 @@ import (
 	"crypto/md5" //nolint:all
 	"encoding/hex"
 	"errors"
+	"fmt"
+
+	"github.com/anytypeio/any-sync/app"
+
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/database"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/localstore/objectstore"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/pb/model"
 )
-import "github.com/anytypeio/any-sync/app"
 
 const (
 	CName = "kanban"
 )
 
 func New() Service {
-	return &service{groupColumns: make(map[model.RelationFormat]Grouper)}
+	return &service{groupColumns: make(map[model.RelationFormat]func(key string) Grouper)}
 }
 
 type Grouper interface {
@@ -32,15 +35,21 @@ type Service interface {
 
 type service struct {
 	objectStore  objectstore.ObjectStore
-	groupColumns map[model.RelationFormat]Grouper
+	groupColumns map[model.RelationFormat]func(string) Grouper
 }
 
 func (s *service) Init(a *app.App) (err error) {
 	s.objectStore = a.MustComponent(objectstore.CName).(objectstore.ObjectStore)
 
-	s.groupColumns[model.RelationFormat_status] = &GroupStatus{store: s.objectStore}
-	s.groupColumns[model.RelationFormat_tag] = &GroupTag{store: s.objectStore}
-	s.groupColumns[model.RelationFormat_checkbox] = &GroupCheckBox{}
+	s.groupColumns[model.RelationFormat_status] = func(key string) Grouper {
+		return &GroupStatus{key: key, store: s.objectStore}
+	}
+	s.groupColumns[model.RelationFormat_tag] = func(key string) Grouper {
+		return &GroupTag{Key: key, store: s.objectStore}
+	}
+	s.groupColumns[model.RelationFormat_checkbox] = func(key string) Grouper {
+		return &GroupCheckBox{}
+	}
 
 	return nil
 }
@@ -52,15 +61,16 @@ func (s *service) Name() (name string) {
 func (s *service) Grouper(key string) (Grouper, error) {
 	rel, err := s.objectStore.GetRelationByKey(key)
 	if err != nil {
-		return nil, err
+
+		return nil, fmt.Errorf("can't get relation %s: %v", key, err)
 	}
 
-	grouper, ok := s.groupColumns[rel.Format]
+	grouperFn, ok := s.groupColumns[rel.Format]
 	if !ok {
 		return nil, errors.New("unsupported relation format")
 	}
 
-	return grouper, nil
+	return grouperFn(key), nil
 }
 
 func GroupsToStrSlice(groups []*model.BlockContentDataviewGroup) []string {
