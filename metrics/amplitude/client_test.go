c172963dd47d5104d830c1bc7a7c148a9f6baf86
Roman Khafizianov <requilence@gmail.com>
2023-04-25 18:17:59 +0200

    GO-966 fux amplitud test

--- metrics/amplitude/client_test.go
+++ metrics/amplitude/client_test.go
@@ -2,34 +2,9 @@ package amplitude
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 )
-
-func ExampleEvent() {
-	keyResp, bodyResp, server := mockServer("event")
-	defer server.Close()
-
-	client := New("s3cr3ts")
-	client.eventEndpoint = server.URL
-
-	client.Event(Event{
-		UserID:    "0000001",
-		EventType: "joined",
-	})
-
-	key := <-keyResp
-	body := <-bodyResp
-	fmt.Printf("Key: %s\n%s", string(key), string(body))
-
-	// Output:
-	// Key: s3cr3ts
-	// {
-	//   "event_type": "joined",
-	//   "user_id": "0000001"
-	// }
-}
 
 func mockServer(msgKey string) (chan []byte, chan []byte, *httptest.Server) {
 	key, body := make(chan []byte, 1), make(chan []byte, 1)
