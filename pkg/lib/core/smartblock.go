edb6a41259ef904df3a5ce4faa950fbef3f354e1
Roman Khafizianov <requilence@gmail.com>
2023-04-25 18:17:59 +0200

    GO-966 remove unused code

--- pkg/lib/core/smartblock.go
+++ pkg/lib/core/smartblock.go
@@ -2,13 +2,13 @@ package core
 
 import (
 	"context"
+
 	"github.com/gogo/protobuf/proto"
 	"github.com/gogo/protobuf/types"
 	cbornode "github.com/ipfs/go-ipld-cbor"
 
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/core/smartblock"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/pb/model"
-	"github.com/anytypeio/go-anytype-middleware/pkg/lib/vclock"
 )
 
 const (
@@ -25,39 +25,10 @@ func init() {
 	cbornode.RegisterCborType(ProfileThreadEncryptionKeys{})
 }
 
-type SmartBlockContentChange struct {
-	state vclock.VClock
-	// to be discussed
-}
-
 type SmartBlockMeta struct {
 	ObjectTypes   []string
 	RelationLinks []*model.RelationLink
 	Details       *types.Struct
-}
-
-type SmartBlockMetaChange struct {
-	SmartBlockMeta
-	state vclock.VClock
-}
-
-func (meta *SmartBlockMetaChange) State() vclock.VClock {
-	return meta.state
-}
-
-func (meta *SmartBlockContentChange) State() vclock.VClock {
-	return meta.state
-}
-
-type SmartBlockChange struct {
-	Content *SmartBlockContentChange
-	Meta    *SmartBlockMetaChange
-}
-
-type SmartBlockVersion struct {
-	State    vclock.VClock
-	Snapshot SmartBlockSnapshot
-	Changes  []SmartBlockChange
 }
 
 type SmartBlock interface {
