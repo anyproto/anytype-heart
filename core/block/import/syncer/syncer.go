cbfd46c7fab36733ffb4ef2617251f6dd708a69a
AnastasiaShemyakinskaya <shem98a@mail.ru>
2023-04-25 18:17:59 +0200

    remove syncer
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/block/import/syncer/syncer.go
+++ core/block/import/syncer/syncer.go
@@ -4,18 +4,14 @@ import "github.com/anytypeio/go-anytype-middleware/core/block/simple"
 
 type Factory struct {
 	fs Syncer
-	bs Syncer
 	is Syncer
 }
 
-func New(fs *FileSyncer, bs *BookmarkSyncer, is *IconSyncer) *Factory {
-	return &Factory{fs: fs, bs: bs, is: is}
+func New(fs *FileSyncer, is *IconSyncer) *Factory {
+	return &Factory{fs: fs, is: is}
 }
 
 func (f *Factory) GetSyncer(b simple.Block) Syncer {
-	if bm := b.Model().GetBookmark(); bm != nil {
-		return f.bs
-	}
 	if file := b.Model().GetFile(); file != nil {
 		return f.fs
 	}
