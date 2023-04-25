bdc54bd6ce2b62c8d541f9582792be2813e401d9
Roman Khafizianov <requilence@gmail.com>
2023-04-25 18:17:59 +0200

    remove Account and Device from core

--- pkg/lib/core/core.go
+++ pkg/lib/core/core.go
@@ -3,8 +3,6 @@ package core
 import (
 	"context"
 	"fmt"
-	"os"
-	"path/filepath"
 	"strings"
 	"sync"
 
@@ -16,22 +14,15 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/anytypeio/go-anytype-middleware/core/anytype/config"
-	"github.com/anytypeio/go-anytype-middleware/core/configfetcher"
-	"github.com/anytypeio/go-anytype-middleware/core/event"
 	"github.com/anytypeio/go-anytype-middleware/core/wallet"
 	"github.com/anytypeio/go-anytype-middleware/metrics"
-	"github.com/anytypeio/go-anytype-middleware/pb"
-	"github.com/anytypeio/go-anytype-middleware/pkg/lib/cafe"
 	coresb "github.com/anytypeio/go-anytype-middleware/pkg/lib/core/smartblock"
-	"github.com/anytypeio/go-anytype-middleware/pkg/lib/datastore"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/files"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/localstore/addr"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/localstore/filestore"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/localstore/objectstore"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/logging"
-	"github.com/anytypeio/go-anytype-middleware/pkg/lib/pb/model"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/threads"
-	"github.com/anytypeio/go-anytype-middleware/space"
 )
 
 var log = logging.Logger("anytype-core")
@@ -39,16 +30,12 @@ var log = logging.Logger("anytype-core")
 var ErrObjectDoesNotBelongToWorkspace = fmt.Errorf("object does not belong to workspace")
 
 const (
-	CName  = "anytype"
-	tmpDir = "tmp"
+	CName = "anytype"
 )
 
 type Service interface {
-	Account() string // deprecated, use wallet component
-	Device() string  // deprecated, use wallet component
 	Stop() error
 	IsStarted() bool
-	SpaceService() space.Service
 
 	EnsurePredefinedBlocks(ctx context.Context) error
 	PredefinedBlocks() threads.DerivedSmartblockIds
@@ -67,16 +54,9 @@ type Service interface {
 	GetAllWorkspaces() ([]string, error)
 	GetWorkspaceIdForObject(objectId string) (string, error)
 
-	ObjectStore() objectstore.ObjectStore // deprecated
-	FileStore() filestore.FileStore       // deprecated
-	ThreadsIds() ([]string, error)        // deprecated
-
-	ObjectInfoWithLinks(id string) (*model.ObjectInfoWithLinks, error)
-
 	ProfileInfo
 
 	app.ComponentRunnable
-	TempDir() string
 }
 
 var _ app.Component = (*Anytype)(nil)
@@ -89,36 +69,24 @@ type ObjectsDeriver interface {
 }
 
 type Anytype struct {
-	files        *files.Service
-	cafe         cafe.Client
-	objectStore  objectstore.ObjectStore
-	fileStore    filestore.FileStore
-	fetcher      configfetcher.ConfigFetcher
-	sendEvent    func(event *pb.Event)
-	deriver      ObjectsDeriver
-	spaceService space.Service
-
-	ds datastore.Datastore
+	files       *files.Service
+	objectStore objectstore.ObjectStore
+	fileStore   filestore.FileStore
+	deriver     ObjectsDeriver
 
 	predefinedBlockIds threads.DerivedSmartblockIds
 
-	replicationWG    sync.WaitGroup
 	migrationOnce    sync.Once
 	lock             sync.Mutex
 	isStarted        bool // use under the lock
 	shutdownStartsCh chan struct {
 	} // closed when node shutdown starts
 
-	subscribeOnce       sync.Once
-	config              *config.Config
-	wallet              wallet.Wallet
-	tmpFolderAutocreate sync.Once
-	tempDir             string
-	commonFiles         fileservice.FileService
-}
+	subscribeOnce sync.Once
+	config        *config.Config
+	wallet        wallet.Wallet
 
-func (a *Anytype) ThreadsIds() ([]string, error) {
-	return nil, nil
+	commonFiles fileservice.FileService
 }
 
 func New() *Anytype {
@@ -132,35 +100,14 @@ func (a *Anytype) Init(ap *app.App) (err error) {
 	a.config = ap.MustComponent(config.CName).(*config.Config)
 	a.objectStore = ap.MustComponent(objectstore.CName).(objectstore.ObjectStore)
 	a.fileStore = ap.MustComponent(filestore.CName).(filestore.FileStore)
-	a.ds = ap.MustComponent(datastore.CName).(datastore.Datastore)
-	a.cafe = ap.MustComponent(cafe.CName).(cafe.Client)
 	a.files = ap.MustComponent(files.CName).(*files.Service)
 	a.commonFiles = ap.MustComponent(fileservice.CName).(fileservice.FileService)
-	a.sendEvent = ap.MustComponent(event.CName).(event.Sender).Send
-	a.fetcher = ap.MustComponent(configfetcher.CName).(configfetcher.ConfigFetcher)
 	a.deriver = ap.MustComponent(treegetter.CName).(ObjectsDeriver)
-	a.spaceService = ap.MustComponent(space.CName).(space.Service)
 	return
 }
 
 func (a *Anytype) Name() string {
 	return CName
-}
-
-func (a *Anytype) SpaceService() space.Service {
-	return a.spaceService
-}
-
-// Deprecated, use wallet component directly
-func (a *Anytype) Account() string {
-	pk := a.wallet.GetAccountPrivkey()
-	return pk.GetPublic().Account()
-}
-
-// Deprecated, use wallet component directly
-func (a *Anytype) Device() string {
-	pk := a.wallet.GetDevicePrivkey()
-	return pk.GetPublic().PeerId()
 }
 
 func (a *Anytype) Run(ctx context.Context) (err error) {
@@ -259,30 +206,5 @@ func (a *Anytype) Stop() error {
 		close(a.shutdownStartsCh)
 	}
 
-	// fixme useless!
-	a.replicationWG.Wait()
-
 	return nil
-}
-
-func (a *Anytype) TempDir() string {
-	// it shouldn't be a case when it is called before wallet init, but just in case lets add the check here
-	if a.wallet == nil || a.wallet.RootPath() == "" {
-		return os.TempDir()
-	}
-
-	var err error
-	// simultaneous calls to TempDir will wait for the once func to finish, so it will be fine
-	a.tmpFolderAutocreate.Do(func() {
-		path := filepath.Join(a.wallet.RootPath(), tmpDir)
-		err = os.MkdirAll(path, 0700)
-		if err != nil {
-			log.Errorf("failed to make temp dir, use the default system one: %s", err.Error())
-			a.tempDir = os.TempDir()
-		} else {
-			a.tempDir = path
-		}
-	})
-
-	return a.tempDir
 }
