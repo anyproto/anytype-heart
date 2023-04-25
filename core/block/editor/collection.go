55ae6be40c58789078cb5896f9ed4451db85aa3c
Sergey <juesid@protonmail.com>
2023-04-25 18:17:59 +0200

    Add hook to collection smartblock

--- core/block/editor/collection.go
+++ core/block/editor/collection.go
@@ -1,6 +1,7 @@
 package editor
 
 import (
+	"github.com/anytypeio/go-anytype-middleware/app"
 	"github.com/anytypeio/go-anytype-middleware/core/block/editor/smartblock"
 	relation2 "github.com/anytypeio/go-anytype-middleware/core/relation"
 	"github.com/anytypeio/go-anytype-middleware/pkg/lib/core"
@@ -20,5 +21,16 @@ func NewCollection(
 }
 
 func (c *Collection) Init(ctx *smartblock.InitContext) (err error) {
-	return c.Set.Init(ctx)
+	err = c.Set.Init(ctx)
+	if err != nil {
+		return
+	}
+
+	// TODO clean up
+	type collectionService interface {
+		RegisterCollection(sb smartblock.SmartBlock)
+	}
+	colService := app.MustComponent[collectionService](ctx.App)
+	colService.RegisterCollection(c.SmartBlock)
+	return nil
 }
