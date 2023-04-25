083c353eb6007c224ea5cfd7776b9e1bbc270dc9
mcrakhman <rakhmanov.m@gmail.com>
2023-04-25 18:17:59 +0200

    Fix validator tests compile error

--- core/block/import/notion/validator_test.go
+++ core/block/import/notion/validator_test.go
@@ -26,9 +26,8 @@ func Test_ValidateTokenNotValid(t *testing.T) {
 	tv := NewTokenValidator()
 	tv.ping = p
 
-	errCode, err := tv.Validate(context.TODO(), "123123")
-	assert.Equal(t, errCode, pb.RpcObjectImportNotionValidateTokenResponseError_UNAUTHORIZED)
-	assert.Equal(t, nil, err)
+	_, err := tv.Validate(context.TODO(), "123123")
+	assert.Equal(t, err, pb.RpcObjectImportNotionValidateTokenResponseError_UNAUTHORIZED)
 }
 
 func Test_ValidateTokenSuccess(t *testing.T) {
@@ -42,9 +41,8 @@ func Test_ValidateTokenSuccess(t *testing.T) {
 	tv := NewTokenValidator()
 	tv.ping = p
 
-	errCode, err := tv.Validate(context.TODO(), "123123")
-	assert.Equal(t, errCode, pb.RpcObjectImportNotionValidateTokenResponseError_NULL)
-	assert.Equal(t, nil, err)
+	_, err := tv.Validate(context.TODO(), "123123")
+	assert.Equal(t, err, pb.RpcObjectImportNotionValidateTokenResponseError_NULL)
 }
 
 func Test_ValidateTokenInternalError(t *testing.T) {
@@ -61,9 +59,8 @@ func Test_ValidateTokenInternalError(t *testing.T) {
 	tv := NewTokenValidator()
 	tv.ping = p
 
-	errCode, err := tv.Validate(context.TODO(), "123123")
-	assert.Equal(t, errCode, pb.RpcObjectImportNotionValidateTokenResponseError_INTERNAL_ERROR)
-	assert.NotNil(t, err)
+	_, err := tv.Validate(context.TODO(), "123123")
+	assert.Equal(t, err, pb.RpcObjectImportNotionValidateTokenResponseError_INTERNAL_ERROR)
 }
 
 func Test_ValidateTokenNotionUnavailable(t *testing.T) {
@@ -80,9 +77,8 @@ func Test_ValidateTokenNotionUnavailable(t *testing.T) {
 	tv := NewTokenValidator()
 	tv.ping = p
 
-	errCode, err := tv.Validate(context.TODO(), "123123")
-	assert.Equal(t, errCode, pb.RpcObjectImportNotionValidateTokenResponseError_SERVICE_UNAVAILABLE)
-	assert.Equal(t, nil, err)
+	_, err := tv.Validate(context.TODO(), "123123")
+	assert.Equal(t, err, pb.RpcObjectImportNotionValidateTokenResponseError_SERVICE_UNAVAILABLE)
 }
 
 func Test_ValidateTokenNotionForbidden(t *testing.T) {
@@ -99,7 +95,6 @@ func Test_ValidateTokenNotionForbidden(t *testing.T) {
 	tv := NewTokenValidator()
 	tv.ping = p
 
-	errCode, err := tv.Validate(context.TODO(), "123123")
-	assert.Equal(t, errCode, pb.RpcObjectImportNotionValidateTokenResponseError_FORBIDDEN)
-	assert.Equal(t, nil, err)
+	_, err := tv.Validate(context.TODO(), "123123")
+	assert.Equal(t, err, pb.RpcObjectImportNotionValidateTokenResponseError_FORBIDDEN)
 }
