0ad69bb9811bbfd9c6fdf76dd2306229fe6a9c12
Sergey Cherepanov <cheggaaa@gmail.com>
2023-04-25 18:17:59 +0200

    fix win build

--- core/debug/graphviz.go
+++ core/debug/graphviz.go
@@ -1,7 +1,9 @@
-//go:build (!linux && !darwin) || android || ios || nographviz || (!arm64 && !amd64)
-// +build !linux,!darwin android ios nographviz !arm64,!amd64
+//go:build (!linux && !darwin) || android || ios || nographviz || wwindows
+// +build !linux,!darwin android ios nographviz wwindows
 
 package debug
+
+import "fmt"
 
 func GraphvizSvg(gv, svgFilename string) (err error) {
 	return fmt.Errorf("graphviz is not supported on the current platform")
