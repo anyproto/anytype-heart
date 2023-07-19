fe23eb569aecd37d46c440030a753c195d8a0411
AnastasiaShemyakinskaya <shem98a@mail.ru>
2023-07-19 11:50:31 +0200

    GO-1136: remove not needed code
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/block/editor/state/normalize.go
+++ core/block/editor/state/normalize.go
@@ -5,7 +5,6 @@ import (
 	"github.com/globalsign/mgo/bson"
 
 	"github.com/anyproto/anytype-heart/core/block/simple"
-	"github.com/anyproto/anytype-heart/pkg/lib/bundle"
 	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
 	"github.com/anyproto/anytype-heart/util/pbtypes"
 	"github.com/anyproto/anytype-heart/util/slice"
@@ -56,9 +55,6 @@ func (s *State) normalize(withLayouts bool) (err error) {
 		if layout := b.Model().GetLayout(); layout != nil {
 			s.normalizeLayoutRow(b)
 		}
-	}
-	if pbtypes.Get(s.Details(), bundle.RelationKeyLayout.String()) == nil {
-		s.SetDetail(bundle.RelationKeyLayout.String(), pbtypes.Float64(float64(model.ObjectType_basic)))
 	}
 	if withLayouts {
 		return s.normalizeTree()
