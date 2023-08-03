bc33f8cb6ab13cc381a4c34f095234f92b7e4a94
kirillston <stonozhenko@anytype.io>
2023-08-03 18:46:50 +0200

    GO-1825 Fix for Windows

--- cmd/grpcserver/debug_not_win.go
+++ cmd/grpcserver/debug_not_win.go
@@ -1,1 +1,17 @@
+//go:build !nogrpcserver && !_test && !windows
+
 package main
+
+import (
+	"os"
+	"syscall"
+)
+
+var signals = append(commonOSSignals, syscall.SIGUSR1)
+
+func shouldSaveStack(signal os.Signal) bool {
+	if signal == syscall.SIGUSR1 {
+		return true
+	}
+	return false
+}
--- cmd/grpcserver/debug_win.go
+++ cmd/grpcserver/debug_win.go
@@ -1,1 +1,13 @@
+//go:build !nogrpcserver && !_test && windows
+
 package main
+
+import (
+	"os"
+)
+
+var signals = commonOSSignals
+
+func shouldSaveStack(signal os.Signal) bool {
+	return false
+}
