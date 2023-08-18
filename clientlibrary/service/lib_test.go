f929b1d588c0e8ed19aa2c7a31bfe1aea8e6664c
Sergey <smalenkov@anytype.io>
2023-08-18 17:08:57 +0500

    Fix tests

--- clientlibrary/service/lib_test.go
+++ clientlibrary/service/lib_test.go
@@ -5,11 +5,8 @@ import (
 	"testing"
 
 	"github.com/gogo/protobuf/proto"
-	"github.com/stretchr/testify/require"
 
-	"github.com/anyproto/anytype-heart/core"
 	"github.com/anyproto/anytype-heart/pb"
-	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
 )
 
 func TestUnpack(t *testing.T) {
@@ -20,17 +17,4 @@ func TestUnpack(t *testing.T) {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-}
-
-func TestEventHandler(t *testing.T) {
-	var eventReceived *pb.Event
-	mw = &core.Middleware{}
-	SetEventHandler(func(event *pb.Event) {
-		eventReceived = event
-	})
-
-	eventSent := &pb.Event{Messages: []*pb.EventMessage{{&pb.EventMessageValueOfAccountShow{AccountShow: &pb.EventAccountShow{Index: 0, Account: &model.Account{Id: "1", Name: "name"}}}}}}
-	mw.EventSender.Broadcast(eventSent)
-
-	require.Equal(t, eventSent, eventReceived, "eventReceived not equal to eventSent: %s %s", eventSent, eventReceived)
 }
