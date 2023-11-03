cf9af589b49247790ecebdd5b02eb973c5e6853c
kirillston <stonozhenko@anytype.io>
2023-11-03 14:19:16 +0100

    GO-2143 Add schema validation

--- go.mod
+++ go.mod
@@ -234,6 +234,9 @@ require (
 	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
 	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
 	github.com/whyrusleeping/chunker v0.0.0-20181014151217-fe64bd25879f // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	github.com/zeebo/blake3 v0.2.3 // indirect
 	github.com/zeebo/errs v1.3.0 // indirect
--- util/gallery/gallery.go
+++ util/gallery/gallery.go
@@ -2,27 +2,32 @@ package gallery
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"time"
 
+	"github.com/xeipuuv/gojsonschema"
+
 	"github.com/anyproto/anytype-heart/pb"
 )
 
+const timeout = time.Second * 2
+
+type schemaHandler struct {
+	Schema string `json:"$schema"`
+}
+
 func DownloadManifest(url string) (info *pb.RpcGalleryDownloadManifestResponseManifestInfo, err error) {
-	spaceClient := http.Client{
-		Timeout: time.Second * 2, // Timeout after 2 seconds
-	}
+	client := http.Client{Timeout: timeout}
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return
 	}
 
-	req.Header.Set("User-Agent", "spacecount-tutorial")
-
-	res, getErr := spaceClient.Do(req)
-	if getErr != nil {
+	res, err := client.Do(req)
+	if err != nil {
 		return
 	}
 
@@ -30,14 +35,35 @@ func DownloadManifest(url string) (info *pb.RpcGalleryDownloadManifestResponseMa
 		defer res.Body.Close()
 	}
 
-	body, readErr := io.ReadAll(res.Body)
-	if readErr != nil {
-		return nil, readErr
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
 	}
 
-	jsonErr := json.Unmarshal(body, &info)
-	if jsonErr != nil {
-		return nil, jsonErr
+	schemaWrapper := schemaHandler{}
+	err = json.Unmarshal(body, &schemaWrapper)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshall json to get schema: %w", err)
 	}
+
+	err = json.Unmarshal(body, &info)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshall json to get manifest: %w", err)
+	}
+
+	if schemaWrapper.Schema != "" {
+		var result *gojsonschema.Result
+		schemaLoader := gojsonschema.NewReferenceLoader(schemaWrapper.Schema)
+		jsonLoader := gojsonschema.NewGoLoader(info)
+		result, err = gojsonschema.Validate(schemaLoader, jsonLoader)
+		if err != nil {
+			return nil, err
+		}
+		if !result.Valid() {
+			return nil, fmt.Errorf("manifest does not correspond provided schema")
+		}
+		info.Schema = schemaWrapper.Schema
+	}
+
 	return info, nil
 }
