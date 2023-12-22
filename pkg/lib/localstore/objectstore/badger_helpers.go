8d2ddf597c018c9e3f67c7dab5cee4b85441f289
Roman Khafizianov <requilence@gmail.com>
2023-12-22 11:44:15 +0100

    fix iterateKeysByPrefixBatchedTx
    copy key

--- pkg/lib/localstore/objectstore/badger_helpers.go
+++ pkg/lib/localstore/objectstore/badger_helpers.go
@@ -45,7 +45,7 @@ func iterateKeysByPrefixBatchedTx(
 	count := 0
 
 	for iter.Rewind(); iter.Valid(); iter.Next() {
-		key := iter.Item().Key()
+		key := iter.Item().KeyCopy(nil)
 		batch = append(batch, key)
 		count++
 
