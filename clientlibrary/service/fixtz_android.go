9de7abe3bda18a4eb86bd5642becf8ec04257ff8
Roman Khafizianov <requilence@gmail.com>
2024-01-08 16:20:21 +0100

    fix tz

--- clientlibrary/service/fixtz_android.go
+++ clientlibrary/service/fixtz_android.go
@@ -1,5 +1,11 @@
 package service
 
+import (
+	"os/exec"
+	"strings"
+	"time"
+)
+
 func fixTZ() {
 	out, err := exec.Command("/system/bin/getprop", "persist.sys.timezone").Output()
 	if err != nil {
