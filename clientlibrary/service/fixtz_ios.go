e78fbe0eb1cb7556bee54a2ee0da635dcbb9d307
Roman Khafizianov <requilence@gmail.com>
2024-01-08 17:32:59 +0100

    fix ios tz code

--- clientlibrary/service/fixtz_ios.go
+++ clientlibrary/service/fixtz_ios.go
@@ -1,14 +1,31 @@
+//go:build cgo && ios
+
 package service
+
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework Foundation
+#import <Foundation/Foundation.h>
+
+const char* getSystemTimeZone() {
+    NSTimeZone *timeZone = [NSTimeZone systemTimeZone];
+    NSString *timeZoneName = [timeZone description];
+    return [timeZoneName UTF8String];
+}
+*/
+import "C"
 
 import (
 	"strings"
 	"time"
-
-	"ObjC/Foundation/NSTimeZone"
 )
 
+func getSystemTimeZone() string {
+	tz := C.getSystemTimeZone()
+	return C.GoString(tz)
+}
+
 func fixTZ() {
-	NSTimeZone.SystemTimeZone()
-	z, _ := time.LoadLocation(strings.Split(NSTimeZone.SystemTimeZone().Description(), " ")[0])
+	z, _ := time.LoadLocation(strings.Split(getSystemTimeZone(), " ")[0])
 	time.Local = z
 }
