5dae36587347d9ec17a7e685e4e5b74f6dfad75e
Sergey <smalenkov@anytype.io>
2024-01-19 11:26:12 +0100

    GO-2691: Fix observing own identity

--- space/internal/components/dependencies/identityservice.go
+++ space/internal/components/dependencies/identityservice.go
@@ -2,11 +2,14 @@ package dependencies
 
 import (
 	"github.com/anyproto/any-sync/util/crypto"
+	"github.com/gogo/protobuf/types"
 
 	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
 )
 
 type IdentityService interface {
+	GetMyProfileDetails() (identity string, metadataKey crypto.SymKey, details *types.Struct)
+
 	RegisterIdentity(spaceId string, identity string, encryptionKey crypto.SymKey, observer func(identity string, profile *model.IdentityProfile)) error
 
 	// UnregisterIdentity removes the observer for the identity in specified space
