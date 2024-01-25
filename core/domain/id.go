c50ea398b5a6102d3be1d39dacd4ef86365712ea
mcrakhman <rakhmanov.m@gmail.com>
2024-01-25 22:47:18 +0100

    GO-2489 Fix id

--- core/domain/id.go
+++ core/domain/id.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"fmt"
+	"strings"
 )
 
 type FullID struct {
@@ -14,5 +15,6 @@ type ObjectTypeKey string
 const ParticipantPrefix = "_participant_"
 
 func NewParticipantId(spaceId, identity string) string {
+	spaceId = strings.Replace(spaceId, ".", "_", 1)
 	return fmt.Sprintf("%s%s_%s", ParticipantPrefix, spaceId, identity)
 }
