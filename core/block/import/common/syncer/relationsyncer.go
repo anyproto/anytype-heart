8e6987ae07cc5665f62b4a48527d320c43b7462c
Sergey <smalenkov@anytype.io>
2024-02-01 15:49:12 +0100

    GO-1420: Fix some linter issues

--- core/block/import/common/syncer/relationsyncer.go
+++ core/block/import/common/syncer/relationsyncer.go
@@ -33,6 +33,11 @@ func NewFileRelationSyncer(service *block.Service, fileStore filestore.FileStore
 }
 
 func (fs *FileRelationSyncer) Sync(spaceID string, fileId string, snapshotPayloads map[string]treestorage.TreeStorageCreatePayload, origin objectorigin.ObjectOrigin) string {
+	// If file is created during import, do nothing
+	if _, ok := snapshotPayloads[fileId]; ok {
+		return fileId
+	}
+
 	var (
 		fileObjectId string
 		err          error
@@ -46,27 +51,25 @@ func (fs *FileRelationSyncer) Sync(spaceID string, fileId string, snapshotPayloa
 		if err != nil {
 			log.Errorf("file uploading %s", err)
 		}
-	} else {
-		if _, ok := snapshotPayloads[fileId]; ok {
+		return fileObjectId
+	}
+
+	_, err = cid.Decode(fileId)
+	if err == nil {
+		fileObjectId, err = fs.fileObjectService.CreateFromImport(domain.FullFileId{SpaceId: spaceID, FileId: domain.FileId(fileId)}, origin)
+		if err != nil {
+			log.With("fileId", fileId).Errorf("create file object: %v", err)
 			return fileId
 		}
-		_, err = cid.Decode(fileId)
-		if err == nil {
-			fileObjectId, err = fs.fileObjectService.CreateFromImport(domain.FullFileId{SpaceId: spaceID, FileId: domain.FileId(fileId)}, origin)
-			if err != nil {
-				log.With("fileId", fileId).Errorf("create file object: %v", err)
-				return fileId
-			}
-			return fileObjectId
-		}
-		req := block.FileUploadRequest{
-			RpcFileUploadRequest: pb.RpcFileUploadRequest{LocalPath: fileId},
-			ObjectOrigin:         origin,
-		}
-		fileObjectId, _, err = fs.service.UploadFile(context.Background(), spaceID, req)
-		if err != nil {
-			log.Errorf("file uploading %s", err)
-		}
+		return fileObjectId
+	}
+	req := block.FileUploadRequest{
+		RpcFileUploadRequest: pb.RpcFileUploadRequest{LocalPath: fileId},
+		ObjectOrigin:         origin,
+	}
+	fileObjectId, _, err = fs.service.UploadFile(context.Background(), spaceID, req)
+	if err != nil {
+		log.Errorf("file uploading %s", err)
 	}
 	return fileObjectId
 }
