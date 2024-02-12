2d9ea20e68894d2ead46686d14d28e984c6b7682
Sergey <smalenkov@anytype.io>
2024-02-12 09:47:19 +0100

    GO-2489: Address code review

--- core/indexer/indexer.go
+++ core/indexer/indexer.go
@@ -123,9 +123,9 @@ func (i *indexer) RemoveAclIndexes(spaceId string) (err error) {
 				Value:       pbtypes.String(spaceId),
 			},
 			{
-				RelationKey: bundle.RelationKeyType.String(),
+				RelationKey: bundle.RelationKeyLayout.String(),
 				Condition:   model.BlockContentDataviewFilter_Equal,
-				Value:       pbtypes.String(bundle.TypeKeyParticipant.String()),
+				Value:       pbtypes.Int64(int64(model.ObjectType_participant)),
 			},
 		},
 	})
