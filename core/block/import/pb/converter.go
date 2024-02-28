5c730dc5b49b7371e0bb15d8e33ab64b3e5f0599
AnastasiaShemyakinskaya <shem98a@mail.ru>
2024-02-28 19:36:15 +0100

    GO-2788: update source path in notion and pb import
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/block/import/pb/converter.go
+++ core/block/import/pb/converter.go
@@ -450,8 +450,8 @@ func (p *Pb) injectImportDetails(sn *pb.SnapshotWithType) {
 
 func (p *Pb) setSourceFilePath(sn *pb.SnapshotWithType) {
 	spaceId := pbtypes.GetString(sn.Snapshot.Data.Details, bundle.RelationKeySpaceId.String())
-	pageId := pbtypes.GetString(sn.Snapshot.Data.Details, bundle.RelationKeyId.String())
-	sourceFilePath := filepath.Join(spaceId, pageId, string(filepath.Separator))
+	id := pbtypes.GetString(sn.Snapshot.Data.Details, bundle.RelationKeyId.String())
+	sourceFilePath := filepath.Join(spaceId, "/", id)
 	sn.Snapshot.Data.Details.Fields[bundle.RelationKeySourceFilePath.String()] = pbtypes.String(sourceFilePath)
 }
 
