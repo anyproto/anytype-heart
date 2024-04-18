fbd75ba1fc903aa47c9923170057c86e8d67a593
AnastasiaShemyakinskaya <shem98a@mail.ru>
2024-04-18 12:56:45 +0200

    GO-3027: add tests
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/indexer/reindex.go
+++ core/indexer/reindex.go
@@ -33,7 +33,7 @@ const (
 	ForceFilesReindexCounter int32 = 12 //
 
 	// ForceBundledObjectsReindexCounter reindex objects like anytypeProfile
-	ForceBundledObjectsReindexCounter int32 = 5 // reindex objects like anytypeProfile
+	ForceBundledObjectsReindexCounter int32 = 6 // reindex objects like anytypeProfile
 
 	// ForceIdxRebuildCounter erases localstore indexes and reindex all type of objects
 	// (no need to increase ForceObjectsReindexCounter & ForceFilesReindexCounter)
@@ -65,7 +65,6 @@ func (i *indexer) buildFlags(spaceID string) (reindexFlags, error) {
 				// per space
 				FilestoreKeysForceReindexCounter: ForceFilestoreKeysReindexCounter,
 				// global
-				BundledObjects:     ForceBundledObjectsReindexCounter,
 				AreOldFilesRemoved: true,
 			}
 		}
@@ -253,7 +252,7 @@ func (i *indexer) ReindexMarketplaceSpace(space clientspace.Space) error {
 	}
 	if flags.bundledObjects {
 		// hardcoded for now
-		ids := []string{addr.AnytypeProfileId}
+		ids := []string{addr.AnytypeProfileId, addr.MissingObject}
 		err := i.reindexIDs(ctx, space, metrics.ReindexTypeBundledObjects, ids)
 		if err != nil {
 			return fmt.Errorf("reindex profile and missing object: %w", err)
@@ -285,12 +284,6 @@ func (i *indexer) ReindexMarketplaceSpace(space clientspace.Space) error {
 			return fmt.Errorf("reindex bundled templates: %w", err)
 		}
 	}
-
-	err = i.reindexIDs(ctx, space, metrics.ReindexTypeBundledObjects, []string{addr.MissingObject})
-	if err != nil {
-		return fmt.Errorf("reindex missing object: %w", err)
-	}
-
 	return i.saveLatestChecksums(space.Id())
 }
 
---
+++ core/indexer/reindex_test.go
@@ -0,0 +1,122 @@
+package indexer
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/anyproto/anytype-heart/core/block/editor"
+	"github.com/anyproto/anytype-heart/core/block/editor/smartblock/smarttest"
+	"github.com/anyproto/anytype-heart/core/block/object/objectcache/mock_objectcache"
+	"github.com/anyproto/anytype-heart/core/indexer/mock_indexer"
+	"github.com/anyproto/anytype-heart/pkg/lib/bundle"
+	coresb "github.com/anyproto/anytype-heart/pkg/lib/core/smartblock"
+	"github.com/anyproto/anytype-heart/pkg/lib/localstore/addr"
+	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
+	"github.com/anyproto/anytype-heart/space/clientspace"
+	"github.com/anyproto/anytype-heart/space/spacecore/storage/mock_storage"
+)
+
+func TestReindexMarketplaceSpace(t *testing.T) {
+	t.Run("reindex missing object", func(t *testing.T) {
+		// given
+		indexerFx := NewIndexerFixture(t)
+		mockHasher := mock_indexer.NewMockHasher(t)
+		mockHasher.EXPECT().Hash().Return("")
+		indexerFx.btHash = mockHasher
+		checksums := &model.ObjectStoreChecksums{
+			BundledObjectTypes:               bundle.TypeChecksum,
+			BundledRelations:                 bundle.RelationChecksum,
+			BundledTemplates:                 indexerFx.btHash.Hash(),
+			ObjectsForceReindexCounter:       ForceObjectsReindexCounter,
+			FilesForceReindexCounter:         ForceFilesReindexCounter,
+			IdxRebuildCounter:                ForceIdxRebuildCounter,
+			FilestoreKeysForceReindexCounter: ForceFilestoreKeysReindexCounter,
+			AreOldFilesRemoved:               true,
+		}
+		err := indexerFx.store.SaveChecksums("spaceId", checksums)
+		assert.Nil(t, err)
+
+		virtualSpace := clientspace.NewVirtualSpace("spaceId", clientspace.VirtualSpaceDeps{
+			Indexer: indexerFx,
+		})
+		mockCache := mock_objectcache.NewMockCache(t)
+		smartTest := smarttest.New(addr.MissingObject)
+		smartTest.SetSpace(virtualSpace)
+
+		smartTest.SetType(coresb.SmartBlockTypePage)
+		smartTest.SetSpaceId("spaceId")
+		mockCache.EXPECT().GetObject(context.Background(), addr.MissingObject).Return(editor.NewMissingObject(smartTest), nil)
+		mockCache.EXPECT().GetObject(context.Background(), addr.AnytypeProfileId).Return(smartTest, nil)
+		virtualSpace.Cache = mockCache
+
+		storage := mock_storage.NewMockClientStorage(t)
+		storage.EXPECT().BindSpaceID(mock.Anything, mock.Anything).Return(nil)
+		indexerFx.storageService = storage
+
+		// when
+		err = indexerFx.ReindexMarketplaceSpace(virtualSpace)
+
+		// then
+		details, err := indexerFx.store.GetDetails(addr.MissingObject)
+		assert.Nil(t, err)
+		assert.NotNil(t, details)
+
+		checksum, err := indexerFx.store.GetChecksums("spaceId")
+		assert.Nil(t, err)
+		assert.NotNil(t, checksum)
+		assert.Equal(t, ForceBundledObjectsReindexCounter, checksum.BundledObjects)
+	})
+	t.Run("reindex missing object - old counter in checksum", func(t *testing.T) {
+		// given
+		indexerFx := NewIndexerFixture(t)
+		mockHasher := mock_indexer.NewMockHasher(t)
+		mockHasher.EXPECT().Hash().Return("")
+		indexerFx.btHash = mockHasher
+		checksums := &model.ObjectStoreChecksums{
+			BundledObjectTypes:               bundle.TypeChecksum,
+			BundledRelations:                 bundle.RelationChecksum,
+			BundledObjects:                   5,
+			BundledTemplates:                 indexerFx.btHash.Hash(),
+			ObjectsForceReindexCounter:       ForceObjectsReindexCounter,
+			FilesForceReindexCounter:         ForceFilesReindexCounter,
+			IdxRebuildCounter:                ForceIdxRebuildCounter,
+			FilestoreKeysForceReindexCounter: ForceFilestoreKeysReindexCounter,
+			AreOldFilesRemoved:               true,
+		}
+		err := indexerFx.store.SaveChecksums("spaceId", checksums)
+		assert.Nil(t, err)
+
+		virtualSpace := clientspace.NewVirtualSpace("spaceId", clientspace.VirtualSpaceDeps{
+			Indexer: indexerFx,
+		})
+		mockCache := mock_objectcache.NewMockCache(t)
+		smartTest := smarttest.New(addr.MissingObject)
+		smartTest.SetSpace(virtualSpace)
+
+		smartTest.SetType(coresb.SmartBlockTypePage)
+		smartTest.SetSpaceId("spaceId")
+		mockCache.EXPECT().GetObject(context.Background(), addr.MissingObject).Return(editor.NewMissingObject(smartTest), nil)
+		mockCache.EXPECT().GetObject(context.Background(), addr.AnytypeProfileId).Return(smartTest, nil)
+		virtualSpace.Cache = mockCache
+
+		storage := mock_storage.NewMockClientStorage(t)
+		storage.EXPECT().BindSpaceID(mock.Anything, mock.Anything).Return(nil)
+		indexerFx.storageService = storage
+
+		// when
+		err = indexerFx.ReindexMarketplaceSpace(virtualSpace)
+
+		// then
+		details, err := indexerFx.store.GetDetails(addr.MissingObject)
+		assert.Nil(t, err)
+		assert.NotNil(t, details)
+
+		checksum, err := indexerFx.store.GetChecksums("spaceId")
+		assert.Nil(t, err)
+		assert.NotNil(t, checksum)
+		assert.Equal(t, ForceBundledObjectsReindexCounter, checksum.BundledObjects)
+	})
+}
