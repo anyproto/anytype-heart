661523a2f99818029a16ac1ebab1f51624eb9ee7
AnastasiaShemyakinskaya <shem98a@mail.ru>
2024-05-23 11:10:25 +0200

    GO-3170: fix linter
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/syncstatus/syncstatus.go
+++ core/syncstatus/syncstatus.go
@@ -60,7 +60,6 @@ type treeHeadsEntry struct {
 type treeStatus struct {
 	treeId string
 	status SyncStatus
-	heads  []string
 }
 
 type syncStatusService struct {
@@ -162,7 +161,7 @@ func (s *syncStatusService) update(ctx context.Context) (err error) {
 			s.Unlock()
 			return
 		}
-		s.treeStatusBuf = append(s.treeStatusBuf, treeStatus{treeId, treeHeads.syncStatus, treeHeads.heads})
+		s.treeStatusBuf = append(s.treeStatusBuf, treeStatus{treeId, treeHeads.syncStatus})
 	}
 	nodeStatus := s.nodeStatus
 	s.Unlock()
