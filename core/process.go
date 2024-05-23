eb51e9166c464c76a6c20d21d5e4ae8cb77111b3
AnastasiaShemyakinskaya <shem98a@mail.ru>
2024-05-23 17:40:39 +0200

    GO-3453: add methods
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/process.go
+++ core/process.go
@@ -23,3 +23,25 @@ func (mw *Middleware) ProcessCancel(cctx context.Context, req *pb.RpcProcessCanc
 	}
 	return response(pb.RpcProcessCancelResponseError_NULL, nil)
 }
+
+func (mw *Middleware) ProcessSubscribe(cctx context.Context, req *pb.RpcProcessSubscribeRequest) *pb.RpcProcessSubscribeResponse {
+	response := func(code pb.RpcProcessSubscribeResponseErrorCode, err error) *pb.RpcProcessSubscribeResponse {
+		m := &pb.RpcProcessSubscribeResponse{Error: &pb.RpcProcessSubscribeResponseError{Code: code}}
+		if err != nil {
+			m.Error.Description = err.Error()
+		}
+		return m
+	}
+	return response(pb.RpcProcessSubscribeResponseError_NULL, nil)
+}
+
+func (mw *Middleware) ProcessUnsubscribe(cctx context.Context, req *pb.RpcProcessUnsubscribeRequest) *pb.RpcProcessUnsubscribeResponse {
+	response := func(code pb.RpcProcessUnsubscribeResponseErrorCode, err error) *pb.RpcProcessUnsubscribeResponse {
+		m := &pb.RpcProcessUnsubscribeResponse{Error: &pb.RpcProcessUnsubscribeResponseError{Code: code}}
+		if err != nil {
+			m.Error.Description = err.Error()
+		}
+		return m
+	}
+	return response(pb.RpcProcessUnsubscribeResponseError_NULL, nil)
+}
