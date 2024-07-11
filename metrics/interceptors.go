fc84d67ce7a90e270116bf7f2650b8a1c9acd764
Mikhail Iudin <yudin.reso@gmail.com>
2024-07-11 16:50:27 +0200

    GO-3747 Send traces for long methods

--- metrics/interceptors.go
+++ metrics/interceptors.go
@@ -104,8 +104,19 @@ func SharedLongMethodsInterceptor(ctx context.Context, req any, methodName strin
 		// todo: save long stack trace to files
 		lastTraceB := debug.CompressBytes([]byte(lastTrace.String()))
 		l.With("ver", 2).With("error", err).With("in_progress", false).With("goroutines", lastTraceB).With("total", time.Since(start).Milliseconds()).Warnf("grpc unary request took too long")
+		Service.Send(
+			&LongMethodEvent{
+				methodName: methodName,
+				middleTime: elapsed.Milliseconds(),
+				stack:      lastTrace.Load(),
+			},
+		)
 	}
 	return resp, err
+}
+
+func stackTraceHasMethod(method string, stackTrace []byte) bool {
+	return bytes.Contains(stackTrace, []byte("core.(*Middleware)."+method+"("))
 }
 
 func SendMethodEvent(method string, err error, resp any, delta int64) {
