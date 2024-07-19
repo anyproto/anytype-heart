7c7bf025edda821fd313ed13d2994e8b4d6fe629
mcrakhman <rakhmanov.m@gmail.com>
2024-07-19 10:55:53 +0200

    GO-3769 Remove sorting from sub

--- core/syncstatus/syncsubscritions/objectsubscription.go
+++ core/syncstatus/syncsubscritions/objectsubscription.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/cheggaaa/mb/v3"
 	"github.com/gogo/protobuf/types"
-	"github.com/huandu/skiplist"
 
 	"github.com/anyproto/anytype-heart/core/subscription"
 	"github.com/anyproto/anytype-heart/pb"
@@ -29,7 +28,6 @@ func newEntry[T any](id string, data T) *entry[T] {
 
 type (
 	extract[T any] func(*types.Struct) (string, T)
-	compare[T any] func(T, T) int
 	update[T any]  func(string, *types.Value, T) T
 	unset[T any]   func([]string, T) T
 )
@@ -37,7 +35,6 @@ type (
 type SubscriptionParams[T any] struct {
 	Request subscription.SubscribeRequest
 	Extract extract[T]
-	Order   compare[T]
 	Update  update[T]
 	Unset   unset[T]
 }
@@ -47,7 +44,6 @@ func NewIdSubscription(service subscription.Service, request subscription.Subscr
 		request: request,
 		service: service,
 		ch:      make(chan struct{}),
-		order:   nil,
 		extract: func(t *types.Struct) (string, struct{}) {
 			return pbtypes.GetString(t, bundle.RelationKeyId.String()), struct{}{}
 		},
@@ -65,7 +61,6 @@ func NewObjectSubscription[T any](service subscription.Service, params Subscript
 		request: params.Request,
 		service: service,
 		ch:      make(chan struct{}),
-		order:   params.Order,
 		extract: params.Extract,
 		update:  params.Update,
 		unset:   params.Unset,
@@ -79,8 +74,7 @@ type ObjectSubscription[T any] struct {
 	events  *mb.MB[*pb.EventMessage]
 	ctx     context.Context
 	cancel  context.CancelFunc
-	skl     *skiplist.SkipList
-	order   compare[T]
+	sub     map[string]*entry[T]
 	extract extract[T]
 	update  update[T]
 	unset   unset[T]
@@ -94,11 +88,10 @@ func (o *ObjectSubscription[T]) Run() error {
 	}
 	o.ctx, o.cancel = context.WithCancel(context.Background())
 	o.events = resp.Output
-	o.skl = skiplist.New(o)
+	o.sub = map[string]*entry[T]{}
 	for _, rec := range resp.Records {
 		id, data := o.extract(rec)
-		e := &entry[T]{id: id, data: data}
-		o.skl.Set(e, nil)
+		o.sub[id] = newEntry(id, data)
 	}
 	go o.read()
 	return nil
@@ -113,44 +106,18 @@ func (o *ObjectSubscription[T]) Close() {
 func (o *ObjectSubscription[T]) Len() int {
 	o.mx.Lock()
 	defer o.mx.Unlock()
-	return o.skl.Len()
+	return len(o.sub)
 }
 
 func (o *ObjectSubscription[T]) Iterate(iter func(id string, data T) bool) {
 	o.mx.Lock()
 	defer o.mx.Unlock()
-	cur := o.skl.Front()
-	for cur != nil {
-		el := cur.Key().(*entry[T])
-		if !iter(el.id, el.data) {
+	for id, ent := range o.sub {
+		if !iter(id, ent.data) {
 			return
 		}
-		cur = cur.Next()
 	}
 	return
-}
-
-func (o *ObjectSubscription[T]) Compare(lhs, rhs interface{}) (comp int) {
-	le := lhs.(*entry[T])
-	re := rhs.(*entry[T])
-	if le.id == re.id {
-		return 0
-	}
-	if o.order != nil {
-		comp = o.order(le.data, re.data)
-	}
-	if comp == 0 {
-		if le.id > re.id {
-			return 1
-		} else {
-			return -1
-		}
-	}
-	return comp
-}
-
-func (o *ObjectSubscription[T]) CalcScore(key interface{}) float64 {
-	return 0
 }
 
 func (o *ObjectSubscription[T]) read() {
@@ -160,32 +127,29 @@ func (o *ObjectSubscription[T]) read() {
 		defer o.mx.Unlock()
 		switch v := event.Value.(type) {
 		case *pb.EventMessageValueOfSubscriptionAdd:
-			o.skl.Set(newEmptyEntry[T](v.SubscriptionAdd.Id), nil)
+			o.sub[v.SubscriptionAdd.Id] = newEmptyEntry[T](v.SubscriptionAdd.Id)
 		case *pb.EventMessageValueOfSubscriptionRemove:
-			o.skl.Remove(newEmptyEntry[T](v.SubscriptionRemove.Id))
+			delete(o.sub, v.SubscriptionRemove.Id)
 		case *pb.EventMessageValueOfObjectDetailsAmend:
-			curEntry := o.skl.Get(newEmptyEntry[T](v.ObjectDetailsAmend.Id))
+			curEntry := o.sub[v.ObjectDetailsAmend.Id]
 			if curEntry == nil {
 				return
 			}
-			e := curEntry.Key().(*entry[T])
 			for _, value := range v.ObjectDetailsAmend.Details {
-				e.data = o.update(value.Key, value.Value, e.data)
+				curEntry.data = o.update(value.Key, value.Value, curEntry.data)
 			}
 		case *pb.EventMessageValueOfObjectDetailsUnset:
-			curEntry := o.skl.Get(newEmptyEntry[T](v.ObjectDetailsUnset.Id))
+			curEntry := o.sub[v.ObjectDetailsUnset.Id]
 			if curEntry == nil {
 				return
 			}
-			e := curEntry.Key().(*entry[T])
-			e.data = o.unset(v.ObjectDetailsUnset.Keys, e.data)
+			curEntry.data = o.unset(v.ObjectDetailsUnset.Keys, curEntry.data)
 		case *pb.EventMessageValueOfObjectDetailsSet:
-			curEntry := o.skl.Get(newEmptyEntry[T](v.ObjectDetailsSet.Id))
+			curEntry := o.sub[v.ObjectDetailsSet.Id]
 			if curEntry == nil {
 				return
 			}
-			e := curEntry.Key().(*entry[T])
-			_, e.data = o.extract(v.ObjectDetailsSet.Details)
+			_, curEntry.data = o.extract(v.ObjectDetailsSet.Details)
 		}
 	}
 	for {
