ef08953b97dd01ed930000be9f69f1d239500433
mcrakhman <rakhmanov.m@gmail.com>
2024-07-31 02:00:46 +0200

    GO-3844 Fix and simplify subscriptions

--- core/syncstatus/syncsubscriptions/syncingobjects.go
+++ core/syncstatus/syncsubscriptions/syncingobjects.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/anyproto/anytype-heart/core/domain"
 	"github.com/anyproto/anytype-heart/core/subscription"
-	"github.com/anyproto/anytype-heart/core/syncstatus/filesyncstatus"
 	"github.com/anyproto/anytype-heart/pkg/lib/bundle"
 	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
 	"github.com/anyproto/anytype-heart/util/pbtypes"
@@ -13,7 +12,6 @@ import (
 )
 
 type syncingObjects struct {
-	fileSubscription   *ObjectSubscription[struct{}]
 	objectSubscription *ObjectSubscription[struct{}]
 	service            subscription.Service
 	spaceId            string
@@ -35,36 +33,8 @@ func (s *syncingObjects) Run() error {
 		Filters: []*model.BlockContentDataviewFilter{
 			{
 				RelationKey: bundle.RelationKeySyncStatus.String(),
-				Condition:   model.BlockContentDataviewFilter_Equal,
-				Value:       pbtypes.Int64(int64(domain.SpaceSyncStatusSyncing)),
-			},
-			{
-				RelationKey: bundle.RelationKeyLayout.String(),
-				Condition:   model.BlockContentDataviewFilter_NotIn,
-				Value: pbtypes.IntList(
-					int(model.ObjectType_file),
-					int(model.ObjectType_image),
-					int(model.ObjectType_video),
-					int(model.ObjectType_audio),
-				),
-			},
-			{
-				RelationKey: bundle.RelationKeySpaceId.String(),
-				Condition:   model.BlockContentDataviewFilter_Equal,
-				Value:       pbtypes.String(s.spaceId),
-			},
-		},
-	}
-	fileReq := subscription.SubscribeRequest{
-		SubId:             fmt.Sprintf("spacestatus.files.%s", s.spaceId),
-		Internal:          true,
-		NoDepSubscription: true,
-		Keys:              []string{bundle.RelationKeyId.String()},
-		Filters: []*model.BlockContentDataviewFilter{
-			{
-				RelationKey: bundle.RelationKeyFileBackupStatus.String(),
 				Condition:   model.BlockContentDataviewFilter_In,
-				Value:       pbtypes.IntList(int(filesyncstatus.Syncing), int(filesyncstatus.Queued)),
+				Value:       pbtypes.IntList(int(domain.SpaceSyncStatusSyncing), int(domain.ObjectSyncStatusQueued), int(domain.ObjectSyncStatusError)),
 			},
 			{
 				RelationKey: bundle.RelationKeySpaceId.String(),
@@ -73,23 +43,16 @@ func (s *syncingObjects) Run() error {
 			},
 		},
 	}
-	s.fileSubscription = NewIdSubscription(s.service, fileReq)
 	s.objectSubscription = NewIdSubscription(s.service, objectReq)
-	errFiles := s.fileSubscription.Run()
 	errObjects := s.objectSubscription.Run()
-	if errFiles != nil || errObjects != nil {
-		return fmt.Errorf("error running syncing objects: %w %w", errFiles, errObjects)
+	if errObjects != nil {
+		return fmt.Errorf("error running syncing objects: %w", errObjects)
 	}
 	return nil
 }
 
 func (s *syncingObjects) Close() {
-	s.fileSubscription.Close()
 	s.objectSubscription.Close()
-}
-
-func (s *syncingObjects) GetFileSubscription() *ObjectSubscription[struct{}] {
-	return s.fileSubscription
 }
 
 func (s *syncingObjects) GetObjectSubscription() *ObjectSubscription[struct{}] {
@@ -104,8 +67,4 @@ func (s *syncingObjects) SyncingObjectsCount(missing []string) int {
 	})
 	_, added := slice.DifferenceRemovedAdded(ids, missing)
 	return len(ids) + len(added)
-}
-
-func (s *syncingObjects) FileSyncingObjectsCount() int {
-	return s.fileSubscription.Len()
 }
--- core/syncstatus/syncsubscriptions/syncsubscriptions_test.go
+++ core/syncstatus/syncsubscriptions/syncsubscriptions_test.go
@@ -11,37 +11,11 @@ import (
 
 	"github.com/anyproto/anytype-heart/core/domain"
 	"github.com/anyproto/anytype-heart/core/subscription"
-	"github.com/anyproto/anytype-heart/core/syncstatus/filesyncstatus"
 	"github.com/anyproto/anytype-heart/pkg/lib/bundle"
 	"github.com/anyproto/anytype-heart/pkg/lib/localstore/objectstore"
 	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
 	"github.com/anyproto/anytype-heart/util/pbtypes"
 )
-
-func mapFileStatus(status filesyncstatus.Status) domain.ObjectSyncStatus {
-	switch status {
-	case filesyncstatus.Syncing:
-		return domain.ObjectSyncStatusSyncing
-	case filesyncstatus.Queued:
-		return domain.ObjectSyncStatusSyncing
-	case filesyncstatus.Limited:
-		return domain.ObjectSyncStatusError
-	default:
-		return domain.ObjectSyncStatusSynced
-	}
-}
-
-func genFileObject(fileStatus filesyncstatus.Status, spaceId string) objectstore.TestObject {
-	id := fmt.Sprintf("%d", rand.Int())
-	return objectstore.TestObject{
-		bundle.RelationKeyId:               pbtypes.String(id),
-		bundle.RelationKeySyncStatus:       pbtypes.Int64(int64(mapFileStatus(fileStatus))),
-		bundle.RelationKeyFileBackupStatus: pbtypes.Int64(int64(fileStatus)),
-		bundle.RelationKeyLayout:           pbtypes.Int64(int64(model.ObjectType_file)),
-		bundle.RelationKeyName:             pbtypes.String("name" + id),
-		bundle.RelationKeySpaceId:          pbtypes.String(spaceId),
-	}
-}
 
 func genObject(syncStatus domain.ObjectSyncStatus, spaceId string) objectstore.TestObject {
 	id := fmt.Sprintf("%d", rand.Int())
@@ -57,7 +31,6 @@ func genObject(syncStatus domain.ObjectSyncStatus, spaceId string) objectstore.T
 func TestSyncSubscriptions(t *testing.T) {
 	testSubs := subscription.NewInternalTestService(t)
 	var objects []objectstore.TestObject
-	fileObjs := map[string]struct{}{}
 	objs := map[string]struct{}{}
 	for i := 0; i < 10; i++ {
 		obj := genObject(domain.ObjectSyncStatusSyncing, "spaceId")
@@ -66,19 +39,6 @@ func TestSyncSubscriptions(t *testing.T) {
 	}
 	for i := 0; i < 10; i++ {
 		objects = append(objects, genObject(domain.ObjectSyncStatusSynced, "spaceId"))
-	}
-	for i := 0; i < 10; i++ {
-		obj := genFileObject(filesyncstatus.Syncing, "spaceId")
-		objects = append(objects, obj)
-		fileObjs[obj[bundle.RelationKeyId].GetStringValue()] = struct{}{}
-	}
-	for i := 0; i < 10; i++ {
-		obj := genFileObject(filesyncstatus.Queued, "spaceId")
-		objects = append(objects, obj)
-		fileObjs[obj[bundle.RelationKeyId].GetStringValue()] = struct{}{}
-	}
-	for i := 0; i < 10; i++ {
-		objects = append(objects, genFileObject(filesyncstatus.Synced, "spaceId"))
 	}
 	testSubs.AddObjects(t, objects)
 	subs := New()
@@ -89,20 +49,12 @@ func TestSyncSubscriptions(t *testing.T) {
 	spaceSub, err := subs.GetSubscription("spaceId")
 	require.NoError(t, err)
 	syncCnt := spaceSub.SyncingObjectsCount([]string{"1", "2"})
-	fileCnt := spaceSub.FileSyncingObjectsCount()
 	require.Equal(t, 12, syncCnt)
-	require.Equal(t, 20, fileCnt)
-	require.Len(t, fileObjs, 20)
 	require.Len(t, objs, 10)
-	spaceSub.GetFileSubscription().Iterate(func(id string, data struct{}) bool {
-		delete(fileObjs, id)
-		return true
-	})
 	spaceSub.GetObjectSubscription().Iterate(func(id string, data struct{}) bool {
 		delete(objs, id)
 		return true
 	})
-	require.Empty(t, fileObjs)
 	require.Empty(t, objs)
 	for i := 0; i < 10; i++ {
 		objects[i][bundle.RelationKeySyncStatus] = pbtypes.Int64(int64(domain.ObjectSyncStatusSynced))
