2adb3b053ad8374325fb3f70c6935c0607575e03
Sergey <smalenkov@anytype.io>
2024-08-02 13:48:27 +0200

    GO-3854: Humble step

--- core/block/editor/storeobject/object.go
+++ core/block/editor/storeobject/object.go
@@ -4,5 +4,12 @@ import "github.com/anyproto/anytype-heart/core/block/editor/smartblock"
 
 type StoreObject interface {
 	smartblock.SmartBlock
-	NewPState()
+}
+
+type storeObject struct {
+	smartblock.SmartBlock
+}
+
+func New(sb smartblock.SmartBlock) StoreObject {
+	return &storeObject{SmartBlock: sb}
 }
