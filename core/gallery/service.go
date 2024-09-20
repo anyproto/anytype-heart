32fafce1898ce0a9616778e8a9c0f269de122115
kirillston <stonozhenko@anytype.io>
2024-09-20 17:42:30 +0200

    GO-3946 Add builtins back

--- core/gallery/service.go
+++ core/gallery/service.go
@@ -3,6 +3,7 @@ package gallery
 import (
 	"archive/zip"
 	"context"
+	_ "embed"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -33,6 +34,35 @@ import (
 	"github.com/anyproto/anytype-heart/util/anyerror"
 	"github.com/anyproto/anytype-heart/util/uri"
 )
+
+// TODO: Remove all embeds when clients support cache
+//
+//go:embed builtin/get_started.zip
+var getStartedZip []byte
+
+//go:embed builtin/personal_projects.zip
+var personalProjectsZip []byte
+
+//go:embed builtin/knowledge_base.zip
+var knowledgeBaseZip []byte
+
+//go:embed builtin/notes_diary.zip
+var notesDiaryZip []byte
+
+//go:embed builtin/strategic_writing.zip
+var strategicWritingZip []byte
+
+//go:embed builtin/empty.zip
+var emptyZip []byte
+
+var archives = map[pb.RpcObjectImportUseCaseRequestUseCase][]byte{
+	pb.RpcObjectImportUseCaseRequest_GET_STARTED:       getStartedZip,
+	pb.RpcObjectImportUseCaseRequest_PERSONAL_PROJECTS: personalProjectsZip,
+	pb.RpcObjectImportUseCaseRequest_KNOWLEDGE_BASE:    knowledgeBaseZip,
+	pb.RpcObjectImportUseCaseRequest_NOTES_DIARY:       notesDiaryZip,
+	pb.RpcObjectImportUseCaseRequest_STRATEGIC_WRITING: strategicWritingZip,
+	pb.RpcObjectImportUseCaseRequest_EMPTY:             emptyZip,
+}
 
 const (
 	CName                          = "gallery-service"
@@ -117,8 +147,10 @@ func (s *service) ImportBuiltInUseCase(
 	}
 
 	if cachePath == "" {
-		return pb.RpcObjectImportUseCaseResponseError_BAD_INPUT,
-			fmt.Errorf("failed to import built-in usecase: no path to client cache provided")
+		// TODO: Remove this call when clients support cache
+		return s.importUseCase(ctx, spaceID, title, useCase)
+		// return pb.RpcObjectImportUseCaseResponseError_BAD_INPUT,
+		// 	fmt.Errorf("failed to import built-in usecase: no path to client cache provided")
 	}
 
 	if err = s.ImportExperience(ctx, spaceID, "", title, cachePath, true); err != nil {
@@ -155,6 +187,30 @@ func (s *service) ImportExperience(ctx context.Context, spaceID, url, title, cac
 
 	removeFunc(path)
 	return importErr
+}
+
+// TODO: Remove this method when clients support cache
+func (s *service) importUseCase(
+	ctx context.Context, spaceID, title string, useCase pb.RpcObjectImportUseCaseRequestUseCase,
+) (code pb.RpcObjectImportUseCaseResponseErrorCode, err error) {
+	archive, found := archives[useCase]
+	if !found {
+		return pb.RpcObjectImportUseCaseResponseError_BAD_INPUT,
+			fmt.Errorf("failed to import built-in usecase: invalid Use Case value: %v", useCase)
+	}
+
+	path, remove, err := s.saveArchiveToTempFile(archive)
+	if err != nil {
+		return pb.RpcObjectImportUseCaseResponseError_UNKNOWN_ERROR, fmt.Errorf("failed to save built-in usecase in temp file: %w", err)
+	}
+
+	err = s.importArchive(ctx, spaceID, path, title, pb.RpcObjectImportRequestPbParams_EXPERIENCE, nil, true)
+	remove(path)
+
+	if err != nil {
+		return pb.RpcObjectImportUseCaseResponseError_UNKNOWN_ERROR, err
+	}
+	return pb.RpcObjectImportUseCaseResponseError_NULL, nil
 }
 
 func (s *service) getPathAndRemoveFunc(
