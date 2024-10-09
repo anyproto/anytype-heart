93b6d9c1caaa049e3e4ca4b22f6c33df52e8624e
kirillston <stonozhenko@anytype.io>
2024-10-09 16:23:54 +0200

    GO-3941 Filter flow links by spaceId

--- core/navigation.go
+++ core/navigation.go
@@ -8,10 +8,12 @@ import (
 
 	"github.com/anyproto/anytype-heart/core/block/object/idresolver"
 	"github.com/anyproto/anytype-heart/pb"
+	"github.com/anyproto/anytype-heart/pkg/lib/bundle"
 	"github.com/anyproto/anytype-heart/pkg/lib/core/smartblock"
-	"github.com/anyproto/anytype-heart/pkg/lib/localstore/objectstore"
+	"github.com/anyproto/anytype-heart/pkg/lib/localstore/objectstore/spaceindex"
 	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
 	"github.com/anyproto/anytype-heart/space/spacecore/typeprovider"
+	"github.com/anyproto/anytype-heart/util/pbtypes"
 )
 
 func (mw *Middleware) NavigationListObjects(cctx context.Context, req *pb.RpcNavigationListObjectsRequest) *pb.RpcNavigationListObjectsResponse {
@@ -25,7 +27,7 @@ func (mw *Middleware) NavigationListObjects(cctx context.Context, req *pb.RpcNav
 	return response(pb.RpcNavigationListObjectsResponseError_UNKNOWN_ERROR, nil, fmt.Errorf("not implemented"))
 }
 
-func (mw *Middleware) NavigationGetObjectInfoWithLinks(cctx context.Context, req *pb.RpcNavigationGetObjectInfoWithLinksRequest) *pb.RpcNavigationGetObjectInfoWithLinksResponse {
+func (mw *Middleware) NavigationGetObjectInfoWithLinks(_ context.Context, req *pb.RpcNavigationGetObjectInfoWithLinksRequest) *pb.RpcNavigationGetObjectInfoWithLinksResponse {
 	response := func(code pb.RpcNavigationGetObjectInfoWithLinksResponseErrorCode, object *model.ObjectInfoWithLinks, err error) *pb.RpcNavigationGetObjectInfoWithLinksResponse {
 		m := &pb.RpcNavigationGetObjectInfoWithLinksResponse{Error: &pb.RpcNavigationGetObjectInfoWithLinksResponseError{Code: code}, Object: object}
 		if err != nil {
@@ -40,25 +42,29 @@ func (mw *Middleware) NavigationGetObjectInfoWithLinks(cctx context.Context, req
 	}
 
 	resolver := getService[idresolver.Resolver](mw)
-	store := app.MustComponent[objectstore.ObjectStore](mw.applicationService.GetApp())
-	spaceID, err := resolver.ResolveSpaceID(req.ObjectId)
+	store := app.MustComponent[spaceindex.Store](mw.applicationService.GetApp())
+	spaceId, err := resolver.ResolveSpaceID(req.ObjectId)
 	if err != nil {
-		return response(pb.RpcNavigationGetObjectInfoWithLinksResponseError_UNKNOWN_ERROR, nil, fmt.Errorf("resolve spaceID: %w", err))
+		return response(pb.RpcNavigationGetObjectInfoWithLinksResponseError_UNKNOWN_ERROR, nil, fmt.Errorf("resolve spaceId: %w", err))
 	}
-	page, err := store.SpaceIndex(spaceID).GetWithLinksInfoById(req.ObjectId)
+	page, err := store.GetWithLinksInfoById(req.ObjectId)
 	if err != nil {
 		return response(pb.RpcNavigationGetObjectInfoWithLinksResponseError_UNKNOWN_ERROR, nil, err)
 	}
 
 	sbTypeProvider := getService[typeprovider.SmartBlockTypeProvider](mw)
-	filter := func(Objects []*model.ObjectInfo) []*model.ObjectInfo {
+	filter := func(objects []*model.ObjectInfo) []*model.ObjectInfo {
 		var filtered []*model.ObjectInfo
-		for _, obj := range Objects {
-			sbType, err := sbTypeProvider.Type(spaceID, obj.Id)
+		for _, obj := range objects {
+			sbType, err := sbTypeProvider.Type(spaceId, obj.Id)
 			if err != nil {
 				log.Error("get smartblock type: %v", err)
 			}
 			if sbType == smartblock.SmartBlockTypeArchive || sbType == smartblock.SmartBlockTypeFileObject || sbType == smartblock.SmartBlockTypeWidget {
+				continue
+			}
+
+			if pbtypes.GetString(obj.Details, bundle.RelationKeySpaceId.String()) != spaceId {
 				continue
 			}
 
