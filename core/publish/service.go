b5105a60a9e40fcce775c9f9ed20772919df115c
Anatolii Smolianinov <zarkonesmall@gmail.com>
2024-10-14 14:05:28 +0200

    GO-4179 remove publishById

--- core/publish/service.go
+++ core/publish/service.go
@@ -7,7 +7,6 @@ import (
 	"github.com/anyproto/any-sync/app"
 	"github.com/anyproto/any-sync/commonfile/fileservice"
 	"github.com/anyproto/any-sync/util/crypto"
-	"github.com/gogo/protobuf/proto"
 
 	"github.com/anyproto/anytype-heart/core/block"
 	"github.com/anyproto/anytype-heart/core/domain"
@@ -58,53 +57,6 @@ func (s *service) Close(_ context.Context) error {
 
 func (s *service) Name() (name string) {
 	return CName
-}
-
-func (s *service) publishById(ctx context.Context, spaceId, pageObjId string) (res PublishResult, err error) {
-	id := domain.FullID{
-		SpaceID:  spaceId,
-		ObjectID: pageObjId,
-	}
-
-	obj, err := s.blockService.ShowBlock(id, true)
-	if err != nil {
-		return
-	}
-
-	key, err := crypto.NewRandomAES()
-	if err != nil {
-		return
-	}
-
-	rawObj, err := proto.Marshal(obj)
-	if err != nil {
-		return
-	}
-	data, err := key.Encrypt(rawObj)
-	if err != nil {
-		return
-	}
-
-	rd := bytes.NewReader(data)
-	node, err := s.commonFile.AddFile(ctx, rd)
-	if err != nil {
-		return
-	}
-
-	cidStr := node.Cid().String()
-	err = s.fileSyncService.UploadSynchronously(ctx, s.techSpaceId, domain.FileId(cidStr))
-	if err != nil {
-		return
-	}
-
-	keyStr, err := encode.EncodeKeyToBase58(key)
-	if err != nil {
-		return
-	}
-
-	res.Cid = cidStr
-	res.Key = keyStr
-	return
 }
 
 func (s *service) publishData(ctx context.Context, spaceId, input string) (res PublishResult, err error) {
