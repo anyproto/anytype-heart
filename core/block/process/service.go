34fb02569c7d6087b9eb4819789dc7a2af3a49a4
AnastasiaShemyakinskaya <shem98a@mail.ru>
2024-10-29 00:26:19 +0100

    GO-4261: fix merge conflicts
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/block/process/service.go
+++ core/block/process/service.go
@@ -153,22 +153,6 @@ func (s *service) monitor(p Process) {
 				notificationSender.SendNotification()
 			}
 			return
-		case <-updated:
-			info := p.Info()
-			if !infoEquals(info, prevInfo) {
-				s.eventSender.BroadcastExceptSessions(&pb.Event{
-					Messages: []*pb.EventMessage{
-						{
-							Value: &pb.EventMessageValueOfProcessUpdate{
-								ProcessUpdate: &pb.EventProcessUpdate{
-									Process: &info,
-								},
-							},
-						},
-					},
-				}, s.getExcludedSessions())
-				prevInfo = info
-			}
 		}
 	}
 }
