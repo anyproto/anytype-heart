7f8d289bac2c95425093271d0862fab538e0683e
AnastasiaShemyakinskaya <shem98a@mail.ru>
2024-11-19 19:19:14 +0100

    GO-4246: fixed
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/block/editor/page.go
+++ core/block/editor/page.go
@@ -253,11 +253,19 @@ func (p *Page) setDefaultValueForBoolRelation() func(s *state.State) {
 	return func(s *state.State) {
 		links := s.GetRelationLinks()
 		for _, link := range links {
-			if !bundle.IsSystemRelation(domain.RelationKey(link.Key)) && link.Format == model.RelationFormat_checkbox {
-				if pbtypes.Get(s.Details(), link.Key) == nil {
-					s.SetDetail(link.Key, pbtypes.Bool(false))
-				}
+			if p.shouldAddDefaultValue(s, link) {
+				s.SetDetail(link.Key, pbtypes.Bool(false))
 			}
 		}
 	}
+}
+
+func (p *Page) shouldAddDefaultValue(s *state.State, link *model.RelationLink) bool {
+	return p.isSuitableRelation(link) && pbtypes.Get(s.Details(), link.Key) == nil
+}
+
+func (p *Page) isSuitableRelation(link *model.RelationLink) bool {
+	return (!bundle.IsSystemRelation(domain.RelationKey(link.Key)) ||
+		link.Key == bundle.RelationKeyDone.String() || link.Key == bundle.RelationKeyStarred.String()) &&
+		link.Format == model.RelationFormat_checkbox
 }
