add103eb611fe0fa250734e6f04444d16e182efe
mcrakhman <rakhmanov.m@gmail.com>
2024-12-23 15:34:00 +0100

    GO-4146 Zip folder where the data is saved

--- core/debug/treeexporter.go
+++ core/debug/treeexporter.go
@@ -6,15 +6,17 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"io/ioutil"
 	stdlog "log"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
-	"github.com/anyproto/any-sync/commonspace/object/tree/exporter"
+	anystore "github.com/anyproto/any-store"
 	"github.com/anyproto/any-sync/commonspace/object/tree/objecttree"
 
-	"github.com/anyproto/anytype-heart/core/debug/treearchive"
+	"github.com/anyproto/anytype-heart/core/debug/exporter"
 	"github.com/anyproto/anytype-heart/core/domain"
 	"github.com/anyproto/anytype-heart/pkg/lib/localstore/objectstore"
 	"github.com/anyproto/anytype-heart/util/anonymize"
@@ -26,35 +28,50 @@ type treeExporter struct {
 	s          objectstore.ObjectStore
 	anonymized bool
 	id         domain.FullID
-	zw         *zip.Writer
 }
 
 func (e *treeExporter) Export(ctx context.Context, path string, tree objecttree.ReadableObjectTree) (filename string, err error) {
-	filename = filepath.Join(path, fmt.Sprintf("at.dbg.%s.%s.zip", e.id, time.Now().Format("20060102.150405.99")))
-	archiveWriter, err := treearchive.NewArchiveWriter(filename)
+	var (
+		exportDirPath  = filepath.Join(path, fmt.Sprintf("at.dbg.%s.%s", e.id, time.Now().Format("20060102.150405.99")))
+		dbPath         = filepath.Join(exportDirPath, "db")
+		localStorePath = filepath.Join(exportDirPath, "localstore.json")
+		logPath        = filepath.Join(exportDirPath, "creation.log")
+	)
+	filename = exportDirPath + ".zip"
+	err = os.Mkdir(exportDirPath, 0755)
 	if err != nil {
 		return
 	}
-	defer archiveWriter.Close()
-
-	e.zw = archiveWriter.ZipWriter()
-	params := exporter.TreeExporterParams{
-		ListStorageExporter: archiveWriter,
-		TreeStorageExporter: archiveWriter,
-		DataConverter:       &changeDataConverter{anonymize: e.anonymized},
+	defer func() {
+		_ = os.RemoveAll(exportDirPath)
+	}()
+	err = os.Mkdir(dbPath, 0755)
+	if err != nil {
+		return
 	}
-	anySyncExporter := exporter.NewTreeExporter(params)
-	logBuf := bytes.NewBuffer(nil)
-
-	e.log = stdlog.New(io.MultiWriter(logBuf, os.Stderr), "", stdlog.LstdFlags)
-	e.log.Printf("exporting tree and acl")
+	anyStore, err := anystore.Open(ctx, dbPath, nil)
+	if err != nil {
+		return
+	}
+	defer func() {
+		_ = anyStore.Close()
+	}()
+	exportParams := exporter.ExportParams{
+		Readable:  tree,
+		Store:     anyStore,
+		Converter: &changeDataConverter{anonymize: e.anonymized},
+	}
 	st := time.Now()
-	err = anySyncExporter.ExportUnencrypted(tree)
+	err = exporter.ExportTree(ctx, exportParams)
 	if err != nil {
-		e.log.Printf("export tree in zip error: %v", err)
 		return
 	}
-
+	err = anyStore.Checkpoint(ctx, true)
+	if err != nil {
+		return
+	}
+	logBuf := bytes.NewBuffer(nil)
+	e.log = stdlog.New(io.MultiWriter(logBuf, os.Stderr), "", stdlog.LstdFlags)
 	e.log.Printf("exported tree for a %v", time.Since(st))
 	data, err := e.s.SpaceIndex(e.id.SpaceID).GetInfosByIds([]string{e.id.ObjectID})
 	if err != nil {
@@ -67,25 +84,21 @@ func (e *treeExporter) Export(ctx context.Context, path string, tree objecttree.
 				data[0].Relations[i] = transform(r, e.anonymized, anonymize.Relation)
 			}
 			osData := pbtypes.Sprint(data[0])
-			lsWr, er := e.zw.Create("localstore.json")
+			er := ioutil.WriteFile(localStorePath, []byte(osData), 0644)
 			if er != nil {
-				e.log.Printf("create file in zip error: %v", er)
+				e.log.Printf("localstore.json write error: %v", err)
 			} else {
-				if _, err := lsWr.Write([]byte(osData)); err != nil {
-					e.log.Printf("localstore.json write error: %v", err)
-				} else {
-					e.log.Printf("localstore.json wrote")
-				}
+				e.log.Printf("localstore.json wrote")
 			}
 		} else {
-			e.log.Printf("not data in objectstore")
+			e.log.Printf("no data in objectstore")
 		}
 	}
-	logW, err := e.zw.Create("creation.log")
+	err = ioutil.WriteFile(logPath, logBuf.Bytes(), 0644)
 	if err != nil {
 		return
 	}
-	io.Copy(logW, logBuf)
+	err = zipFolder(exportDirPath, filename)
 	return
 }
 
@@ -94,4 +107,45 @@ func transform[T any](in T, transformed bool, f func(T) T) T {
 		return f(in)
 	}
 	return in
+}
+
+func zipFolder(source, targetZip string) error {
+	zipFile, err := os.Create(targetZip)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	writer := zip.NewWriter(zipFile)
+	defer writer.Close()
+
+	return filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		relPath, err := filepath.Rel(source, path)
+		if err != nil {
+			return err
+		}
+		if relPath == "." {
+			return nil
+		}
+		if info.IsDir() {
+			_, err := writer.Create(strings.ReplaceAll(relPath, "\\", "/") + "/")
+			return err
+		}
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		zipWriter, err := writer.Create(strings.ReplaceAll(relPath, "\\", "/"))
+		if err != nil {
+			return err
+		}
+
+		// Copy the file data to the zip entry.
+		_, err = io.Copy(zipWriter, f)
+		return err
+	})
 }
