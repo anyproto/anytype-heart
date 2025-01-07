838f04dedfc707f01f3b1e09da269a84d1820899
kirillston <stonozhenko@anytype.io>
2025-01-07 12:05:12 +0300

    GO-4588 Allow to update recLayout via SetDetails

--- core/block/editor/basic/details.go
+++ core/block/editor/basic/details.go
@@ -3,7 +3,6 @@ package basic
 import (
 	"errors"
 	"fmt"
-	"slices"
 	"strings"
 	"time"
 
@@ -23,10 +22,6 @@ import (
 )
 
 var log = logging.Logger("anytype-mw-editor-basic")
-
-var relationsProhibitedToUpdate = []domain.RelationKey{
-	bundle.RelationKeyRecommendedLayout,
-}
 
 func (bs *basic) SetDetails(ctx session.Context, details []domain.Detail, showEvent bool) (err error) {
 	_, err = bs.setDetails(ctx, details, showEvent)
@@ -52,13 +47,6 @@ func (bs *basic) setDetails(ctx session.Context, details []domain.Detail, showEv
 	// Collect updates handling special cases. These cases could update details themselves, so we
 	// have to apply changes later
 	updates, updatedKeys := bs.collectDetailUpdates(details, s)
-
-	for _, rel := range relationsProhibitedToUpdate {
-		if slices.Contains(updatedKeys, rel) {
-			return nil, fmt.Errorf("it is prohibited to update %s detail via ObjectSetDetails", rel.String())
-		}
-	}
-
 	newDetails := applyDetailUpdates(s.CombinedDetails(), updates)
 	s.SetDetails(newDetails)
 
