2cb03241f6e3f49fa1d310d602e7ef2906a4e76d
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2025-01-11 18:44:30 +0100

    GO-4459: Refactor pagination middleware

--- cmd/api/server/router.go
+++ cmd/api/server/router.go
@@ -4,11 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"github.com/webstradev/gin-pagination/v2/pkg/pagination"
 
 	"github.com/anyproto/anytype-heart/cmd/api/auth"
 	"github.com/anyproto/anytype-heart/cmd/api/export"
 	"github.com/anyproto/anytype-heart/cmd/api/object"
+	"github.com/anyproto/anytype-heart/cmd/api/pagination"
 	"github.com/anyproto/anytype-heart/cmd/api/search"
 	"github.com/anyproto/anytype-heart/cmd/api/space"
 )
@@ -18,20 +18,19 @@ func (s *Server) NewRouter() *gin.Engine {
 	router := gin.Default()
 
 	// Pagination middleware setup
-	paginator := pagination.New(
-		pagination.WithPageText("offset"),
-		pagination.WithSizeText("limit"),
-		pagination.WithDefaultPage(0),
-		pagination.WithDefaultPageSize(100),
-		pagination.WithMinPageSize(1),
-		pagination.WithMaxPageSize(1000),
-	)
+	paginator := pagination.New(pagination.Config{
+		DefaultPage:     0,
+		DefaultPageSize: 100,
+		MinPageSize:     1,
+		MaxPageSize:     1000,
+	})
 
 	// Swagger route
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	// API routes
 	v1 := router.Group("/v1")
+	v1.Use(paginator)
 	v1.Use(s.initAccountInfo())
 	v1.Use(s.ensureAuthenticated())
 	{
@@ -44,20 +43,20 @@ func (s *Server) NewRouter() *gin.Engine {
 		v1.POST("/spaces/:space_id/objects/export/:format", export.GetSpaceExportHandler(s.exportService))
 
 		// Object
-		v1.GET("/spaces/:space_id/objects", paginator, object.GetObjectsHandler(s.objectService))
+		v1.GET("/spaces/:space_id/objects", object.GetObjectsHandler(s.objectService))
 		v1.GET("/spaces/:space_id/objects/:object_id", object.GetObjectHandler(s.objectService))
 		v1.DELETE("/spaces/:space_id/objects/:object_id", object.DeleteObjectHandler(s.objectService))
-		v1.GET("/spaces/:space_id/object_types", paginator, object.GetTypesHandler(s.objectService))
-		v1.GET("/spaces/:space_id/object_types/:typeId/templates", paginator, object.GetTemplatesHandler(s.objectService))
+		v1.GET("/spaces/:space_id/object_types", object.GetTypesHandler(s.objectService))
+		v1.GET("/spaces/:space_id/object_types/:typeId/templates", object.GetTemplatesHandler(s.objectService))
 		v1.POST("/spaces/:space_id/objects", object.CreateObjectHandler(s.objectService))
 		v1.PUT("/spaces/:space_id/objects/:object_id", object.UpdateObjectHandler(s.objectService))
 
 		// Search
-		v1.GET("/search", paginator, search.SearchHandler(s.searchService))
+		v1.GET("/search", search.SearchHandler(s.searchService))
 
 		// Space
-		v1.GET("/spaces", paginator, space.GetSpacesHandler(s.spaceService))
-		v1.GET("/spaces/:space_id/members", paginator, space.GetMembersHandler(s.spaceService))
+		v1.GET("/spaces", space.GetSpacesHandler(s.spaceService))
+		v1.GET("/spaces/:space_id/members", space.GetMembersHandler(s.spaceService))
 		v1.POST("/spaces", space.CreateSpaceHandler(s.spaceService))
 	}
 
