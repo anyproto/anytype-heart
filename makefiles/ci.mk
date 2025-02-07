88956cb41c98b65aa9cb970168304bb9989270ec
Grigory Efimov <grigory.efimov@gmail.com>
2025-02-07 16:48:54 -0300

    fixed .release dir

--- makefiles/ci.mk
+++ makefiles/ci.mk
@@ -9,7 +9,6 @@ install-brew-and-node-deps:
 	npm i -g node-gyp
 
 BUILD_TAGS ?= $(BUILD_TAG_NETWORK) nographviz nowatchdog nosigar nomutexdeadlockdetector
-OUTPUT_DIR ?= .release
 
 cross-compile-library:
 	echo $(FLAGS)
@@ -20,10 +19,7 @@ cross-compile-library:
 		cross-compile-library-linux-amd64 \
 		cross-compile-library-linux-arm64
 
-$(OUTPUT_DIR):
-	mkdir -p $(OUTPUT_DIR)
-
-cross-compile-library-darwin-amd64: $(OUTPUT_DIR)
+cross-compile-library-darwin-amd64:
 	echo $(SDKROOT)
 	GOOS="darwin" \
 		CGO_CFLAGS="-mmacosx-version-min=11" \
@@ -32,7 +28,7 @@ cross-compile-library-darwin-amd64: $(OUTPUT_DIR)
 		CGO_ENABLED="1" \
 		go build -tags="$(BUILD_TAGS)" -ldflags="$(FLAGS)" -o darwin-amd64 github.com/anyproto/anytype-heart/cmd/grpcserver
 
-cross-compile-library-darwin-arm64: $(OUTPUT_DIR)
+cross-compile-library-darwin-arm64:
 	SDKROOT=$(shell xcrun --sdk macosx --show-sdk-path)
 	echo $(SDKROOT)
 	GOOS="darwin" \
@@ -42,7 +38,7 @@ cross-compile-library-darwin-arm64: $(OUTPUT_DIR)
 		CGO_ENABLED="1" \
 		go build -tags="$(BUILD_TAGS)" -ldflags="$(FLAGS)" -o darwin-arm64 github.com/anyproto/anytype-heart/cmd/grpcserver
 
-cross-compile-library-windows-amd64: $(OUTPUT_DIR)
+cross-compile-library-windows-amd64:
 	GOOS="windows" \
 		GOARCH="amd64" \
 		CGO_ENABLED="1" \
@@ -50,14 +46,14 @@ cross-compile-library-windows-amd64: $(OUTPUT_DIR)
 		CXX="x86_64-w64-mingw32-g++" \
 		go build -tags="$(BUILD_TAGS) noheic" -ldflags="$(FLAGS) -linkmode external -extldflags=-static" -o windows-amd64 github.com/anyproto/anytype-heart/cmd/grpcserver
 
-cross-compile-library-linux-amd64: $(OUTPUT_DIR)
+cross-compile-library-linux-amd64:
 	GOOS="linux" \
 		GOARCH="amd64" \
 		CGO_ENABLED="1" \
 		CC="x86_64-linux-musl-gcc" \
 		go build -tags="$(BUILD_TAGS) noheic" -ldflags="$(FLAGS) -linkmode external -extldflags '-static -Wl,-z stack-size=1000000'" -o linux-amd64 github.com/anyproto/anytype-heart/cmd/grpcserver
 
-cross-compile-library-linux-arm64: $(OUTPUT_DIR)
+cross-compile-library-linux-arm64:
 	GOOS="linux" \
 		GOARCH="arm64" \
 		CGO_ENABLED="1" \
