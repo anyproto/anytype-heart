35fe1f0efb41f6536b36c32487abbfd9c387ac94
kirillston <stonozhenko@anytype.io>
2025-02-07 16:46:12 +0300

    GO-4594 Update required relations for types, relations and pages

--- Makefile
+++ Makefile
@@ -295,13 +295,13 @@ run-server: build-server
 	@echo 'Running server...'
 	@./dist/server
 
-install-dev-js-addon: setup build-lib build-js-addon protos-js
+install-dev-js-addon:  build-lib build-js-addon protos-js
 	@echo 'Installing JS-addon (dev-mode) in ${CLIENT_DESKTOP_PATH}...'
 	@rm -rf $(CLIENT_DESKTOP_PATH)/build
 	@cp -r clientlibrary/jsaddon/build $(CLIENT_DESKTOP_PATH)/
 	@cp -r dist/js/pb/* $(CLIENT_DESKTOP_PATH)/dist/lib
 
-install-dev-js: setup-go build-server protos-js
+install-dev-js:  build-server protos-js
 	@echo 'Installing JS-server (dev-mode) in $(CLIENT_DESKTOP_PATH)...'
 	@rm -f $(CLIENT_DESKTOP_PATH)/dist/anytypeHelper
 
--- pkg/lib/bundle/systemRelations.gen.go
+++ pkg/lib/bundle/systemRelations.gen.go
@@ -6,7 +6,7 @@ package bundle
 
 import domain "github.com/anyproto/anytype-heart/core/domain"
 
-const SystemRelationsChecksum = "cebd4ab7522c2ca901215e20861e005c1ad1a6ca2a77d7c1205e9e51edd901db"
+const SystemRelationsChecksum = "fef2bc9f46d95ba8105f4e8c5421466e35eb010920100332a3427bcf59071fc8"
 
 // SystemRelations contains relations that have some special biz logic depends on them in some objects
 // in case EVERY object depend on the relation please add it to RequiredInternalRelations
@@ -82,4 +82,7 @@ var SystemRelations = append(RequiredInternalRelations, []domain.RelationKey{
 	RelationKeyChatId,
 	RelationKeyHasChat,
 	RelationKeyTimestamp,
+	RelationKeyRecommendedFeaturedRelations,
+	RelationKeyRecommendedHiddenRelations,
+	RelationKeyRecommendedFileRelations,
 }...)
