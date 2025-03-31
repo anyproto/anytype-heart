cf52393df19c29ad7a42314eba477b5ba2bb1d7a
Anastasia Shemyakinskaya <36507473+anastasiashemyakinskaya@users.noreply.github.com>
2025-03-31 14:18:27 +0200

    GO-5320: fix tests
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- core/block/import/common/collection_test.go
+++ core/block/import/common/collection_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/anyproto/anytype-heart/core/block/collection"
 	"github.com/anyproto/anytype-heart/core/domain"
 	"github.com/anyproto/anytype-heart/pkg/lib/bundle"
-	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
 )
 
 func TestMakeImportCollection(t *testing.T) {
@@ -18,57 +17,32 @@ func TestMakeImportCollection(t *testing.T) {
 		needToAddDate     bool
 		shouldBeFavorite  bool
 		shouldAddRelation bool
-		widgetSnapshot    bool
 	}{
-		{"all false", false, false, false, false},
-		{"add date", true, false, false, false},
-		{"add favorite", false, true, false, false},
-		{"add relations", false, false, true, false},
-		{"with existing widget snapshot", false, false, false, true},
-		{"all True", true, true, true, true},
+		{"all false", false, false, false},
+		{"add date", true, false, false},
+		{"add favorite", false, true, false},
+		{"add relations", false, false, true},
+		{"all True", true, true, true},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			importer := NewImportCollection(collection.New())
 
-			var widget *Snapshot
-			if tt.widgetSnapshot {
-				widget = &Snapshot{
-					Id:       "widget-id",
-					FileName: "existing",
-					Snapshot: &SnapshotModel{
-						Data: &StateSnapshot{
-							Blocks: []*model.Block{
-								{
-									Id:          "root-block",
-									ChildrenIds: []string{},
-									Content: &model.BlockContentOfSmartblock{
-										Smartblock: &model.BlockContentSmartblock{},
-									},
-								},
-							},
-						},
-					},
-				}
-			}
-
 			req := NewImportCollectionSetting(
 				WithCollectionName("My Collection"),
 				WithTargetObjects([]string{"obj1", "obj2"}),
 				WithIcon("icon.png"),
-				WithWidgetSnapshot(widget),
 			)
 
 			req.needToAddDate = tt.needToAddDate
 			req.shouldBeFavorite = tt.shouldBeFavorite
 			req.shouldAddRelations = tt.shouldAddRelation
 
-			root, widgetSnap, err := importer.MakeImportCollection(req)
+			root, err := importer.MakeImportCollection(req)
 
 			assert.NoError(t, err)
 			assert.NotNil(t, root)
-			assert.NotNil(t, widgetSnap)
 
 			if tt.needToAddDate {
 				assert.Contains(t, root.FileName, time.Now().Format("2006"))
@@ -82,11 +56,6 @@ func TestMakeImportCollection(t *testing.T) {
 				assert.Equal(t, domain.Bool(false), root.Snapshot.Data.Details.Get(bundle.RelationKeyIsFavorite))
 			}
 
-			if tt.widgetSnapshot {
-				assert.Equal(t, "existing", widgetSnap.FileName)
-			} else {
-				assert.Equal(t, "rootWidget", widgetSnap.FileName)
-			}
 		})
 	}
 }
