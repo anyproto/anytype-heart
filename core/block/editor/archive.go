e12520ddd6694b416b69f5187c8169a3cf8594c1
Roman Khafizianov <requilence@gmail.com>
2025-03-31 12:38:21 +0200

    GO-5376 All objects

--- core/block/editor/archive.go
+++ core/block/editor/archive.go
@@ -55,7 +55,7 @@ func (p *Archive) Init(ctx *smartblock.InitContext) (err error) {
 
 func (p *Archive) CreationStateMigration(ctx *smartblock.InitContext) migration.Migration {
 	return migration.Migration{
-		Version: 2,
+		Version: 3,
 		Proc: func(st *state.State) {
 			template.InitTemplate(st,
 				template.WithEmpty,
