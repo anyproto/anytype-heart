433537998fd92aac1c62c58f55d1b8d3a05cbf24
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2025-05-14 00:25:58 +0200

    GO-5589: Format pretty openapi.json

--- makefiles/tools.mk
+++ makefiles/tools.mk
@@ -13,7 +13,8 @@ lint:
 openapi:
 	@echo 'Generating openapi docs...'
 	@swag init --v3.1 -q -d core/api -g service.go -o $(OPENAPI_DOCS_DIR)
-	@mv $(OPENAPI_DOCS_DIR)/swagger.json $(OPENAPI_DOCS_DIR)/openapi.json
 	@mv $(OPENAPI_DOCS_DIR)/swagger.yaml $(OPENAPI_DOCS_DIR)/openapi.yaml
+	@mv $(OPENAPI_DOCS_DIR)/swagger.json $(OPENAPI_DOCS_DIR)/openapi.json
+	@jq . "$(OPENAPI_DOCS_DIR)/openapi.json" > "$(OPENAPI_DOCS_DIR)/pretty.json" && mv "$(OPENAPI_DOCS_DIR)/pretty.json" "$(OPENAPI_DOCS_DIR)/openapi.json"
 	@echo 'Formatting openapi docs...'
 	@swag fmt -d core/api
