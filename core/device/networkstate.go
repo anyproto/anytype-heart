f72c0a53ebfc5dc77e4c9205a920d56f8b828784
Mikhail Rakhmanov <rakhmanov.m@gmail.com>
2025-06-15 14:54:50 +0200

    GO-5762 WIP Flush pool on state change

--- core/device/networkstate.go
+++ core/device/networkstate.go
@@ -1,10 +1,15 @@
 package device
 
 import (
+	"context"
 	"sync"
+	"time"
 
 	"github.com/anyproto/any-sync/app"
+	"github.com/anyproto/any-sync/net/pool"
+	"go.uber.org/zap"
 
+	"github.com/anyproto/anytype-heart/core/domain"
 	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
 )
 
@@ -12,17 +17,40 @@ const CName = "networkState"
 
 type NetworkState interface {
 	app.Component
+	app.ComponentStatable
 	GetNetworkState() model.DeviceNetworkType
 	SetNetworkState(networkState model.DeviceNetworkType)
 	RegisterHook(hook func(network model.DeviceNetworkType))
 }
 
+const networkInvalid = time.Second * 10
+
 type networkState struct {
-	networkState model.DeviceNetworkType
-	networkMu    sync.Mutex
+	networkState          model.DeviceNetworkType
+	networkMu             sync.Mutex
+	lastDeviceState       domain.CompState
+	lastDeviceStateChange time.Time
 
 	onNetworkUpdateHooks []func(network model.DeviceNetworkType)
 	hookMu               sync.Mutex
+	pool                 pool.Service
+}
+
+func (n *networkState) StateChange(state int) {
+	n.hookMu.Lock()
+	defer n.hookMu.Unlock()
+	// ioslogger.DebugLog(fmt.Sprintf("change network state to %d", state))
+	devState := domain.CompState(state)
+	timePassed := time.Since(n.lastDeviceStateChange)
+	if n.lastDeviceState != devState && devState == domain.CompStateAppWentForeground && timePassed > networkInvalid {
+		err := n.pool.Flush(context.Background())
+		if err != nil {
+			log.Debug("failed to flush pool on network state change", zap.Error(err))
+		}
+		// ioslogger.DebugLog("flushed pool on network state change")
+	}
+	n.lastDeviceStateChange = time.Now()
+	n.lastDeviceState = devState
 }
 
 func New() NetworkState {
@@ -30,6 +58,7 @@ func New() NetworkState {
 }
 
 func (n *networkState) Init(a *app.App) (err error) {
+	n.pool = a.MustComponent(pool.CName).(pool.Service)
 	return
 }
 
