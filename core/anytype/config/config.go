a703e5d7eab3ece81bbcd956221645339feca6e5
Sergey Cherepanov <cheggaaa@gmail.com>
2025-06-16 13:03:41 +0200

    GO-5636 remove debug

--- core/anytype/config/config.go
+++ core/anytype/config/config.go
@@ -510,11 +510,8 @@ func (c *Config) GetPublishServer() publishclient.Config {
 }
 
 func (c *Config) GetPushConfig() PushConfig {
-	// pushPeerId := "12D3KooWMATrdteJNq2YvYhtq3RDeWxq6RVXDAr36MsGd5RJzXDn"
-	// pushAddr := "anytype-push-server.anytype.io:4941"
-
-	pushPeerId := "12D3KooWR8Ci1XidFCCXoZppGrUmiy4D1Mjoux9xK6QoZrpbQC3J"
-	pushAddr := "127.0.0.1:4940"
+	pushPeerId := "12D3KooWMATrdteJNq2YvYhtq3RDeWxq6RVXDAr36MsGd5RJzXDn"
+	pushAddr := "anytype-push-server.anytype.io:4941"
 
 	if peerId := os.Getenv("ANYTYPE_PUSH_PEERID"); peerId != "" {
 		if addr := os.Getenv("ANYTYPE_PUSH_ADDRESS"); addr != "" {
