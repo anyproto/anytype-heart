855f8e5759e228d0e52f136985e5141fd829fd46
Sergey Cherepanov <cheggaaa@gmail.com>
2025-06-17 14:59:02 +0200

    GO-5747 fix subscription batcher

--- core/subscription/service.go
+++ core/subscription/service.go
@@ -787,9 +787,9 @@ func (s *spaceSubscriptions) recordsHandler() {
 			}
 		}
 	}
+	ctx := mb.CtxWithTimeLimit(s.ctx, time.Millisecond*100)
 	for {
-		ctx := mb.CtxWithTimeLimit(s.ctx, time.Millisecond*100)
-		records, err := s.recBatch.WaitCond(ctx, mb.WaitCond[database.Record]{Max: 1000})
+		records, err := s.recBatch.WaitCond(ctx, mb.WaitCond[database.Record]{Min: 50, Max: 1000})
 		if err != nil {
 			return
 		}
