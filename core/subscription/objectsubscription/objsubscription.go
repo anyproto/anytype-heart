165a733b06183cd14472ddadadb7ae1e1e28dd76
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2025-07-31 15:38:32 +0200

    GO-5645: Undo adding remove callback to objsubscription.go

--- core/subscription/objectsubscription/objsubscription.go
+++ core/subscription/objectsubscription/objsubscription.go
@@ -16,7 +16,6 @@ type (
 	extract[T any] func(*domain.Details) (string, T)
 	update[T any]  func(string, domain.Value, T) T
 	unset[T any]   func([]string, T) T
-	remove[T any]  func(string, T) T
 )
 
 type entry[T any] struct {
@@ -36,7 +35,6 @@ type SubscriptionParams[T any] struct {
 	Extract extract[T]
 	Update  update[T]
 	Unset   unset[T]
-	Remove  remove[T]
 }
 
 type ObjectSubscription[T any] struct {
@@ -50,7 +48,6 @@ type ObjectSubscription[T any] struct {
 	extract extract[T]
 	update  update[T]
 	unset   unset[T]
-	remove  remove[T]
 	mx      sync.Mutex
 }
 
@@ -66,9 +63,6 @@ func NewIdSubscription(service subscription.Service, request subscription.Subscr
 		Unset: func(strings []string, s struct{}) struct{} {
 			return struct{}{}
 		},
-		Remove: func(s string, s2 struct{}) struct{} {
-			return struct{}{}
-		},
 	})
 }
 
@@ -80,7 +74,6 @@ func New[T any](service subscription.Service, params SubscriptionParams[T]) *Obj
 		extract: params.Extract,
 		update:  params.Update,
 		unset:   params.Unset,
-		remove:  params.Remove,
 	}
 }
 
@@ -131,11 +124,6 @@ func (o *ObjectSubscription[T]) read() {
 		case *pb.EventMessageValueOfSubscriptionAdd:
 			o.sub[v.SubscriptionAdd.Id] = newEmptyEntry[T]()
 		case *pb.EventMessageValueOfSubscriptionRemove:
-			curEntry := o.sub[v.SubscriptionRemove.Id]
-			if curEntry == nil {
-				return
-			}
-			o.remove(v.SubscriptionRemove.Id, curEntry.data)
 			delete(o.sub, v.SubscriptionRemove.Id)
 		case *pb.EventMessageValueOfObjectDetailsAmend:
 			curEntry := o.sub[v.ObjectDetailsAmend.Id]
