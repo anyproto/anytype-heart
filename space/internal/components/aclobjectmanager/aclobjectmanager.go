599f450e6aedf75b554f857f09489178ce197ea6
kirillston <stonozhenko@anytype.io>
2025-08-11 18:16:54 +0300

    GO-6054 Broadcast own identity on participant reqister

--- space/internal/components/aclobjectmanager/aclobjectmanager.go
+++ space/internal/components/aclobjectmanager/aclobjectmanager.go
@@ -145,17 +145,13 @@ func (a *aclObjectManager) process() {
 		return
 	}
 	a.spaceLoaderListener.OnSpaceLoad(a.sp.Id())
-	err := a.participantWatcher.UpdateAccountParticipantFromProfile(a.ctx, a.sp)
-	if err != nil {
-		log.Error("init my identity", zap.Error(err))
-	}
 
 	common := a.sp.CommonSpace()
 	acl := common.Acl()
 	acl.SetAclUpdater(a)
 	acl.RLock()
 	defer acl.RUnlock()
-	err = a.processAcl()
+	err := a.processAcl()
 	if err != nil {
 		log.Error("error processing acl", zap.Error(err))
 		return
@@ -314,12 +310,6 @@ func (a *aclObjectManager) processStates(states []list.AccountState, upToDate bo
 		err = a.participantWatcher.UpdateParticipantFromAclState(a.ctx, a.sp, state)
 		if err != nil {
 			return err
-		}
-		if state.PubKey == myIdentity {
-			err = a.participantWatcher.UpdateAccountParticipantFromProfile(a.ctx, a.sp)
-			if err != nil {
-				return err
-			}
 		}
 		err = a.participantWatcher.WatchParticipant(a.ctx, a.sp, state)
 		if err != nil {
