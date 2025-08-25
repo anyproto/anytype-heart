fec490fe029034601150522836169104cb4ad843
Sergey <smalenkov@anytype.io>
2025-08-25 14:24:18 +0200

    GO-6147: Repository with subscriptions

--- core/files/filesync/repository.go
+++ core/files/filesync/repository.go
@@ -1,23 +1,143 @@
 package filesync
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	anystore "github.com/anyproto/any-store"
 	"github.com/anyproto/any-store/anyenc"
+	"github.com/anyproto/any-store/query"
 	"github.com/ipfs/go-cid"
 
 	"github.com/anyproto/anytype-heart/core/domain"
 )
 
+var errNoRows = fmt.Errorf("no rows")
+
 type anystoreFileRepository struct {
+	arenaPool *anyenc.ArenaPool
+	coll      anystore.Collection
+
+	ctx       context.Context
+	ctxCancel context.CancelFunc
+
+	updateCh      chan FileInfo
+	subscriptions map[chan FileInfo]struct{}
+
+	subscribeCh   chan chan FileInfo
+	unsubscribeCh chan chan FileInfo
+}
+
+func newAnystoreFileRepository(coll anystore.Collection) *anystoreFileRepository {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &anystoreFileRepository{
+		arenaPool:     &anyenc.ArenaPool{},
+		coll:          coll,
+		ctx:           ctx,
+		ctxCancel:     cancel,
+		updateCh:      make(chan FileInfo),
+		subscriptions: make(map[chan FileInfo]struct{}),
+		subscribeCh:   make(chan chan FileInfo),
+		unsubscribeCh: make(chan chan FileInfo),
+	}
+}
+
+func (r *anystoreFileRepository) runSubscriptions() {
+	go func() {
+		for {
+			select {
+			case ch := <-r.subscribeCh:
+				r.subscriptions[ch] = struct{}{}
+			case ch := <-r.unsubscribeCh:
+				if _, ok := r.subscriptions[ch]; ok {
+					delete(r.subscriptions, ch)
+					close(ch)
+				}
+			case it := <-r.updateCh:
+				for sub := range r.subscriptions {
+					sub <- it
+				}
+			case <-r.ctx.Done():
+				return
+			}
+		}
+	}()
+}
+
+func (r *anystoreFileRepository) subscribe() chan FileInfo {
+	ch := make(chan FileInfo)
+	r.subscribeCh <- ch
+	return ch
+}
+
+func (r *anystoreFileRepository) unsubscribe(ch chan FileInfo) {
+	// Drain the channel
+	select {
+	case <-ch:
+	default:
+	}
+
+	r.unsubscribeCh <- ch
+}
+
+func (r *anystoreFileRepository) upsert(file FileInfo) error {
+	arena := r.arenaPool.Get()
+	defer r.arenaPool.Put(arena)
+
+	doc := marshalFileInfo(file, arena)
+	err := r.coll.UpsertOne(r.ctx, doc)
+	if err != nil {
+		return err
+	}
+	r.updateCh <- file
+	return nil
+}
+
+func (r *anystoreFileRepository) queryOne(filter query.Filter, sorts []query.Sort) (*FileInfo, error) {
+	sortsArgs := make([]any, 0, len(sorts))
+	for _, sort := range sorts {
+		sortsArgs = append(sortsArgs, sort)
+	}
+	iter, err := r.coll.Find(filter).Sort(sortsArgs...).Limit(1).Iter(r.ctx)
+	if err != nil {
+		return nil, fmt.Errorf("create iterator: %w", err)
+	}
+	defer iter.Close()
+
+	ok := iter.Next()
+	if !ok {
+		return nil, errNoRows
+	}
+	err = iter.Err()
+	if err != nil {
+		return nil, fmt.Errorf("iterate: %w", err)
+	}
+
+	doc, err := iter.Doc()
+	if err != nil {
+		return nil, fmt.Errorf("get doc: %w", err)
+	}
+
+	fi, err := unmarshalFileInfo(doc.Value())
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal: %w", err)
+	}
+
+	return &fi, nil
+}
+
+func (r *anystoreFileRepository) Close() {
+	if r.ctxCancel != nil {
+		r.ctxCancel()
+	}
 }
 
 func marshalFileInfo(info FileInfo, arena *anyenc.Arena) *anyenc.Value {
 	obj := arena.NewObject()
 	obj.Set("fileId", arena.NewString(info.FileId.String()))
 	obj.Set("spaceId", arena.NewString(info.SpaceId))
-	obj.Set("objectId", arena.NewString(info.ObjectId))
+	obj.Set("id", arena.NewString(info.ObjectId))
 	obj.Set("state", arena.NewNumberInt(int(info.State)))
 	obj.Set("addedAt", arena.NewNumberInt(int(info.AddedAt.UTC().Unix())))
 	obj.Set("handledAt", arena.NewNumberInt(int(info.HandledAt.UTC().Unix())))
@@ -66,7 +186,7 @@ func unmarshalFileInfo(doc *anyenc.Value) (FileInfo, error) {
 	return FileInfo{
 		FileId:        fileId,
 		SpaceId:       doc.GetString("spaceId"),
-		ObjectId:      doc.GetString("objectId"),
+		ObjectId:      doc.GetString("id"),
 		State:         FileState(doc.GetInt("state")),
 		AddedAt:       time.Unix(int64(doc.GetInt("addedAt")), 0).UTC(),
 		HandledAt:     time.Unix(int64(doc.GetInt("handledAt")), 0).UTC(),
--- core/files/filesync/repository_test.go
+++ core/files/filesync/repository_test.go
@@ -1,23 +1,180 @@
 package filesync
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/anyproto/any-store/anyenc"
+	"github.com/anyproto/any-store/query"
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/anyproto/anytype-heart/core/domain"
+	"github.com/anyproto/anytype-heart/pkg/lib/datastore/anystoreprovider"
 )
 
 func TestMarshalUnmarshal(t *testing.T) {
 	arena := &anyenc.Arena{}
 
+	fi := givenFileInfo()
+
+	doc := marshalFileInfo(fi, arena)
+
+	got, err := unmarshalFileInfo(doc)
+
+	require.NoError(t, err)
+	assert.Equal(t, fi, got)
+}
+
+func newTestRepo(t *testing.T) *anystoreFileRepository {
+	provider, err := anystoreprovider.NewInPath(t.TempDir())
+	require.NoError(t, err)
+
+	ctx := context.Background()
+
+	db := provider.GetCommonDb()
+	coll, err := db.Collection(ctx, "queue")
+	require.NoError(t, err)
+
+	repo := newAnystoreFileRepository(coll)
+	go repo.runSubscriptions()
+
+	return repo
+}
+
+func TestRepositorySubscriptions(t *testing.T) {
+	repo := newTestRepo(t)
+
+	updatesCh1 := repo.subscribe()
+	updatesCh2 := repo.subscribe()
+
+	want := givenFileInfo()
+
+	go func() {
+		for range 2 {
+			got := <-updatesCh1
+			assert.Equal(t, want, got)
+		}
+	}()
+	go func() {
+		for range 2 {
+			got := <-updatesCh2
+			assert.Equal(t, want, got)
+		}
+	}()
+
+	for range 2 {
+		err := repo.upsert(want)
+		require.NoError(t, err)
+	}
+}
+
+func TestRepositoryUnsubscribe(t *testing.T) {
+	repo := newTestRepo(t)
+
+	updatesCh := repo.subscribe()
+	repo.unsubscribe(updatesCh)
+
+	err := repo.upsert(givenFileInfo())
+	require.NoError(t, err)
+
+	timeout := time.After(50 * time.Millisecond)
+	select {
+	case _, ok := <-updatesCh:
+		assert.False(t, ok)
+	case <-timeout:
+		t.Fatal("unsubscribe timeout")
+	}
+}
+
+func TestRepositoryQuery(t *testing.T) {
+	t.Run("no rows", func(t *testing.T) {
+		repo := newTestRepo(t)
+
+		_, err := repo.queryOne(nil, nil)
+		assert.ErrorIs(t, err, errNoRows)
+	})
+
+	t.Run("no filters", func(t *testing.T) {
+		repo := newTestRepo(t)
+
+		want := givenFileInfo()
+
+		err := repo.upsert(want)
+		require.NoError(t, err)
+
+		got, err := repo.queryOne(nil, nil)
+		require.NoError(t, err)
+		assert.Equal(t, want, *got)
+	})
+
+	t.Run("state filter", func(t *testing.T) {
+		repo := newTestRepo(t)
+
+		want := givenFileInfo()
+
+		err := repo.upsert(want)
+		require.NoError(t, err)
+
+		got, err := repo.queryOne(query.Key{
+			Path:   []string{"state"},
+			Filter: query.NewComp(query.CompOpEq, int(FileStateUploading)),
+		}, nil)
+		require.NoError(t, err)
+		assert.Equal(t, want, *got)
+	})
+
+	t.Run("state filter and sort", func(t *testing.T) {
+		repo := newTestRepo(t)
+
+		infos := make([]FileInfo, 3)
+		for i := range 3 {
+			want := givenFileInfo()
+			want.ObjectId = fmt.Sprintf("object%d", i)
+			want.HandledAt = want.HandledAt.Add(time.Duration(i) * time.Minute)
+
+			err := repo.upsert(want)
+			require.NoError(t, err)
+
+			infos[i] = want
+		}
+
+		got, err := repo.queryOne(query.Key{
+			Path:   []string{"state"},
+			Filter: query.NewComp(query.CompOpEq, int(FileStateUploading)),
+		}, []query.Sort{
+			&query.SortField{
+				Field:   "handledAt",
+				Path:    []string{"handledAt"},
+				Reverse: false,
+			},
+		})
+		require.NoError(t, err)
+		assert.Equal(t, infos[0], *got)
+
+		got, err = repo.queryOne(query.Key{
+			Path:   []string{"state"},
+			Filter: query.NewComp(query.CompOpEq, int(FileStateUploading)),
+		}, []query.Sort{
+			&query.SortField{
+				Field:   "handledAt",
+				Path:    []string{"handledAt"},
+				Reverse: true,
+			},
+		})
+		require.NoError(t, err)
+		assert.Equal(t, infos[2], *got)
+	})
+}
+
+func givenFileInfo() FileInfo {
+
 	testFileId := domain.FileId("bafybeihdwdcefgh4dqkjv67uzcmw7ojee6xedzdetojuzjevtenxquvyku")
 
-	fi := FileInfo{
+	return FileInfo{
 		FileId:        testFileId,
 		SpaceId:       "space1",
 		ObjectId:      "object1",
@@ -32,11 +189,4 @@ func TestMarshalUnmarshal(t *testing.T) {
 			cid.MustParse(testFileId.String()): {},
 		},
 	}
-
-	doc := marshalFileInfo(fi, arena)
-
-	got, err := unmarshalFileInfo(doc)
-
-	require.NoError(t, err)
-	assert.Equal(t, fi, got)
 }
