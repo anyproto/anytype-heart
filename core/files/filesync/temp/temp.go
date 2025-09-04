0d86542516869907ea03398294d425fc16ba7f3a
Sergey <smalenkov@anytype.io>
2025-09-04 16:07:29 +0200

    GO-6147: WIP Queue

--- core/files/filesync/temp/temp.go
+++ core/files/filesync/temp/temp.go
@@ -2,66 +2,314 @@ package temp
 
 import (
 	"fmt"
-	"sync"
+	"slices"
+	"sort"
+	"time"
+
+	"github.com/ipfs/go-cid"
+
+	"github.com/anyproto/anytype-heart/core/domain"
+)
+
+type FileState int
+
+const (
+	FileStatePendingUpload FileState = iota
+	FileStateUploading
+	FileStateLimited
+	FileStatePendingDeletion
+	FileStateDone
+	FileStateDeleted
 )
 
 type FileInfo struct {
-	ObjectId string
-	State    string
-	Value    int
+	FileId      domain.FileId
+	SpaceId     string
+	ObjectId    string
+	State       FileState
+	ScheduledAt time.Time
+	HandledAt   time.Time
+	Variants    []domain.FileId
+	AddedByUser bool
+	Imported    bool
+
+	BytesToUpload int
+	CidsToUpload  map[cid.Cid]struct{}
 }
 
-func (i FileInfo) Key() string {
-	return i.ObjectId
-}
-
-type inmemoryQueue struct {
-	lock  sync.Mutex
+type storage struct {
 	files map[string]FileInfo
-
-	processing map[string]*sync.Mutex
 }
 
-func newInmemoryQueue() *inmemoryQueue {
-	return &inmemoryQueue{
-		files:      make(map[string]FileInfo),
-		processing: make(map[string]*sync.Mutex),
+func (s *storage) get(objectId string) FileInfo {
+	return s.files[objectId]
+}
+
+func (s *storage) set(objectId string, file FileInfo) {
+	s.files[objectId] = file
+}
+
+func (s *storage) query(filter func(FileInfo) bool, schedulingOrder func(info FileInfo) time.Time) (FileInfo, bool) {
+	var res []FileInfo
+	for _, file := range s.files {
+		if filter(file) {
+			res = append(res, file)
+		}
+	}
+	sort.Slice(res, func(i, j int) bool {
+		return schedulingOrder(res[i]).Before(schedulingOrder(res[j]))
+	})
+	if len(res) == 0 {
+		return FileInfo{}, false
+	}
+	return res[0], true
+}
+
+type getByIdRequest struct {
+	objectId   string
+	responseCh chan getByIdResponse
+}
+
+type getByIdResponse struct {
+	info FileInfo
+}
+
+type getNextRequest struct {
+	requestId   string
+	filter      func(FileInfo) bool
+	scheduledAt func(FileInfo) time.Time
+
+	responseCh chan getByIdResponse
+}
+
+type scheduledItem struct {
+	timer         *time.Timer
+	cancelTimerCh chan struct{}
+	item          FileInfo
+
+	request    getNextRequest
+	responseCh chan getByIdResponse
+}
+
+type queue struct {
+	store *storage
+
+	closeCh     chan struct{}
+	getByIdCh   chan getByIdRequest
+	getNextCh   chan getNextRequest
+	releaseCh   chan FileInfo
+	scheduledCh chan scheduledItem
+
+	getByIdWaiters   map[string][]chan getByIdResponse
+	scheduledWaiters map[string][]scheduledItem
+	getNextWaiters   []getNextRequest
+
+	taskLocked map[string]struct{}
+	scheduled  map[string]scheduledItem
+}
+
+func newQueue(store *storage) *queue {
+	return &queue{
+		store:            store,
+		closeCh:          make(chan struct{}),
+		getByIdCh:        make(chan getByIdRequest),
+		getNextCh:        make(chan getNextRequest),
+		scheduledCh:      make(chan scheduledItem),
+		releaseCh:        make(chan FileInfo),
+		getByIdWaiters:   make(map[string][]chan getByIdResponse),
+		taskLocked:       make(map[string]struct{}),
+		scheduled:        make(map[string]scheduledItem),
+		scheduledWaiters: map[string][]scheduledItem{},
 	}
 }
 
-func (q *inmemoryQueue) isProcessing(key string) bool {
-	_, ok := q.processing[key]
-	return ok
+func (q *queue) run() {
+	for {
+		select {
+		case <-q.closeCh:
+			return
+		case req := <-q.getByIdCh:
+			_, isLocked := q.taskLocked[req.objectId]
+			if isLocked {
+				q.getByIdWaiters[req.objectId] = append(q.getByIdWaiters[req.objectId], req.responseCh)
+			} else {
+				q.taskLocked[req.objectId] = struct{}{}
+				req.responseCh <- getByIdResponse{info: q.store.get(req.objectId)}
+			}
+		case req := <-q.getNextCh:
+			q.handleGetNext(req)
+		case req := <-q.releaseCh:
+			delete(q.taskLocked, req.ObjectId)
+			q.store.set(req.ObjectId, req)
+
+			for _, sch := range q.scheduled {
+				if sch.item.ObjectId == req.ObjectId {
+					close(sch.cancelTimerCh)
+					if sch.request.filter(req) {
+						q.scheduleItem(sch.request, req)
+					} else {
+						q.handleGetNext(sch.request)
+					}
+				} else if sch.request.filter(req) && sch.request.scheduledAt(req).Before(sch.request.scheduledAt(sch.item)) {
+					fmt.Println("schedule earlier item")
+					close(sch.cancelTimerCh)
+					q.scheduleItem(sch.request, req)
+				}
+			}
+
+			// Prioritize scheduled items
+			var responded bool
+			scheduledWaiters := q.scheduledWaiters[req.ObjectId]
+			if len(scheduledWaiters) > 0 {
+				filtered := scheduledWaiters[:0]
+
+				for _, nextScheduled := range scheduledWaiters {
+					// Still OK
+					if nextScheduled.request.filter(req) {
+						if !responded {
+							scheduledWaiters = scheduledWaiters[1:]
+							q.taskLocked[req.ObjectId] = struct{}{}
+
+							nextScheduled.responseCh <- getByIdResponse{info: req}
+							responded = true
+						} else {
+							filtered = append(filtered, nextScheduled)
+						}
+					} else {
+						fmt.Println("sch waiter: item is no longer fit the filter, re-schedule")
+						q.handleGetNext(nextScheduled.request)
+					}
+				}
+
+				scheduledWaiters = filtered
+				if len(scheduledWaiters) == 0 {
+					delete(q.scheduledWaiters, req.ObjectId)
+				} else {
+					q.scheduledWaiters[req.ObjectId] = scheduledWaiters
+				}
+			}
+
+			waiters := q.getByIdWaiters[req.ObjectId]
+			if !responded && len(waiters) > 0 {
+				nextResponseCh := waiters[0]
+				waiters = waiters[1:]
+				q.taskLocked[req.ObjectId] = struct{}{}
+				nextResponseCh <- getByIdResponse{info: req}
+
+				if len(waiters) == 0 {
+					delete(q.getByIdWaiters, req.ObjectId)
+				} else {
+					q.getByIdWaiters[req.ObjectId] = waiters
+				}
+			}
+
+			for i, waiter := range q.getNextWaiters {
+				if waiter.filter(req) {
+					q.getNextWaiters = slices.Delete(q.getNextWaiters, i, i+1)
+					q.handleGetNext(waiter)
+					break
+				}
+			}
+
+		case req := <-q.scheduledCh:
+			id := req.item.ObjectId
+			delete(q.scheduled, req.request.requestId)
+			_, isLocked := q.taskLocked[id]
+			if isLocked {
+				q.scheduledWaiters[id] = append(q.scheduledWaiters[id], req)
+			} else {
+				q.taskLocked[id] = struct{}{}
+				req.responseCh <- getByIdResponse{info: q.store.get(id)}
+			}
+		}
+	}
 }
 
-func (q *inmemoryQueue) process(key string, proc func(exists bool, info FileInfo) (FileInfo, error)) {
-	q.lock.Lock()
-	procLock, ok := q.processing[key]
+func (q *queue) close() {
+	close(q.closeCh)
+}
+
+func (q *queue) handleGetNext(req getNextRequest) {
+	// TODO Skip locked rows
+	next, ok := q.store.query(func(info FileInfo) bool {
+		if _, ok := q.taskLocked[info.ObjectId]; ok {
+			return false
+		}
+		return req.filter(info)
+	}, req.scheduledAt)
 	if !ok {
-		procLock = &sync.Mutex{}
-		q.processing[key] = procLock
-		procLock.Lock()
-	}
-	q.lock.Unlock()
-
-	if ok {
-		procLock.Lock()
-	}
-	defer procLock.Unlock()
-
-	q.lock.Lock()
-	fi, exists := q.files[key]
-	q.lock.Unlock()
-
-	// Critical section
-
-	next, err := proc(exists, fi)
-	if err != nil {
-		fmt.Println("ERROR", err)
+		q.getNextWaiters = append(q.getNextWaiters, req)
+		return
 	}
 
-	q.lock.Lock()
-	q.files[key] = next
-	delete(q.processing, key)
-	q.lock.Unlock()
+	if next.ScheduledAt.Before(time.Now()) {
+		q.taskLocked[next.ObjectId] = struct{}{}
+		req.responseCh <- getByIdResponse{info: next}
+		return
+	}
+
+	q.scheduleItem(req, next)
+
+	// TODO Get next by filter + order, ignore locked
+	// TODO 2. If it's not, run timer --> if scheduled item changed, check if it's still valid, reschedule if needed OR un-schedule and add to waiter like in 3
+	/*
+		In cases 2 and 3 client will receive
+	*/
+}
+
+func (q *queue) scheduleItem(req getNextRequest, next FileInfo) {
+	fmt.Println("schedule item", next.ObjectId)
+	timer := time.NewTimer(time.Until(next.ScheduledAt))
+	cancelTimerCh := make(chan struct{})
+	scheduled := scheduledItem{
+		timer:         timer,
+		cancelTimerCh: cancelTimerCh,
+		item:          next,
+
+		request:    req,
+		responseCh: req.responseCh,
+	}
+	q.scheduled[req.requestId] = scheduled
+
+	go func() {
+		defer timer.Stop()
+
+		select {
+		case <-timer.C:
+			fmt.Println("deadline came", next.ObjectId)
+			q.scheduledCh <- scheduled
+		case <-cancelTimerCh:
+			return
+		}
+	}()
+}
+
+func (q *queue) get(objectId string) FileInfo {
+	responseCh := make(chan getByIdResponse, 1)
+
+	q.getByIdCh <- getByIdRequest{
+		objectId:   objectId,
+		responseCh: responseCh,
+	}
+
+	task := <-responseCh
+	return task.info
+}
+
+func (q *queue) getNext(filter func(FileInfo) bool, scheduledAt func(FileInfo) time.Time) FileInfo {
+	responseCh := make(chan getByIdResponse, 1)
+
+	q.getNextCh <- getNextRequest{
+		filter:      filter,
+		scheduledAt: scheduledAt,
+
+		responseCh: responseCh,
+	}
+
+	task := <-responseCh
+	return task.info
+}
+
+func (q *queue) release(task FileInfo) {
+	q.releaseCh <- task
 }
--- core/files/filesync/temp/temp_test.go
+++ core/files/filesync/temp/temp_test.go
@@ -1,49 +1,211 @@
 package temp
 
 import (
-	"fmt"
 	"sync"
 	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestQueue(t *testing.T) {
-	q := newInmemoryQueue()
+	store := &storage{files: make(map[string]FileInfo)}
+	q := newQueue(store)
 
-	for _, fi := range []FileInfo{
-		{
-			ObjectId: "f1",
-			Value:    1,
-		},
-		{
-			ObjectId: "f2",
-			Value:    2,
-		},
-		{
-			ObjectId: "f3",
-			Value:    3,
-		},
-	} {
-		q.process(fi.Key(), func(exists bool, info FileInfo) (FileInfo, error) {
-			return fi, nil
-		})
-	}
+	go func() {
+		q.run()
+	}()
+
+	q.release(FileInfo{
+		ObjectId: "obj1",
+	})
 
 	var wg sync.WaitGroup
-	for range 10 {
+	for range 100 {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			for _, key := range []string{
-				"f1", "f2", "f3",
-			} {
-				q.process(key, func(exists bool, info FileInfo) (FileInfo, error) {
-					info.Value++
-					return info, nil
-				})
-			}
+			task := q.get("obj1")
+			task.BytesToUpload++
+			q.release(task)
 		}()
 	}
 
 	wg.Wait()
-	fmt.Println("fsdfs")
+	want := FileInfo{
+		ObjectId:      "obj1",
+		BytesToUpload: 100,
+	}
+	got := q.get("obj1")
+	assert.Equal(t, want, got)
+}
+
+func TestQueueSchedule(t *testing.T) {
+	t.Run("basic schedule", func(t *testing.T) {
+		synctest.Run(func() {
+			store := &storage{files: make(map[string]FileInfo)}
+			q := newQueue(store)
+
+			go func() {
+				q.run()
+			}()
+			defer q.close()
+
+			q.release(FileInfo{
+				ObjectId:    "obj1",
+				State:       FileStateUploading,
+				ScheduledAt: time.Now().Add(time.Minute),
+			})
+
+			next := q.getNext(func(info FileInfo) bool {
+				return info.State == FileStateUploading
+			}, func(info FileInfo) time.Time {
+				return info.ScheduledAt
+			})
+			assert.Equal(t, "obj1", next.ObjectId)
+		})
+	})
+
+	t.Run("wait for suitable item", func(t *testing.T) {
+		synctest.Run(func() {
+			store := &storage{files: make(map[string]FileInfo)}
+			q := newQueue(store)
+
+			go func() {
+				q.run()
+			}()
+			defer q.close()
+
+			go func() {
+				time.Sleep(10 * time.Minute)
+				q.release(FileInfo{
+					ObjectId:    "obj1",
+					State:       FileStateUploading,
+					ScheduledAt: time.Now().Add(time.Minute),
+				})
+			}()
+
+			next := q.getNext(func(info FileInfo) bool {
+				return info.State == FileStateUploading
+			}, func(info FileInfo) time.Time {
+				return info.ScheduledAt
+			})
+			assert.Equal(t, "obj1", next.ObjectId)
+		})
+	})
+
+	t.Run("skip locked", func(t *testing.T) {
+		synctest.Run(func() {
+			store := &storage{files: make(map[string]FileInfo)}
+			q := newQueue(store)
+
+			go func() {
+				q.run()
+			}()
+			defer q.close()
+
+			q.release(FileInfo{
+				ObjectId:    "obj1",
+				State:       FileStateUploading,
+				ScheduledAt: time.Now().Add(time.Minute),
+			})
+			q.release(FileInfo{
+				ObjectId:    "obj2",
+				State:       FileStateUploading,
+				ScheduledAt: time.Now().Add(10 * time.Minute),
+			})
+
+			// Lock obj1
+			q.get("obj1")
+
+			next := q.getNext(func(info FileInfo) bool {
+				return info.State == FileStateUploading
+			}, func(info FileInfo) time.Time {
+				return info.ScheduledAt
+			})
+			assert.Equal(t, "obj2", next.ObjectId)
+		})
+	})
+
+	t.Run("the second object became scheduled for earlier", func(t *testing.T) {
+		synctest.Run(func() {
+			store := &storage{files: make(map[string]FileInfo)}
+			q := newQueue(store)
+
+			go func() {
+				q.run()
+			}()
+			defer q.close()
+
+			q.release(FileInfo{
+				ObjectId:    "obj1",
+				State:       FileStateUploading,
+				ScheduledAt: time.Now().Add(time.Minute),
+			})
+			q.release(FileInfo{
+				ObjectId:    "obj2",
+				State:       FileStateUploading,
+				ScheduledAt: time.Now().Add(time.Hour),
+			})
+
+			go func() {
+				time.Sleep(500 * time.Millisecond)
+
+				q.release(FileInfo{
+					ObjectId:    "obj2",
+					State:       FileStateUploading,
+					ScheduledAt: time.Now().Add(time.Millisecond),
+				})
+			}()
+
+			next := q.getNext(func(info FileInfo) bool {
+				return info.State == FileStateUploading
+			}, func(info FileInfo) time.Time {
+				return info.ScheduledAt
+			})
+			assert.Equal(t, "obj2", next.ObjectId)
+		})
+	})
+
+	t.Run("re-schedule when changed in mid time", func(t *testing.T) {
+		synctest.Run(func() {
+			store := &storage{files: make(map[string]FileInfo)}
+			q := newQueue(store)
+
+			go func() {
+				q.run()
+			}()
+			defer q.close()
+
+			q.release(FileInfo{
+				ObjectId:    "obj1",
+				State:       FileStateUploading,
+				ScheduledAt: time.Now().Add(time.Minute),
+			})
+			q.release(FileInfo{
+				ObjectId:    "obj2",
+				State:       FileStateUploading,
+				ScheduledAt: time.Now().Add(time.Hour),
+			})
+
+			// Object1 was locked for 2 minutes and was changed, so it was no longer satisfied a filter.
+			// Object2 then should be scheduled next
+			go func() {
+				q.get("obj1")
+				time.Sleep(2 * time.Minute)
+				q.release(FileInfo{
+					ObjectId: "obj1",
+					State:    FileStateDeleted,
+				})
+			}()
+
+			next := q.getNext(func(info FileInfo) bool {
+				return info.State == FileStateUploading
+			}, func(info FileInfo) time.Time {
+				return info.ScheduledAt
+			})
+			assert.Equal(t, "obj2", next.ObjectId)
+		})
+	})
 }
