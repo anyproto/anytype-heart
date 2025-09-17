19c82dbf8ebb1903f5de67b8ffbad9dd0f56e917
kirillston <stonozhenko@anytype.io>
2025-09-17 15:43:09 +0200

    Fix test imports

--- core/block/editor/basic/details_test.go
+++ core/block/editor/basic/details_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/anyproto/anytype-heart/core/block/editor/converter"
 	"github.com/anyproto/anytype-heart/core/block/editor/smartblock/smarttest"
