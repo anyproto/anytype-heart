f5b3350f3056d575b1cc647852be9a4392a104b5
kirillston <stonozhenko@anytype.io>
2025-09-17 20:15:02 +0200

    GO-3743 Fix md import of code blocks with indentation

--- core/converter/md/md.go
+++ core/converter/md/md.go
@@ -592,8 +592,10 @@ func (h *MD) renderText(buf writer, in *renderState, b *model.Block) {
 		h.renderChildren(buf, in, b)
 	case model.BlockContentText_Code:
 		buf.WriteString("```\n")
-		buf.WriteString(strings.ReplaceAll(text.Text, "```", "\\`\\`\\`"))
-		buf.WriteString("\n```\n")
+		txt := strings.ReplaceAll(text.Text, "```", "\\`\\`\\`")
+		txt = strings.ReplaceAll(txt, "\n", "\n"+in.indent)
+		buf.WriteString(in.indent + txt)
+		buf.WriteString("\n" + in.indent + "```\n")
 		h.renderChildren(buf, in, b)
 	case model.BlockContentText_Checkbox:
 		if text.Checked {
