90d1ccc23ba6f5fa04f31968ecc4cf98fc0e6483
Sergey Cherepanov <cheggaaa@gmail.com>
2025-09-26 18:26:45 +0200

    GO-6291 fix local only account recovery + async peer connect

--- space/spacecore/localdiscovery/localdiscovery.go
+++ space/spacecore/localdiscovery/localdiscovery.go
@@ -218,7 +218,7 @@ func (l *localDiscovery) startServer() (err error) {
 		l.ipv4, // do not include ipv6 addresses, because they are disabled
 		nil,
 		l.interfacesAddrs.NetInterfaces(),
-		zeroconf.TTL(3600), // big ttl because we don't have re-broadcasting
+		zeroconf.TTL(3600),                            // big ttl because we don't have re-broadcasting
 		zeroconf.ServerSelectIPTraffic(zeroconf.IPv4), // disable ipv6 for now
 		zeroconf.WriteTimeout(time.Second*3),
 	)
@@ -252,8 +252,7 @@ func (l *localDiscovery) readAnswers(ch chan *zeroconf.ServiceEntry) {
 		log.Debug("discovered peer", zap.Strings("addrs", portAddrs), zap.String("peerId", peer.PeerId))
 		if l.notifier != nil {
 			// explicitly use componentCtx, instead of queryCtx here, because we don't want to interrupt the peer connection if we refreshed interfaces and restarted service
-			// todo: consider to do in goroutine, so we can try to connect multiple peers at once
-			l.notifier.PeerDiscovered(l.componentCtx, peer, OwnAddresses{
+			go l.notifier.PeerDiscovered(l.componentCtx, peer, OwnAddresses{
 				Addrs: l.ipv4,
 				Port:  l.port,
 			})
