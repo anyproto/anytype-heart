3e66f22a7193f49ece8a18978b83aafde36f61de
Sergey <smalenkov@anytype.io>
2025-10-02 15:19:29 +0200

    GO-5777: Fix chat object creation

--- core/block/object/objectcreator/chat.go
+++ core/block/object/objectcreator/chat.go
@@ -34,13 +34,18 @@ func (s *service) AddChatDerivedObject(ctx context.Context, space clientspace.Sp
 }
 
 func (s *service) createChatDerived(ctx context.Context, space clientspace.Space, details *domain.Details) (string, *domain.Details, error) {
-	uniqueKey := details.GetString(bundle.RelationKeyUniqueKey)
-	key, err := domain.UnmarshalUniqueKey(uniqueKey)
-	if err != nil {
-		return "", nil, fmt.Errorf("unmarshal unique key: %w", err)
+	uniqueKey, hasUniqueKey := details.TryString(bundle.RelationKeyUniqueKey)
+	var createState *state.State
+	if hasUniqueKey {
+		key, err := domain.UnmarshalUniqueKey(uniqueKey)
+		if err != nil {
+			return "", nil, fmt.Errorf("unmarshal unique key: %w", err)
+		}
+		createState = state.NewDocWithUniqueKey("", nil, key).(*state.State)
+	} else {
+		createState = state.NewDoc("", nil).(*state.State)
 	}
 
-	createState := state.NewDocWithUniqueKey("", nil, key).(*state.State)
 	details.Set(bundle.RelationKeyLayout, domain.Int64(int64(model.ObjectType_chatDerived)))
 	details.Set(bundle.RelationKeyIsHidden, domain.Bool(true))
 	createState.SetDetails(details)
