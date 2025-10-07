4233f9dd3e72116355cd08454359082661daade7
kirillston <stonozhenko@anytype.io>
2025-10-07 15:02:37 +0200

    Fix date relation filters

--- pkg/lib/database/filter_test.go
+++ pkg/lib/database/filter_test.go
@@ -989,7 +989,7 @@ func TestFilterHasPrefix_FilterObject(t *testing.T) {
 }
 
 func TestFilterDate(t *testing.T) {
-	t.Run("filter by date only", func(t *testing.T) {
+	t.Run("filter by date without time", func(t *testing.T) {
 		store := &stubSpaceObjectStore{}
 
 		now := time.Unix(1736332001, 0).UTC()
@@ -1002,10 +1002,34 @@ func TestFilterDate(t *testing.T) {
 		require.NoError(t, err)
 
 		obj1 := domain.NewDetailsFromMap(map[domain.RelationKey]domain.Value{bundle.RelationKeyCreatedDate: domain.Int64(now.Add(-time.Hour * 24).Unix())})
-		obj2 := domain.NewDetailsFromMap(map[domain.RelationKey]domain.Value{bundle.RelationKeyCreatedDate: domain.Int64(now.Unix())})
-		obj3 := domain.NewDetailsFromMap(map[domain.RelationKey]domain.Value{bundle.RelationKeyCreatedDate: domain.Int64(now.Add(24 * time.Hour).Unix())})
+		obj2 := domain.NewDetailsFromMap(map[domain.RelationKey]domain.Value{bundle.RelationKeyCreatedDate: domain.Int64(now.Add(time.Hour).Unix())})
+		obj3 := domain.NewDetailsFromMap(map[domain.RelationKey]domain.Value{bundle.RelationKeyCreatedDate: domain.Int64(now.Unix())})
+		obj4 := domain.NewDetailsFromMap(map[domain.RelationKey]domain.Value{bundle.RelationKeyCreatedDate: domain.Int64(now.Add(24 * time.Hour).Unix())})
 		assertFilter(t, f, obj1, false)
 		assertFilter(t, f, obj2, true)
-		assertFilter(t, f, obj3, false)
+		assertFilter(t, f, obj3, true)
+		assertFilter(t, f, obj4, false)
+	})
+	t.Run("filter by date with time", func(t *testing.T) {
+		store := &stubSpaceObjectStore{}
+
+		now := time.Unix(1736332001, 0).UTC()
+		f, err := MakeFilter("spaceId", FilterRequest{
+			RelationKey: bundle.RelationKeyCreatedDate,
+			Format:      model.RelationFormat_date,
+			Condition:   model.BlockContentDataviewFilter_Equal,
+			Value:       domain.Int64(now.Unix()),
+			IncludeTime: true,
+		}, store)
+		require.NoError(t, err)
+
+		obj1 := domain.NewDetailsFromMap(map[domain.RelationKey]domain.Value{bundle.RelationKeyCreatedDate: domain.Int64(now.Add(-time.Hour * 24).Unix())})
+		obj2 := domain.NewDetailsFromMap(map[domain.RelationKey]domain.Value{bundle.RelationKeyCreatedDate: domain.Int64(now.Add(time.Hour).Unix())})
+		obj3 := domain.NewDetailsFromMap(map[domain.RelationKey]domain.Value{bundle.RelationKeyCreatedDate: domain.Int64(now.Unix())})
+		obj4 := domain.NewDetailsFromMap(map[domain.RelationKey]domain.Value{bundle.RelationKeyCreatedDate: domain.Int64(now.Add(24 * time.Hour).Unix())})
+		assertFilter(t, f, obj1, false)
+		assertFilter(t, f, obj2, false)
+		assertFilter(t, f, obj3, true)
+		assertFilter(t, f, obj4, false)
 	})
 }
