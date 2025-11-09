7ef8b534a82851b720c7b1ab9db3a12899f7f9c4
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2025-11-09 15:10:49 +0100

    GO-5645 Refactor OpenAPI docs gen with improved sed usage

--- makefiles/tools.mk
+++ makefiles/tools.mk
@@ -11,6 +11,6 @@ openapi:
 	@swag init --v3.1 -q -d core/api -g service.go -o $(OPENAPI_DOCS_DIR)
 	@echo 'Removing package prefixes from definitions...'
 	@jq '.components.schemas |= with_entries(.key |= (gsub("apimodel\\."; "") | gsub("apimodel_"; "") | gsub("pagination\\."; "") | gsub("pagination_"; "") | gsub("util\\."; "") | gsub("util_"; ""))) | walk(if type == "string" then (gsub("apimodel\\."; "") | gsub("apimodel_"; "") | gsub("pagination\\."; "") | gsub("pagination_"; "") | gsub("util\\."; "") | gsub("util_"; "")) else . end)' "$(OPENAPI_DOCS_DIR)/swagger.json" > "$(OPENAPI_DOCS_DIR)/openapi.json" && rm "$(OPENAPI_DOCS_DIR)/swagger.json"
-	@sed -i '' 's/apimodel[._]//g; s/pagination[._]//g; s/util[._]//g' "$(OPENAPI_DOCS_DIR)/swagger.yaml" && mv "$(OPENAPI_DOCS_DIR)/swagger.yaml" "$(OPENAPI_DOCS_DIR)/openapi.yaml"
+	@sed -i.bak 's/apimodel[._]//g; s/pagination[._]//g; s/util[._]//g' "$(OPENAPI_DOCS_DIR)/swagger.yaml" && rm -f "$(OPENAPI_DOCS_DIR)/swagger.yaml.bak" && mv "$(OPENAPI_DOCS_DIR)/swagger.yaml" "$(OPENAPI_DOCS_DIR)/openapi.yaml"
 	@echo 'Formatting openapi docs...'
 	@swag fmt -d core/api
