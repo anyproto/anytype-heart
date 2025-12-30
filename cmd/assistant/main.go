f24b01bce638313de87c1600822e0bd7ab9a5107
Anatolii Smolianinov <zarkonesmall@gmail.com>
2025-12-30 13:00:45 +0100

    GO-0000 copy bits of assistant from bot-assistant branch

---
+++ cmd/assistant/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	anystore "github.com/anyproto/any-store"
+	"github.com/anyproto/anytype-heart/pb"
+	"github.com/anyproto/anytype-heart/pkg/lib/logging"
+	"github.com/anyproto/anytype-heart/util/keyvaluestore"
+)
+
+var log = logging.Logger("assistant").Desugar()
+
+type DbProvider interface {
+	GetCommonDb() anystore.DB
+}
+
+func run() error {
+	if len(os.Args) < 3 {
+		return fmt.Errorf("pass datadir and name as argument")
+	}
+	dataDir := os.Args[1]
+
+	ctx := context.Background()
+	app, err := createAccountAndStartApp(ctx, dataDir, os.Args[2], pb.RpcObjectImportUseCaseRequest_CHAT_SPACE)
+	if err != nil {
+		return fmt.Errorf("create account: %w", err)
+	}
+
+	err = app.config.Validate()
+	if err != nil {
+		return fmt.Errorf("invalid config: %w", err)
+	}
+
+	dbProvider := getService[DbProvider](app)
+	db := dbProvider.GetCommonDb()
+	handledMessages, err := keyvaluestore.New(db, "assistant/handledMessages", func(t string) ([]byte, error) {
+		return []byte(t), nil
+	}, func(bytes []byte) (string, error) {
+		return string(bytes), nil
+	})
+	if err != nil {
+		return fmt.Errorf("init handled messages store: %w", err)
+	}
+	_ = handledMessages
+
+	for {
+		msg, err := app.eventQueue.WaitOne(ctx)
+		if err != nil {
+			return fmt.Errorf("wait event: %w", err)
+		}
+		chatAddEv := msg.GetChatAdd()
+		if chatAddEv != nil {
+			fmt.Printf("-- chat msg: %s\n", chatAddEv.Message)
+		}
+	}
+
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
