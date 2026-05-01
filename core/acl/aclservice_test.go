16926e63a67925b20a1b31b6681bae01ff68cc9f
Roman Khafizianov <requilence@gmail.com>
2026-05-02 00:12:09 +0200

    GO-6473 Verify Admin permission-change errors propagate through aclService

--- core/acl/aclservice_test.go
+++ core/acl/aclservice_test.go
@@ -1128,3 +1128,44 @@ func TestService_Accept_Admin(t *testing.T) {
 	err := fx.Accept(ctx, spaceId, identityB, model.ParticipantPermissions_Admin)
 	require.NoError(t, err)
 }
+
+func TestService_ChangePermissions_AdminRejectedByValidator(t *testing.T) {
+	fx := newFixture(t)
+	defer fx.finish(t)
+	spaceId := "spaceId"
+	mockSpace := mock_clientspace.NewMockSpace(t)
+	mockCommonSpace := mock_commonspace.NewMockSpace(fx.ctrl)
+	fx.mockSpaceService.EXPECT().Get(ctx, spaceId).Return(mockSpace, nil)
+	mockSpace.EXPECT().CommonSpace().Times(2).Return(mockCommonSpace)
+	exec := list.NewAclExecutor(spaceId)
+	type cmdErr struct {
+		cmd string
+		err error
+	}
+	cmds := []cmdErr{
+		{"a.init::a", nil},
+		{"a.invite::invId", nil},
+		{"b.join::invId", nil},
+		{"a.approve::b,r", nil},
+	}
+	for _, cmd := range cmds {
+		err := exec.Execute(cmd.cmd)
+		require.Equal(t, cmd.err, err, cmd)
+	}
+	identityB := exec.ActualAccounts()["b"].Keys.SignKey.GetPublic()
+	acl := mockSyncAcl{exec.ActualAccounts()["a"].Acl}
+	mockCommonSpace.EXPECT().Acl().Return(acl)
+	aclClient := mock_aclclient.NewMockAclSpaceClient(fx.ctrl)
+	mockCommonSpace.EXPECT().AclClient().Return(aclClient)
+	// Simulate the underlying any-sync validator rejecting the change because the
+	// caller is not the owner. The heart should wrap this as ErrAclRequestFailed.
+	aclClient.EXPECT().ChangePermissions(ctx, gomock.Any()).Return(list.ErrInsufficientPermissions)
+
+	err := fx.ChangePermissions(ctx, spaceId, []AccountPermissions{{
+		Account:     identityB,
+		Permissions: model.ParticipantPermissions_Admin,
+	}})
+	require.Error(t, err)
+	require.ErrorIs(t, err, ErrAclRequestFailed)
+	require.ErrorIs(t, err, list.ErrInsufficientPermissions)
+}
